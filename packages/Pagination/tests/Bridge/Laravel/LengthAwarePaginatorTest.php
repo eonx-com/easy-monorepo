4d2369d5ef8a93d7697a4ad969324886fae8f489
natepage <nathan.page86@gmail.com>
2019-01-21 12:20:50 +1100

    Create LengthAwarePaginator interface and its implementation for Laravel

---
+++ packages/Pagination/tests/Bridge/Laravel/LengthAwarePaginatorTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace StepTheFkUp\Pagination\Tests\Bridge\Laravel;
+
+use Illuminate\Pagination\LengthAwarePaginator as IlluminateLengthAwarePaginator;
+use StepTheFkUp\Pagination\Bridge\Laravel\LengthAwarePaginator;
+use StepTheFkUp\Pagination\Tests\AbstractTestCase;
+
+class LengthAwarePaginatorTest extends AbstractTestCase
+{
+    /**
+     * Paginator should return expected output data based on input data.
+     *
+     * @return void
+     */
+    public function testGetters(): void
+    {
+        $items = [];
+        $page = 1;
+        $perPage = 10;
+        $total = 100;
+
+        $paginator = new LengthAwarePaginator(new IlluminateLengthAwarePaginator($items, $total, $perPage, $page));
+
+        self::assertEquals($page, $paginator->getCurrentPage());
+        self::assertEquals($items, $paginator->getItems());
+        self::assertEquals($perPage, $paginator->getItemsPerPage());
+        self::assertEquals($total, $paginator->getTotalItems());
+        self::assertEquals(10, $paginator->getTotalPages());
+        self::assertTrue($paginator->hasNextPage());
+        self::assertFalse($paginator->hasPreviousPage());
+    }
+}
