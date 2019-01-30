fd493a29ec5cca57e7e4aa9dd19f0ff6d6b5dc2b
natepage <nathan.page86@gmail.com>
2019-01-30 12:37:00 +1100

    Add config file for Laravel bridge.

---
+++ packages/EasyRepository/src/Bridge/Laravel/config/easy-repository.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Repositories List
+    |--------------------------------------------------------------------------
+    |
+    | Here you will define all your application's repositories to be registered
+    | as services in the container.
+    | Repositories list must be an associative array where the keys are the
+    | abstraction of your repositories (most likely an interface) and the values
+    | the concrete class of your repositories.
+    |
+    | Example:
+    | \App\Repositories\PostRepositoryInterface::class => \App\Repositories\PostRepository::class,
+    | \App\Repositories\CommentRepositoryInterface::class => \App\Repositories\CommentRepository::class
+    |
+    */
+    'repositories' => [
+        // Define your repositories here...
+    ]
+];
