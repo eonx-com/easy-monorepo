4ea77ca5c7b93a854976d7f9c9055a1c7e9e0c5d
natepage <nathan.page86@gmail.com>
2019-02-08 09:20:53 +1100

    Update Abstract Doctrine repository to have a proper ORM implementation.

--- packages/EasyRepository/tests/Implementation/Doctrine/DoctrineOrmRepositoryStub.php
+++ packages/EasyRepository/tests/Implementation/Doctrine/DoctrineOrmRepositoryStub.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 namespace StepTheFkUp\EasyRepository\Tests\Implementation\Doctrine;
 
-use StepTheFkUp\EasyRepository\Implementations\Doctrine\AbstractDoctrineRepository;
+use StepTheFkUp\EasyRepository\Implementations\Doctrine\AbstractDoctrineOrmRepository;
 
-final class DoctrineRepositoryStub extends AbstractDoctrineRepository
+final class DoctrineOrmRepositoryStub extends AbstractDoctrineOrmRepository
 {
     /**
      * Get entity class managed by the repository.
