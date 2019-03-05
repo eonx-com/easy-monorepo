ab9e353398375975cb3e964bbdd91c81c6ef8e15
natepage <nathan.page86@gmail.com>
2019-03-05 13:22:41 +1100

    [Ps7Factory] Update service provider tests to use lumen app

--- packages/Psr7Factory/tests/Bridge/Laravel/Psr7FactoryServiceProviderTest.php
+++ packages/Psr7Factory/tests/Bridge/Laravel/Psr7FactoryServiceProviderTest.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace StepTheFkUp\Psr7Factory\Tests\Bridge\Laravel;
 
-use Illuminate\Contracts\Foundation\Application;
-use Mockery\MockInterface;
 use StepTheFkUp\Psr7Factory\Bridge\Laravel\Psr7FactoryServiceProvider;
 use StepTheFkUp\Psr7Factory\Interfaces\Psr7FactoryInterface;
 use StepTheFkUp\Psr7Factory\Psr7Factory;
@@ -19,16 +17,11 @@ final class Psr7FactoryServiceProviderTest extends AbstractTestCase
      */
     public function testRegisterExpectedServices(): void
     {
+        $app = new \Laravel\Lumen\Application(__DIR__);
+
         /** @var \Illuminate\Contracts\Foundation\Application $app */
-        $app = $this->mock(Application::class, function (MockInterface $app): void {
-            $app->shouldReceive('bind')->once()->withArgs(function (string $abstract, string $concrete): bool {
-                self::assertEquals(Psr7FactoryInterface::class, $abstract);
-                self::assertEquals(Psr7Factory::class, $concrete);
-
-                return true;
-            });
-        });
-
         (new Psr7FactoryServiceProvider($app))->register();
+
+        self::assertInstanceOf(Psr7Factory::class, $app->get(Psr7FactoryInterface::class));
     }
 }
