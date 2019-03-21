f893e2cc37fef54f27b42af4a94d63c86f971a68
natepage <nathan.page86@gmail.com>
2019-03-22 09:42:38 +1100

    [EasyIdentity] Make identity service and user more generic

---
+++ packages/EasyIdentity/src/Interfaces/IdentityServiceNamesInterface.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace StepTheFkUp\EasyIdentity\Interfaces;
+
+interface IdentityServiceNamesInterface
+{
+    /** @var string */
+    public const SERVICE_AUTH0 = 'auth0';
+
+    /** @var string[] */
+    public const SERVICES = [
+        self::SERVICE_AUTH0
+    ];
+}
