2a0f79736e6cb4f70ac112f0e10bea1bb0e55864
natepage <nathan.page86@gmail.com>
2019-05-04 20:08:01 +1000

    [EasyPsr7Factory] Create Symfony bundle

---
+++ packages/EasyPsr7Factory/src/Bridge/Symfony/EasyPsr7FactoryBundle.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace LoyaltyCorp\EasyPsr7Factory\Bridge\Symfony;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class EasyPsr7FactoryBundle extends Bundle
+{
+    // No body needed.
+}
---
+++ packages/EasyPsr7Factory/tests/Bridge/Symfony/Stubs/KernelStub.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace LoyaltyCorp\EasyPsr7Factory\Tests\Bridge\Symfony\Stubs;
+
+use LoyaltyCorp\EasyPsr7Factory\Bridge\Symfony\EasyPsr7FactoryBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+final class KernelStub extends Kernel implements CompilerPassInterface
+{
+    /**
+     * KernelStub constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct('test', true);
+    }
+
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     *
+     * @return void
+     */
+    public function process(ContainerBuilder $container): void
+    {
+        $container
+            ->setDefinition(ServiceStub::class, new Definition(ServiceStub::class))
+            ->setAutowired(true)
+            ->setPublic(true);
+    }
+
+    /**
+     * Returns an array of bundles to register.
+     *
+     * @return iterable|BundleInterface[] An iterable of bundle instances
+     */
+    public function registerBundles()
+    {
+        yield new EasyPsr7FactoryBundle();
+    }
+
+    /**
+     * Loads the container configuration.
+     *
+     * @param \Symfony\Component\Config\Loader\LoaderInterface $loader
+     *
+     * @return void
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        // No body needed.
+    }
+}
