b16515e78d00138a74391687d3e363b7b412aef2
Tim Nagel <tim@nagel.com.au>
2019-12-29 13:36:39 +1100

    Add test for provided ids (or ids that come from persist)

--- packages/EasyEntityChange/tests/Integration/TestCases/Integration/DoctrineORMTestCaseTest.php
+++ packages/EasyEntityChange/tests/Integration/TestCases/Integration/DoctrineORMTestCaseTest.php
@@ -7,6 +7,7 @@ use EonX\EasyEntityChange\DataTransferObjects\UpdatedEntity;
 use EonX\EasyEntityChange\Doctrine\EntityChangeSubscriber;
 use EonX\EasyEntityChange\Events\EntityChangeEvent;
 use EonX\EasyEntityChange\Tests\Integration\Fixtures\SimpleEntity;
+use EonX\EasyEntityChange\Tests\Integration\Fixtures\ProvidedIdEntity;
 use EonX\EasyEntityChange\Tests\Stubs\EventDispatcherStub;
 use Eonx\TestUtils\TestCases\Integration\DoctrineORMTestCase;
 
@@ -20,7 +21,7 @@ class DoctrineORMTestCaseTest extends DoctrineORMTestCase
      *
      * @return void
      */
-    public function testEntityChangeEventIsDispatched(): void
+    public function testEntityChangeEventIsDispatchedWithDbId(): void
     {
         $expectedDispatches = [
             new EntityChangeEvent([
@@ -34,6 +35,33 @@ class DoctrineORMTestCaseTest extends DoctrineORMTestCase
         $entityManager->getEventManager()->addEventSubscriber(new EntityChangeSubscriber($dispatcher));
 
         $entity = new SimpleEntity();
+        $entity->setProperty('hello');
+
+        $entityManager->persist($entity);
+        $entityManager->flush();
+
+        self::assertEquals($expectedDispatches, $dispatcher->getDispatched());
+    }
+
+    /**
+     * Tests that the entity change event fires when doctrine really calls the subscriber.
+     *
+     * @return void
+     */
+    public function testEntityChangeEventIsDispatchedWithProvidedId(): void
+    {
+        $expectedDispatches = [
+            new EntityChangeEvent([
+                new UpdatedEntity(['id', 'property'], ProvidedIdEntity::class, ['id' => 'uuid'])
+            ])
+        ];
+
+        $dispatcher = new EventDispatcherStub();
+
+        $entityManager = $this->getEntityManager();
+        $entityManager->getEventManager()->addEventSubscriber(new EntityChangeSubscriber($dispatcher));
+
+        $entity = new ProvidedIdEntity('uuid');
         $entity->setProperty('hello');
 
         $entityManager->persist($entity);
