af2020f9644913678685597264c3ee9fd8da288e
Nathan Page <nathan.page@eonx.com>
2020-03-11 13:36:07 +1100

    [EasyAsync] Implement messenger middleware for process job log (#157)

--- packages/EasyAsync/src/Bridge/Symfony/DependencyInjection/EasyAsyncExtension.php
+++ packages/EasyAsync/src/Bridge/Symfony/DependencyInjection/EasyAsyncExtension.php
@@ -3,12 +3,15 @@ declare(strict_types=1);
 
 namespace EonX\EasyAsync\Bridge\Symfony\DependencyInjection;
 
+use EonX\EasyAsync\Bridge\Symfony\Messenger\ProcessJobLogMiddleware;
 use EonX\EasyAsync\Exceptions\InvalidImplementationException;
 use EonX\EasyAsync\Interfaces\ImplementationsInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Messenger\DependencyInjection\MessengerPass;
 
 final class EasyAsyncExtension extends Extension
 {
@@ -36,5 +39,10 @@ final class EasyAsyncExtension extends Extension
         }
 
         $loader->load(\sprintf('implementations/%s.yaml', $implementation));
+
+        // Register middleware if messenger present
+        if (\class_exists(MessengerPass::class)) {
+            $container->setDefinition(ProcessJobLogMiddleware::class, new Definition(ProcessJobLogMiddleware::class));
+        }
     }
 }
