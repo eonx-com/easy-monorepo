b772ba94b45f8d31c4937b2ffa30a1e6f01ff42a
Nathan Page <nathan.page@eonx.com>
2020-03-13 09:15:46 +1100

    [EasyPsr7Factory] Remove deprecated code (#160)

--- packages/EasyPsr7Factory/src/EasyPsr7Factory.php
+++ packages/EasyPsr7Factory/src/EasyPsr7Factory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace EonX\EasyPsr7Factory;
 
 use EonX\EasyPsr7Factory\Interfaces\EasyPsr7FactoryInterface;
+use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
@@ -12,10 +13,6 @@ use Symfony\Bridge\PsrHttpMessage\HttpFoundationFactoryInterface;
 use Symfony\Bridge\PsrHttpMessage\HttpMessageFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Zend\Diactoros\ResponseFactory;
-use Zend\Diactoros\ServerRequestFactory;
-use Zend\Diactoros\StreamFactory;
-use Zend\Diactoros\UploadedFileFactory;
 
 final class EasyPsr7Factory implements EasyPsr7FactoryInterface
 {
@@ -34,12 +31,7 @@ final class EasyPsr7Factory implements EasyPsr7FactoryInterface
         ?HttpMessageFactoryInterface $httpMessage = null
     ) {
         $this->httpFoundation = $httpFoundation ?? new HttpFoundationFactory();
-        $this->httpMessage = $httpMessage ?? new PsrHttpFactory(
-            new ServerRequestFactory(),
-            new StreamFactory(),
-            new UploadedFileFactory(),
-            new ResponseFactory()
-        );
+        $this->httpMessage = $httpMessage ?? $this->getDefaultHttpMessageFactory();
     }
 
     public function createRequest(Request $request): ServerRequestInterface
@@ -50,5 +42,12 @@ final class EasyPsr7Factory implements EasyPsr7FactoryInterface
     public function createResponse(ResponseInterface $response): Response
     {
         return $this->httpFoundation->createResponse($response);
+    }
+
+    private function getDefaultHttpMessageFactory(): HttpMessageFactoryInterface
+    {
+        $psr17Factory = new Psr17Factory();
+
+        return new PsrHttpFactory($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
     }
 }
