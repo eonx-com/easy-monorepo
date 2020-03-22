bcc01bf3638759fafd020301e2ff6f6ebb13e454
Cain Seing <CainSeing@users.noreply.github.com>
2020-03-23 10:36:40 +1100

    Add paginator test cases

--- packages/EasyPagination/tests/AbstractTestCase.php
+++ packages/EasyPagination/tests/AbstractTestCase.php
@@ -5,6 +5,7 @@ namespace EonX\EasyPagination\Tests;
 
 use EonX\EasyPagination\Resolvers\Config\StartSizeConfig;
 use Laravel\Lumen\Application;
+use Mockery\MockInterface;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\ServerRequestFactory;
@@ -58,5 +59,28 @@ abstract class AbstractTestCase extends TestCase
         $app->instance(ServerRequestInterface::class, $this->createServerRequest());
 
         return $app;
+    }
+
+    /**
+     * @param mixed $target
+     */
+    protected function mock($target, ?callable $expectations = null): MockInterface
+    {
+        $mock = \Mockery::mock($target);
+
+        if ($expectations !== null) {
+            \call_user_func($expectations, $mock);
+        }
+
+        return $mock;
+    }
+
+    protected function tearDown(): void
+    {
+        $this->addToAssertionCount(\Mockery::getContainer()->mockery_getExpectationCount());
+
+        \Mockery::close();
+
+        parent::tearDown();
     }
 }
