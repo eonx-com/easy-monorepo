4ef67f8188383a50ebf1cab090f93e999eecacf1
Nathan Page <nathan.page@eonx.com>
2020-03-24 16:29:30 +1100

    [EasyCore] Symfony bridge auto configure listeners (#174)
    * [EasyCore] Add auto-configure for event listeners in Symfony
    
    * [EasyCore] Fix cs

---
+++ packages/EasyCore/src/Bridge/Symfony/Interfaces/EventListenerInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyCore\Bridge\Symfony\Interfaces;
+
+interface EventListenerInterface
+{
+    // Marker for all event listeners for auto-configure.
+}
