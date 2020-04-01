434cd09257701f75c517ef2a3a06e35947f3efaf
Nathan Page <nathan.page@eonx.com>
2020-04-02 07:34:33 +1100

    [EasyDecision] Implement restricted decision configurator (#188)

---
+++ packages/EasyDecision/tests/Bridge/Symfony/Stubs/Configurators/NameRestrictedExpressionFunctionConfigurator.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyDecision\Tests\Bridge\Symfony\Stubs\Configurators;
+
+use EonX\EasyDecision\Configurators\AbstractNameRestrictedConfigurator;
+use EonX\EasyDecision\Expressions\ExpressionFunction;
+use EonX\EasyDecision\Interfaces\DecisionInterface;
+
+final class NameRestrictedExpressionFunctionConfigurator extends AbstractNameRestrictedConfigurator
+{
+    public function configure(DecisionInterface $decision): void
+    {
+        $decision->getExpressionLanguage()->addFunction(new ExpressionFunction('restricted', function (): void {
+        }));
+    }
+
+    protected function getName(): string
+    {
+        return 'restricted';
+    }
+}
