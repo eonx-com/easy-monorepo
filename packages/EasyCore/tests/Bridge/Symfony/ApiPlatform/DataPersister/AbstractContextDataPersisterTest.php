82b343a08d89fd37e0fe1fd4364d60bebe8f94cb
albertlabarento <AlbertLabarento@users.noreply.github.com>
2020-04-06 19:35:59 +0800

    [EasyCore] Added AbstractDataPersister and AbstractContextDataPe… (#195)
    * master
    
    - Fixed EasyCoreExtension to sync with Configuration
    - custom_pagination -> custom_pagination_enabled
    - simple_data_persister -> simple_data_persister_enabled
    
    * master
    
    - Fixed simple_data_persister to use service_container instead of container
    
    * master
    
    - Created AbstractContextDataPersister and AbstractDataPersister
    
    * master
    
    - Restored AbstractSimpleDataPersister
    
    * master
    
    - Removed dependency from AbstractDataPersister
    
    * master
    
    - Removed dependency from AbstractDataPersister
    
    * master
    
    - Resolved discussions
    
    * master
    
    - Resolved discussions
    
    * master
    
    - Resolved discussions
    
    * master
    
    - Removed doc blocks
    
    * master
    
    - Doc block for private properties
    
    * master
    
    - Removed doc block for mixed arguments
    
    Co-authored-by: Nathan Page <nathan.page@eonx.com>

---
+++ packages/EasyCore/tests/Bridge/Symfony/ApiPlatform/DataPersister/AbstractContextDataPersisterTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyCore\Tests\Bridge\Symfony\ApiPlatform\DataPersister;
+
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+use EonX\EasyCore\Tests\Bridge\Symfony\AbstractSymfonyTestCase;
+use EonX\EasyCore\Tests\Bridge\Symfony\Stubs\ContextDataPersisterStub;
+use EonX\EasyCore\Tests\Bridge\Symfony\Stubs\EntityStub;
+
+final class AbstractContextDataPersisterTest extends AbstractSymfonyTestCase
+{
+    public function testPersist(): void
+    {
+        $entity = new EntityStub();
+        $context = ['context' => ''];
+
+        $dataPersister = $this->prophesize(ContextAwareDataPersisterInterface::class);
+        $dataPersister->persist($entity, $context)
+            ->shouldBeCalledOnce()
+            ->hasReturnVoid();
+
+        self::assertNull((new ContextDataPersisterStub(
+            $dataPersister->reveal()
+        ))->persist($entity, $context));
+    }
+
+    public function testRemove(): void
+    {
+        $entity = new EntityStub();
+        $context = ['context' => ''];
+
+        $dataPersister = $this->prophesize(ContextAwareDataPersisterInterface::class);
+        $dataPersister->remove($entity, $context)
+            ->shouldBeCalledOnce()
+            ->hasReturnVoid();
+
+        self::assertNull((new ContextDataPersisterStub(
+            $dataPersister->reveal()
+        ))->remove($entity, $context));
+    }
+
+    public function testSupports(): void
+    {
+        self::assertTrue((new ContextDataPersisterStub(
+            $this->prophesize(ContextAwareDataPersisterInterface::class)->reveal()
+        ))->supports(new EntityStub(), []));
+    }
+}
---
+++ packages/EasyCore/tests/Bridge/Symfony/Stubs/DataPersisterStub.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyCore\Tests\Bridge\Symfony\Stubs;
+
+use EonX\EasyCore\Bridge\Symfony\ApiPlatform\DataPersister\AbstractDataPersister;
+
+final class DataPersisterStub extends AbstractDataPersister
+{
+    protected function getApiResourceClass(): string
+    {
+        return EntityStub::class;
+    }
+}
