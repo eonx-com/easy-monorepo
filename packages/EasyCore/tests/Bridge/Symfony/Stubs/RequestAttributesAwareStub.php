92793bc6aed1b5ddc8eb231d82de9eab302a3eca
Nathan Page <nathan.page@eonx.com>
2020-04-28 17:21:37 +1000

    [EasyCore] Allow setRequestAttributes to be set anywhere in the inheritance chain (#215)

--- packages/EasyCore/tests/Bridge/Symfony/Stubs/RequestAttributesAwareStub.php
+++ packages/EasyCore/tests/Bridge/Symfony/Stubs/RequestAttributesAwareStub.php
@@ -4,18 +4,8 @@ declare(strict_types=1);
 
 namespace EonX\EasyCore\Tests\Bridge\Symfony\Stubs;
 
-use EonX\EasyCore\Bridge\Symfony\ApiPlatform\DataPersister\AbstractSimpleDataPersister;
-use EonX\EasyCore\Bridge\Symfony\ApiPlatform\Traits\RequestAttributesAwareTrait;
-
-final class RequestAttributesAwareStub extends AbstractSimpleDataPersister
+final class RequestAttributesAwareStub extends AbstractRequestAttributesAwareStub
 {
-    use RequestAttributesAwareTrait;
-
-    /**
-     * @var mixed[]
-     */
-    private $requestAttributes;
-
     public function getApiResourceClass(): string
     {
         return '';
@@ -38,10 +28,5 @@ final class RequestAttributesAwareStub extends AbstractSimpleDataPersister
     public function persist($data, ?array $context = null)
     {
         return $data;
-    }
-
-    public function setRequestAttributes(string $param1): void
-    {
-        $this->requestAttributes = \func_get_args();
     }
 }
