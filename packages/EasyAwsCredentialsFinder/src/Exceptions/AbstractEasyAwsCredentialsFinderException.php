a909c571b3299f48c5783eea0ec81e15e05ed460
Nathan Page <nathan.page@eonx.com>
2020-06-10 21:49:54 +1000

    [EasyAwsCredentialsFinder] Implement SSO access token provider (#247)

---
+++ packages/EasyAwsCredentialsFinder/src/Exceptions/AbstractEasyAwsCredentialsFinderException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyAwsCredentialsFinder\Exceptions;
+
+use EonX\EasyAwsCredentialsFinder\Interfaces\EasyAwsCredentialsFinderExceptionInterface as ExceptionInterface;
+
+abstract class AbstractEasyAwsCredentialsFinderException extends \RuntimeException implements ExceptionInterface
+{
+    // No body needed.
+}
---
+++ packages/EasyAwsCredentialsFinder/src/Exceptions/InvalidCachedSsoAccessTokenContentsException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyAwsCredentialsFinder\Exceptions;
+
+final class InvalidCachedSsoAccessTokenContentsException extends AbstractEasyAwsCredentialsFinderException
+{
+    // No body needed.
+}
