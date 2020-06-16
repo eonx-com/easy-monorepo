3fa4c3e80016fc100b01cce4caead4f1d41e8122
Nathan Page <nathan.page@eonx.com>
2020-06-16 21:54:29 +1000

    [EasyApiToken] Update JwtDecoder to return null if not able to decode… (#252)

--- packages/EasyApiToken/src/Decoders/JwtTokenDecoder.php
+++ packages/EasyApiToken/src/Decoders/JwtTokenDecoder.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace EonX\EasyApiToken\Decoders;
 
+use EonX\EasyApiToken\Exceptions\InvalidEasyApiTokenFromRequestException;
 use EonX\EasyApiToken\Interfaces\EasyApiTokenDecoderInterface;
 use EonX\EasyApiToken\Interfaces\EasyApiTokenInterface;
 use EonX\EasyApiToken\Interfaces\Tokens\Factories\JwtEasyApiTokenFactoryInterface;
 use EonX\EasyApiToken\Traits\EasyApiTokenDecoderTrait;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 final class JwtTokenDecoder implements EasyApiTokenDecoderInterface
 {
@@ -19,9 +22,15 @@ final class JwtTokenDecoder implements EasyApiTokenDecoderInterface
      */
     private $jwtApiTokenFactory;
 
-    public function __construct(JwtEasyApiTokenFactoryInterface $jwtApiTokenFactory)
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(JwtEasyApiTokenFactoryInterface $jwtApiTokenFactory, ?LoggerInterface $logger = null)
     {
         $this->jwtApiTokenFactory = $jwtApiTokenFactory;
+        $this->logger = $logger ?? new NullLogger();
     }
 
     public function decode(ServerRequestInterface $request): ?EasyApiTokenInterface
@@ -32,6 +41,12 @@ final class JwtTokenDecoder implements EasyApiTokenDecoderInterface
             return null; // If Authorization doesn't start with Basic, return null
         }
 
-        return $this->jwtApiTokenFactory->createFromString($authorization);
+        try {
+            return $this->jwtApiTokenFactory->createFromString($authorization);
+        } catch (InvalidEasyApiTokenFromRequestException $exception) {
+            $this->logger->info(\sprintf('Invalid JWT token from request: "%s"', $exception->getMessage()));
+
+            return null;
+        }
     }
 }
