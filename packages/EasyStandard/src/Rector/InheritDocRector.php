b4e251c03ee112799c43d0536b46f7d98ca7bba5
Vladimir Shevelev <BOB41K@narod.ru>
2020-06-25 13:53:59 +0700

    [EasyStandard] Implement rector for fixing inheritdoc annotations (#257)
    Co-authored-by: v.shevelev <v.varitweb@gmail.com>
    Co-authored-by: Roman <roman.ovchinnikov@eonx.com>

---
+++ packages/EasyStandard/src/Rector/InheritDocRector.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyStandard\Rector;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassMethod;
+use Rector\AttributeAwarePhpDoc\Ast\PhpDoc\AttributeAwarePhpDocTextNode;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\RectorDefinition\CodeSample;
+use Rector\Core\RectorDefinition\RectorDefinition;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+
+final class InheritDocRector extends AbstractRector
+{
+    /**
+     * @var string
+     */
+    private const INHERITDOC_INCORRECT_ANNOTATION = '{@inheritdoc}';
+
+    /**
+     * @var string
+     */
+    private const INHERITDOC_CORRECT_ANNOTATION = '{@inheritDoc}';
+
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class];
+    }
+
+    public function refactor(Node $node): ?Node
+    {
+        /** @var \PhpParser\Node\Stmt\ClassMethod $classMethod */
+        $classMethod = $node;
+
+        /** @var \Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo $phpDocInfo */
+        $phpDocInfo = $classMethod->getAttribute(AttributeKey::PHP_DOC_INFO);
+
+        /** @var AttributeAwarePhpDocTextNode[] $children */
+        $children = $phpDocInfo->getPhpDocNode()->children;
+
+        foreach ($children as $key => $child) {
+            if ($child->getAttribute('original_content') === self::INHERITDOC_INCORRECT_ANNOTATION) {
+                $children[$key]->text = self::INHERITDOC_CORRECT_ANNOTATION;
+            }
+        }
+
+        $phpDocInfo->getPhpDocNode()->children = $children;
+
+        return $classMethod;
+    }
+
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition(
+            'Replaces {@inheritdoc} annotation with {@inheritDoc}.',
+            [
+                new CodeSample(
+                    <<<'PHP'
+/**
+ * {@inheritdoc}
+*/
+public function someMethod(): array
+{
+}
+PHP
+                    ,
+                    <<<'PHP'
+public function someMethod(): array
+{
+}
+PHP
+                ),
+            ]
+        );
+    }
+}
