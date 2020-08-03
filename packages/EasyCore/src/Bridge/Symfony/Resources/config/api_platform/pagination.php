cf8f2b169e278dd65a4e283a89e34193c5a42cc6
v.shevelev <v.varitweb@gmail.com>
2020-08-03 16:42:04 +0700

    hotfix/Symfony-PHP-Services-Config
    - packages/EasyCore/src/Bridge/Symfony/Resources/config/api_platform/debug.yaml
    - packages/EasyCore/src/Bridge/Symfony/Resources/config/api_platform/iri_converter.yaml
    - packages/EasyCore/src/Bridge/Symfony/Resources/config/api_platform/maker_commands.yaml
    - packages/EasyCore/src/Bridge/Symfony/Resources/config/api_platform/no_properties_api_resource.yaml
    - packages/EasyCore/src/Bridge/Symfony/Resources/config/api_platform/pagination.yaml
    - packages/EasyCore/src/Bridge/Symfony/Resources/config/api_platform/simple_data_persister.yaml
    - packages/EasyCore/src/Bridge/Symfony/Resources/config/easy_async_listeners.yaml
    - packages/EasyCore/src/Bridge/Symfony/Resources/config/services.yaml

---
+++ packages/EasyCore/src/Bridge/Symfony/Resources/config/api_platform/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use EonX\EasyCore\Bridge\Symfony\ApiPlatform\Pagination\CustomPaginationListener;
+use EonX\EasyCore\Bridge\Symfony\ApiPlatform\Pagination\SerializerContextBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->set(CustomPaginationListener::class)
+        ->tag('kernel.event_listener', ['event' => 'kernel.view', 'priority' => 17]);
+
+    $services->set(SerializerContextBuilder::class)
+        ->autoconfigure(false)
+        ->decorate('api_platform.serializer.context_builder')
+        ->args([ref('EonX\EasyCore\Bridge\Symfony\ApiPlatform\Pagination\SerializerContextBuilder.inner')]);
+};
