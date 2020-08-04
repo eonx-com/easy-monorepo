160d42cdf5947eef10b0cd943a2a5d5d6c89bf51
Anton Popov <koibpas@gmail.com>
2020-08-04 13:19:01 +0700

    hotfix/fix-easy-error-handler-config - added type casting in config

--- packages/EasyErrorHandler/src/Bridge/Laravel/config/easy-error-handler.php
+++ packages/EasyErrorHandler/src/Bridge/Laravel/config/easy-error-handler.php
@@ -7,7 +7,7 @@ return [
     /**
      * Use extended error response with exception message, trace, etc.
      */
-    'use_extended_response' => \env(
+    'use_extended_response' => \filter_var(
         \env('EASY_ERROR_HANDLER_USE_EXTENDED_RESPONSE', false), \FILTER_VALIDATE_BOOLEAN
     ),
 
