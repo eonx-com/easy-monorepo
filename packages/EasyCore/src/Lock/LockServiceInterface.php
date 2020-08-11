507842c57703b3d3fce28b0b31156d6ee5d28212
natepage <nathan.page86@gmail.com>
2020-08-11 12:52:29 +1000

    [EasyCore] Mark lock classes as deprecated, use eonx-com/easy-lock instead

--- packages/EasyCore/src/Lock/LockServiceInterface.php
+++ packages/EasyCore/src/Lock/LockServiceInterface.php
@@ -7,6 +7,9 @@ namespace EonX\EasyCore\Lock;
 use Symfony\Component\Lock\LockInterface;
 use Symfony\Component\Lock\PersistingStoreInterface;
 
+/**
+ * @deprecated Since 2.4.31. Will be remove in 3.0. Use eonx-com/easy-lock package instead.
+ */
 interface LockServiceInterface
 {
     public function createLock(string $resource, ?float $ttl = null): LockInterface;
--- packages/EasyCore/src/Lock/WithLockDataInterface.php
+++ packages/EasyCore/src/Lock/WithLockDataInterface.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace EonX\EasyCore\Lock;
 
+/**
+ * @deprecated Since 2.4.31. Will be remove in 3.0. Use eonx-com/easy-lock package instead.
+ */
 interface WithLockDataInterface
 {
     public function getLockData(): LockDataInterface;
