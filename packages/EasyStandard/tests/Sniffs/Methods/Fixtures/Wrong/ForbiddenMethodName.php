fd26d648d17c647729b3a6b15114fe2643ffd224
Vladimir Shevelev <BOB41K@narod.ru>
2020-08-20 16:55:08 +0700

    [EasyStandard] Add ecs sniff for test method names validation (#302)
    Co-authored-by: v.shevelev <v.varitweb@gmail.com>

---
+++ packages/EasyStandard/tests/Sniffs/Methods/Fixtures/Wrong/ForbiddenMethodName.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyStandard\Tests\Sniffs\Methods\Fixtures\Wrong;
+
+final class ForbiddenMethodName
+{
+    public function testCreateSucceedWithSomething(): void
+    {
+        // No body needed here
+    }
+}
