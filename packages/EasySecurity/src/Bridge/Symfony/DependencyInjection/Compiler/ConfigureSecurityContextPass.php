4b0046ce0c6b18387d85507ea600c6b980851a92
natepage <nathan.page86@gmail.com>
2020-08-21 08:48:26 +1000

    [WIP] Simplify easy-security

---
+++ packages/EasySecurity/src/Bridge/Symfony/DependencyInjection/Compiler/ConfigureSecurityContextPass.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasySecurity\Bridge\Symfony\DependencyInjection\Compiler;
+
+final class ConfigureSecurityContextPass
+{
+
+}
---
+++ packages/EasySecurity/tests/Bridge/Laravel/EasySecurityServiceProviderTest.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasySecurity\Tests\Bridge\Laravel;
+
+final class EasySecurityServiceProviderTest
+{
+
+}
