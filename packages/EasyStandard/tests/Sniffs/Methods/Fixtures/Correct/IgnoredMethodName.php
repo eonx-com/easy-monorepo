dc0d4d8462adf4d01cbef40c729ae5fa3ee1dc4a
Vladimir Shevelev <BOB41K@narod.ru>
2020-08-20 16:55:08 +0700

    [EasyStandard] Add ecs sniff for test method names validation (#302)
    Co-authored-by: v.shevelev <v.varitweb@gmail.com>

---
+++ packages/EasyStandard/tests/Sniffs/Methods/Fixtures/Correct/IgnoredMethodName.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyStandard\Tests\Sniffs\Methods\Fixtures\Correct;
+
+final class IgnoredMethodName
+{
+    public function testIgnoredMethodNameSuccessful(): void
+    {
+        // No body needed here
+    }
+}
