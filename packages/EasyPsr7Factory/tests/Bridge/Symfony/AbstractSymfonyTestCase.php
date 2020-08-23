7d5249464343981e04176713bdc4b94762840d7d
natepage <nathan.page86@gmail.com>
2020-08-23 17:59:22 +1000

    [EasyPsr7Factory] Fix tests

--- packages/EasyPsr7Factory/tests/Bridge/Symfony/AbstractSymfonyTestCase.php
+++ packages/EasyPsr7Factory/tests/Bridge/Symfony/AbstractSymfonyTestCase.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace EonX\EasyPsr7Factory\Tests\Bridge\Symfony;
 
+use EonX\EasyPsr7Factory\Tests\AbstractTestCase;
 use EonX\EasyPsr7Factory\Tests\Bridge\Symfony\Stubs\KernelStub;
-use EonX\EasySecurity\Tests\AbstractTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\KernelInterface;
 
@@ -19,13 +19,13 @@ abstract class AbstractSymfonyTestCase extends AbstractTestCase
     /**
      * @param null|string[] $configs
      */
-    protected function getKernel(?array $configs = null, ?Request $request = null): KernelInterface
+    protected function getKernel(?array $configs = null): KernelInterface
     {
         if ($this->kernel !== null) {
             return $this->kernel;
         }
 
-        $kernel = new KernelStub('test', true, $configs, $request);
+        $kernel = new KernelStub('test', true, $configs);
         $kernel->boot();
 
         return $this->kernel = $kernel;
