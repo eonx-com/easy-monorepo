eee5d671b46c224445b238975bab6bc465dc295f
natepage <nathan.page86@gmail.com>
2020-08-25 16:00:19 +1000

    [EasyBugsnag] Add Symfony shutdown strategy listener

--- packages/EasyBugsnag/src/Bridge/Symfony/Resources/config/services.php
+++ packages/EasyBugsnag/src/Bridge/Symfony/Resources/config/services.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 use Bugsnag\Client;
 use EonX\EasyBugsnag\Bridge\BridgeConstantsInterface;
+use EonX\EasyBugsnag\Bridge\Symfony\Shutdown\ShutdownStrategyListener;
 use EonX\EasyBugsnag\ClientFactory;
 use EonX\EasyBugsnag\Interfaces\ClientFactoryInterface;
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_iterator;
 
@@ -17,12 +19,20 @@ return static function (ContainerConfigurator $container): void {
         ->autoconfigure()
         ->autowire();
 
+    // Client Factory + Client
     $services
         ->set(ClientFactoryInterface::class, ClientFactory::class)
-        ->call('setConfigurators', [tagged_iterator(BridgeConstantsInterface::TAG_CLIENT_CONFIGURATOR)]);
+        ->call('setConfigurators', [tagged_iterator(BridgeConstantsInterface::TAG_CLIENT_CONFIGURATOR)])
+        ->call('setShutdownStrategy', [ref(ShutdownStrategyListener::class)]);
 
     $services
         ->set(Client::class)
         ->factory([ref(ClientFactoryInterface::class), 'create'])
         ->args(['%' . BridgeConstantsInterface::PARAM_API_KEY . '%']);
+
+    // Shutdown Strategy
+    $services
+        ->set(ShutdownStrategyListener::class)
+        ->tag('kernel.event_listener', ['event' => TerminateEvent::class])
+        ->tag('kernel.event_listener', ['event' => ConsoleTerminateEvent::class]);
 };
