55a5771afc3502342564bfa10d1cfcf2ddac0209
Eugene Ruban <ev.ruban@gmail.com>
2020-08-31 16:45:14 +0400

    [EasyCore] Add JsonbType and Contains function support for PostgreSQL (#333)

---
+++ packages/EasyCore/src/Doctrine/DBAL/Types/JsonbType.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyCore\Doctrine\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\JsonType;
+
+final class JsonbType extends JsonType
+{
+    /**
+     * @var string
+     */
+    public const TYPE_NAME = 'jsonb';
+
+    public function getName(): string
+    {
+        return static::TYPE_NAME;
+    }
+
+    /**
+     * @param mixed[] $fieldDeclaration
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+    {
+        return $platform->getDoctrineTypeMapping(static::TYPE_NAME);
+    }
+}
