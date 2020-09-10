e0627d3c4efc516aaef594a87fdcf3666d751b81
natepage <nathan.page86@gmail.com>
2020-09-10 22:46:48 +1000

    [EasySchedule] Use eonx-com/easy-lock instead of easy-core

--- packages/EasySchedule/src/Interfaces/ScheduleRunnerInterface.php
+++ packages/EasySchedule/src/Interfaces/ScheduleRunnerInterface.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 namespace EonX\EasySchedule\Interfaces;
 
-use EonX\EasyCore\Lock\LockServiceInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 interface ScheduleRunnerInterface
 {
     public function run(ScheduleInterface $schedule, OutputInterface $output): void;
-
-    public function setLockService(LockServiceInterface $lockService): self;
 }
