9e78ef5c42c94f48b4431c7314b585d310923a69
AlbertLabarentoJr <albert.labarentojr@flexisourceit.com.au>
2020-09-21 18:52:56 +0800

    master
    - Revert unrelated changes

--- packages/EasyDecision/packages/expressions/tests/ExpressionFunctionFactoryTest.php
+++ packages/EasyDecision/packages/expressions/tests/ExpressionFunctionFactoryTest.php
@@ -39,8 +39,8 @@ final class ExpressionFunctionFactoryTest extends AbstractTestCase
     {
         $input = [
             'cap', function ($arguments, $value, $max) {
-                return \min($value, $max);
-            }];
+                    return \min($value, $max);
+                }];
 
         $function = $this->getFactory()->create($input);
 
