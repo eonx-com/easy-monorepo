c93fce746fd0e4d03a13be945b4aaccf427cc613
Nathan Page <nathan.page@eonx.com>
2020-09-24 16:37:34 +1000

    [EasyErrorHandler] Fix translations in Symfony bridge (#368)

--- packages/EasyErrorHandler/src/Bridge/Symfony/DependencyInjection/Configuration.php
+++ packages/EasyErrorHandler/src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -22,7 +22,7 @@ final class Configuration implements ConfigurationInterface
                 ->booleanNode('verbose')->defaultFalse()->end()
                 ->booleanNode('use_default_builders')->defaultTrue()->end()
                 ->booleanNode('use_default_reporters')->defaultTrue()->end()
-                ->scalarNode('translation_domain')->defaultNull()->end()
+                ->scalarNode('translation_domain')->defaultValue('EasyErrorHandlerBundle')->end()
                 ->arrayNode('response')
                     ->addDefaultsIfNotSet()
                     ->children()
