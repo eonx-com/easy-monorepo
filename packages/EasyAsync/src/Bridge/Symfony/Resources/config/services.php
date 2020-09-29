10ee2f11b44741ee3e440c22fab2af415bb10e82
albertlabarento <12888117+albertlabarentojr@users.noreply.github.com>
2020-09-29 16:43:13 +0800

    [EasyAsync] Fixed service binding to use \Configurator\ref (#372)
    Co-authored-by: Nathan Page <nathan.page@eonx.com>

--- packages/EasyAsync/src/Bridge/Symfony/Resources/config/services.php
+++ packages/EasyAsync/src/Bridge/Symfony/Resources/config/services.php
@@ -15,6 +15,8 @@ use EonX\EasyAsync\Updaters\JobLogUpdater;
 use EonX\EasyAsync\Updaters\WithEventsJobLogUpdater;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
+use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
@@ -31,8 +33,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set('default_job_log_updater', JobLogUpdater::class);
 
     $services->set(JobLogUpdaterInterface::class, WithEventsJobLogUpdater::class)
-        ->bind('$decorated', '@default_job_log_updater');
+        ->bind('$decorated', ref('default_job_log_updater'));
 
     $services->set(JobPersisterInterface::class, WithEventsJobPersister::class)
-        ->bind('$decorated', '@default_job_persister');
+        ->bind('$decorated', ref('@default_job_persister'));
 };
