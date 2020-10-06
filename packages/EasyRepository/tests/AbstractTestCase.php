508d154275f63f351dcba6210e08698422514d4d
natepage <nathan.page86@gmail.com>
2020-10-06 11:48:33 +1100

    fix cs

--- packages/EasyRepository/tests/AbstractTestCase.php
+++ packages/EasyRepository/tests/AbstractTestCase.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace EonX\EasyRepository\Tests;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Persistence\ObjectRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Mockery;
 use Mockery\LegacyMockInterface;
 use PHPUnit\Framework\TestCase;
