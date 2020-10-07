2977b209b7fd2ea47ab154f6895c5d2b650baba2
TomasVotruba <tomas.vot@gmail.com>
2020-10-07 15:28:28 +0200

    add rector.php

---
+++ rector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/packages',
+    ]);
+
+    // Define what rule sets will be applied
+    $parameters->set(Option::SETS, [
+        SetList::NAMING,
+        SetList::CODE_QUALITY,
+    ]);
+
+    // get services (needed for register a single rule)
+    // $services = $containerConfigurator->services();
+
+    // register a single rule
+    // $services->set(TypedPropertyRector::class);
+};
