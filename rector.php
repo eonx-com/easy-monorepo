99da390458a95419e0c960fe9f8785e26cddb045
TomasVotruba <tomas.vot@gmail.com>
2020-10-07 15:44:31 +0200

    prepare rector config

--- rector.php
+++ rector.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\SetList;
+use Rector\SOLID\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-
     // get parameters
     $parameters = $containerConfigurator->parameters();
 
@@ -15,15 +15,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         __DIR__ . '/packages',
     ]);
 
-    // Define what rule sets will be applied
-    $parameters->set(Option::SETS, [
-        SetList::NAMING,
-        SetList::CODE_QUALITY,
+    $parameters->set(Option::AUTOLOAD_PATHS, [
+        __DIR__ . '/vendor/squizlabs/php_codesniffer/autoload.php',
     ]);
 
-    // get services (needed for register a single rule)
-    // $services = $containerConfigurator->services();
-
-    // register a single rule
-    // $services->set(TypedPropertyRector::class);
+     $services = $containerConfigurator->services();
+     $services->set(FinalizeClassesWithoutChildrenRector::class);
 };
