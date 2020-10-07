b6a75571689029919f78db896dc63f875b37bb6c
TomasVotruba <tomas.vot@gmail.com>
2020-10-07 18:37:17 +0200

    fix autoload path

--- packages/EasyIdentity/tests/bootstrap.php
+++ packages/EasyIdentity/tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // enables mocking of final classes
 // @see https://tomasvotruba.com/blog/2019/03/28/how-to-mock-final-classes-in-phpunit/
