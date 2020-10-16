c62ce237c16203d09914f76fab196b5ec1f9282c
TomasVotruba <tomas.vot@gmail.com>
2020-10-16 23:03:22 +0200

    remove unused property from a stub

--- packages/EasyWebhook/tests/Stubs/EventDispatcherStub.php
+++ packages/EasyWebhook/tests/Stubs/EventDispatcherStub.php
@@ -9,19 +9,12 @@ use EonX\EasyEventDispatcher\Interfaces\EventDispatcherInterface;
 final class EventDispatcherStub implements EventDispatcherInterface
 {
     /**
-     * @var mixed[]
-     */
-    private $dispatched = [];
-
-    /**
      * @param object $event
      *
      * @return object
      */
     public function dispatch($event)
     {
-        $this->dispatched[] = $event;
-
         return $event;
     }
 }
