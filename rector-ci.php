1db800c9e289adaa3ba48d96c4794f7fdd9fe938
TomasVotruba <tomas.vot@gmail.com>
2020-10-19 17:01:53 +0200

    fix cs

--- rector-ci.php
+++ rector-ci.php
@@ -3,14 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
-use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
 use Rector\Set\ValueObject\SetList;
-use Rector\SOLID\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
     $parameters = $containerConfigurator->parameters();
+
     $parameters->set(Option::PATHS, [
         __DIR__ . '/packages',
         __DIR__ . '/tests',
