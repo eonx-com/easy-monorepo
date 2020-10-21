4168e0994f19cf968bbb6afdf74fa3c5b2d7839d
natepage <nathan.page86@gmail.com>
2020-10-21 23:42:32 +1100

    [EasyCore] Fix ecs

--- packages/EasyCore/src/Bridge/Symfony/Resources/config/services.php
+++ packages/EasyCore/src/Bridge/Symfony/Resources/config/services.php
@@ -8,8 +8,6 @@ use EonX\EasyCore\Lock\LockService;
 use EonX\EasyCore\Lock\LockServiceInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-use function Symfony\Component\DependencyInjection\Loader\Configurator\expr;
-
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
