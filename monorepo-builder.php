75a82314a27fc14ab99c24e7a26070beb3d28038
Tomas Votruba <tomas.vot@gmail.com>
2020-10-21 04:13:47 +0200

    [monorepo] make use of pascal mask split (#400)
    Co-authored-by: Nathan Page <nathan.page@eonx.com>

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -10,6 +10,7 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesR
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Split\ValueObject\ConvertFormat;
 use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -22,34 +23,9 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ],
     ]);
 
+    $parameters->set(Option::DIRECTORIES_TO_REPOSITORIES_CONVERT_FORMAT, ConvertFormat::PASCAL_CASE_TO_KEBAB_CASE);
     $parameters->set(Option::DIRECTORIES_TO_REPOSITORIES, [
-        // tip for lazier setup with mask: https://github.com/symplify/monorepo-builder#5-split-directories-to-git-repositories
-        // 'packages/*' => 'git@github.com:eonx-com/*.git',
-        'packages/EasyApiToken' => 'git@github.com:eonx-com/easy-api-token.git',
-        'packages/EasyAsync' => 'git@github.com:eonx-com/easy-async.git',
-        'packages/EasyAwsCredentialsFinder' => 'git@github.com:eonx-com/easy-aws-credentials-finder.git',
-        'packages/EasyBugsnag' => 'git@github.com:eonx-com/easy-bugsnag.git',
-        'packages/EasyCore' => 'git@github.com:eonx-com/easy-core.git',
-        'packages/EasyDecision' => 'git@github.com:eonx-com/easy-decision.git',
-        'packages/EasyEntityChange' => 'git@github.com:eonx-com/easy-entity-change.git',
-        'packages/EasyErrorHandler' => 'git@github.com:eonx-com/easy-error-handler.git',
-        'packages/EasyEventDispatcher' => 'git@github.com:eonx-com/easy-event-dispatcher.git',
-        'packages/EasyIdentity' => 'git@github.com:eonx-com/easy-identity.git',
-        'packages/EasyLock' => 'git@github.com:eonx-com/easy-lock.git',
-        'packages/EasyLogging' => 'git@github.com:eonx-com/easy-logging.git',
-        'packages/EasyNotification' => 'git@github.com:eonx-com/easy-notification.git',
-        'packages/EasyPipeline' => 'git@github.com:eonx-com/easy-pipeline.git',
-        'packages/EasyRepository' => 'git@github.com:eonx-com/easy-repository.git',
-        'packages/EasyRequestId' => 'git@github.com:eonx-com/easy-request-id.git',
-        'packages/EasyPagination' => 'git@github.com:eonx-com/easy-pagination.git',
-        'packages/EasyPsr7Factory' => 'git@github.com:eonx-com/easy-psr7-factory.git',
-        'packages/EasyRandom' => 'git@github.com:eonx-com/easy-random.git',
-        'packages/EasySchedule' => 'git@github.com:eonx-com/easy-schedule.git',
-        'packages/EasySecurity' => 'git@github.com:eonx-com/easy-security.git',
-        'packages/EasySsm' => 'git@github.com:eonx-com/easy-ssm.git',
-        'packages/EasyStandard' => 'git@github.com:eonx-com/easy-standard.git',
-        'packages/EasyTest' => 'git@github.com:eonx-com/easy-test.git',
-        'packages/EasyWebhook' => 'git@github.com:eonx-com/easy-webhook.git',
+        'packages/*' => 'git@github.com:eonx-com/*.git',
     ]);
 
     $services = $containerConfigurator->services();
