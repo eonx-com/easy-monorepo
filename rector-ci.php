d99c1a473dc07337dcd92857c29e9b8590be5e9f
Tomas Votruba <tomas.vot@gmail.com>
2020-10-21 03:26:47 +0200

    [Rector] enable full dead-code set (#399)

--- rector-ci.php
+++ rector-ci.php
@@ -3,28 +3,24 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
-use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
 use Rector\Set\ValueObject\SetList;
-use Rector\SOLID\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
     $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PROJECT_TYPE, Option::PROJECT_TYPE_OPEN_SOURCE);
+
     $parameters->set(Option::PATHS, [
         __DIR__ . '/packages',
         __DIR__ . '/tests',
     ]);
 
-//    $parameters->set(Option::SETS, [
-//        SetList::DEAD_CODE,
-//    ]);
+    $parameters->set(Option::SETS, [
+        SetList::DEAD_CODE,
+    ]);
 
     $parameters->set(Option::AUTOLOAD_PATHS, [
         __DIR__ . '/tests/rector_bootstrap.php',
     ]);
-
-    $services = $containerConfigurator->services();
-    $services->set(FinalizeClassesWithoutChildrenRector::class);
-    $services->set(RemoveUnusedPrivatePropertyRector::class);
 };
