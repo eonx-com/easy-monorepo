c570afd49c7b0e0b5badf7acf54a071a46f29629
TomasVotruba <tomas.vot@gmail.com>
2020-10-22 12:53:25 +0200

    add missing phpunit + bypass final

---
+++ packages/EasyBankFiles/tests/phpunit_bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// enables mocking of final classes
+// @see https://tomasvotruba.com/blog/2019/03/28/how-to-mock-final-classes-in-phpunit/
+DG\BypassFinals::enable();
