77854b6cb0ac0d1964d4e81188051ab25519b4bf
natepage <nathan.page86@gmail.com>
2020-12-14 08:09:59 +1100

    [EasyCore] Implement replacement for api-formats

---
+++ packages/EasyCore/src/Bridge/Laravel/ApiFormats/Interfaces/FormattedApiResponseInterface.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyCore\Bridge\Laravel\ApiFormats\Interfaces;
+
+interface FormattedApiResponseInterface
+{
+    /**
+     * @return mixed
+     */
+    public function getContent();
+
+    /**
+     * @return string[]
+     */
+    public function getHeaders(): array;
+
+    public function getStatusCode(): int;
+}
---
+++ packages/EasyCore/src/Bridge/Laravel/ApiFormats/Responses/FormattedApiResponse.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyCore\Bridge\Laravel\ApiFormats\Responses;
+
+use EonX\EasyCore\Bridge\Laravel\ApiFormats\Interfaces\FormattedApiResponseInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+class FormattedApiResponse extends Response implements FormattedApiResponseInterface
+{
+    /**
+     * @param mixed $content
+     * @param null|mixed[] $headers
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct($content, ?int $statusCode = null, ?array $headers = null)
+    {
+        parent::__construct();
+
+        $this->content = $content;
+        $this->statusCode = $statusCode ?? 200;
+        $this->headers = new ResponseHeaderBag($headers ?? []);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getHeaders(): array
+    {
+        return $this->headers->all();
+    }
+}
