24d2387153fc65f411401126461a8105bbdfd7e6
Dmitry Kapenkin <dkapenkin@rambler.ru>
2020-12-14 22:02:30 +0300

    [EasyStandard] Implement OrderArrayKeysAlphabeticallySniff
    - implement correct tests

--- packages/EasyStandard/tests/Sniffs/Arrays/OrderArrayKeysAlphabeticallySniff/OrderArrayKeysAlphabeticallySniffTest.php
+++ packages/EasyStandard/tests/Sniffs/Arrays/OrderArrayKeysAlphabeticallySniff/OrderArrayKeysAlphabeticallySniffTest.php
@@ -21,10 +21,22 @@ final class OrderArrayKeysAlphabeticallySniffTest extends AbstractCheckerTestCas
         $this->doTestFileInfoWithErrorCountOf($wrongFileInfo, 2);
     }
 
+    public function testProcessMultiLineArraySucceeds(): void
+    {
+        $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/MultiLineArray.php.inc');
+        $this->doTestCorrectFileInfo($fileInfo);
+    }
+
     public function testProcessMultiLineMixedArrayFails(): void
     {
         $wrongFileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Wrong/MultiLineMixedArray.php.inc');
         $this->doTestFileInfoWithErrorCountOf($wrongFileInfo, 4);
+    }
+
+    public function testProcessMultiLineMixedArraySucceeds(): void
+    {
+        $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/MultiLineMixedArray.php.inc');
+        $this->doTestCorrectFileInfo($fileInfo);
     }
 
     public function testProcessMultiLineMultiDimensionalArrayFails(): void
@@ -33,10 +45,22 @@ final class OrderArrayKeysAlphabeticallySniffTest extends AbstractCheckerTestCas
         $this->doTestFileInfoWithErrorCountOf($wrongFileInfo, 4);
     }
 
+    public function testProcessMultiLineMultiDimensionalArraySucceeds(): void
+    {
+        $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/MultiLineMultiDimensionalArray.php.inc');
+        $this->doTestCorrectFileInfo($fileInfo);
+    }
+
     public function testProcessSingleLineArrayFails(): void
     {
         $wrongFileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Wrong/SingleLineArray.php.inc');
         $this->doTestFileInfoWithErrorCountOf($wrongFileInfo, 2);
+    }
+
+    public function testProcessSingleLineArraySucceeds(): void
+    {
+        $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/SingleLineArray.php.inc');
+        $this->doTestCorrectFileInfo($fileInfo);
     }
 
     public function testProcessSingleLineMixedArrayFails(): void
@@ -45,10 +69,34 @@ final class OrderArrayKeysAlphabeticallySniffTest extends AbstractCheckerTestCas
         $this->doTestFileInfoWithErrorCountOf($wrongFileInfo, 4);
     }
 
+    public function testProcessSingleLineMixedArraySucceeds(): void
+    {
+        $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/SingleLineMixedArray.php.inc');
+        $this->doTestCorrectFileInfo($fileInfo);
+    }
+
     public function testProcessSingleLineMultiDimensionalArrayFails(): void
     {
         $wrongFileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Wrong/SingleLineMultiDimensionalArray.php.inc');
         $this->doTestFileInfoWithErrorCountOf($wrongFileInfo, 4);
+    }
+
+    public function testProcessSingleLineMultiDimensionalArraySucceeds(): void
+    {
+        $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/SingleLineMultiDimensionalArray.php.inc');
+        $this->doTestCorrectFileInfo($fileInfo);
+    }
+
+    public function testSkipByClassNameSucceeds(): void
+    {
+        $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/SkipByClassName.php.inc');
+        $this->doTestCorrectFileInfo($fileInfo);
+    }
+
+    public function testSkipByFunctionNameSucceeds(): void
+    {
+        $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/SkipByFunctionName.php.inc');
+        $this->doTestCorrectFileInfo($fileInfo);
     }
 
     protected function getCheckerClass(): string
@@ -64,7 +112,7 @@ final class OrderArrayKeysAlphabeticallySniffTest extends AbstractCheckerTestCas
         return [
             'skipPatterns' => [
                 T_FUNCTION => ['/provide[A-Z]/'],
-                T_CLASS => ['/someClass[A-Z]/'],
+                T_CLASS => ['/SomeClass[A-Z]/'],
             ],
         ];
     }
