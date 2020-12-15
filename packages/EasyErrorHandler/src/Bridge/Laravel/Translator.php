b831a33a1f3f9cd0dec4164a9e5917108f445210
Jay Are Galinada <jayare.galinada@flexisourceit.com.au>
2020-12-15 17:50:14 +0800

    Fix incorrect translation in Laravel/Lumen setup
    - Renamed messages to exceptions translation file
    - Translate with namespace
    - Added TRANSLATION_NAMESPACE constant

--- packages/EasyErrorHandler/src/Bridge/Laravel/Translator.php
+++ packages/EasyErrorHandler/src/Bridge/Laravel/Translator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace EonX\EasyErrorHandler\Bridge\Laravel;
 
+use EonX\EasyErrorHandler\Bridge\BridgeConstantsInterface;
 use EonX\EasyErrorHandler\Interfaces\TranslatorInterface;
 use Illuminate\Contracts\Translation\Translator as IlluminateTranslatorInterface;
 
@@ -24,8 +25,24 @@ final class Translator implements TranslatorInterface
      */
     public function trans(string $message, array $parameters): string
     {
-        // TODO: rework after upgrading all the illuminate and laravel packages to ^6.0
-        $method = \method_exists($this->decorated, 'trans') ? 'trans' : 'get';
+        $translation = $this->getOrLang($message, $parameters);
+
+        if ($translation !== $message) {
+            return $translation;
+        }
+
+        return $this->getOrLang(
+            \sprintf('%s::%s', BridgeConstantsInterface::TRANSLATION_NAMESPACE, $message),
+            $parameters
+        );
+    }
+
+    /**
+     * @param mixed[] $parameters
+     */
+    private function getOrLang(string $message, array $parameters) : string
+    {
+        $method = \method_exists($this->decorated, 'lang') ? 'lang' : 'get';
 
         return $this->decorated->{$method}(\trim($message), $parameters);
     }
