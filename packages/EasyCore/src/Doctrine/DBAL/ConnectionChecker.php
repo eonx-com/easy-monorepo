19c5d53d43c32dc4dcd48390edb3e5a273c2ea00
Nathan Page <nathan.page@eonx.com>
2020-12-15 16:44:25 +1100

    [EasyCore] Check if db connection is ok before processing job (#446)

---
+++ packages/EasyCore/src/Doctrine/DBAL/ConnectionChecker.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyCore\Doctrine\DBAL;
+
+use Doctrine\DBAL\Connection;
+
+final class ConnectionChecker
+{
+    public static function checkConnection(Connection $connection): void
+    {
+        $connection->executeStatement($connection->getDatabasePlatform()->getDummySelectSQL());
+    }
+}
