11d57026df84fa045dd8e42c06ace5a16bd9a152
natepage <nathan.page86@gmail.com>
2020-12-15 21:32:00 +1100

    [EasyErrorHandler] Improve error details

--- packages/EasyErrorHandler/src/Bridge/Symfony/Resources/config/services.php
+++ packages/EasyErrorHandler/src/Bridge/Symfony/Resources/config/services.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 use EonX\EasyErrorHandler\Bridge\BridgeConstantsInterface;
 use EonX\EasyErrorHandler\Bridge\Symfony\Listener\ExceptionEventListener;
 use EonX\EasyErrorHandler\Bridge\Symfony\Translator;
+use EonX\EasyErrorHandler\ErrorDetailsResolver;
 use EonX\EasyErrorHandler\ErrorHandler;
+use EonX\EasyErrorHandler\Interfaces\ErrorDetailsResolverInterface;
 use EonX\EasyErrorHandler\Interfaces\ErrorHandlerInterface;
 use EonX\EasyErrorHandler\Interfaces\ErrorResponseFactoryInterface;
 use EonX\EasyErrorHandler\Interfaces\TranslatorInterface;
 use EonX\EasyErrorHandler\Response\ErrorResponseFactory;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-
 use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_iterator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -20,6 +21,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->autowire()
         ->autoconfigure();
 
+    // ErrorDetailsResolver
+    $services->set(ErrorDetailsResolverInterface::class, ErrorDetailsResolver::class);
+
+    // ErrorHandler
     $services
         ->set(ErrorHandlerInterface::class, ErrorHandler::class)
         ->arg('$builderProviders', tagged_iterator(BridgeConstantsInterface::TAG_ERROR_RESPONSE_BUILDER_PROVIDER))
