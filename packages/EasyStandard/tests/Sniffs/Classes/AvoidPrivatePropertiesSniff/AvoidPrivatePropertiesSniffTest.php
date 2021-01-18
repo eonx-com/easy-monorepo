386512c767189b7c6fb0136429f32a1629b2229d
Dmitry Kapenkin <dkapenkin@rambler.ru>
2021-01-18 19:39:12 +0300

    [EasyStandard] Implement AvoidPrivatePropertiesSniff
    - init changes

---
+++ packages/EasyStandard/tests/Sniffs/Classes/AvoidPrivatePropertiesSniff/AvoidPrivatePropertiesSniffTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyStandard\Tests\Sniffs\Classes\AvoidPrivatePropertiesSniff;
+
+use EonX\EasyStandard\Sniffs\Classes\AvoidPublicPropertiesSniff;
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class AvoidPrivatePropertiesSniffTest extends AbstractCheckerTestCase
+{
+    /**
+     * @return iterable<array<int, SmartFileInfo|int>>
+     *
+     * @throws \Symplify\SmartFileSystem\Exception\FileNotFoundException
+     */
+    public function providerTestSniff(): iterable
+    {
+        yield [new SmartFileInfo(__DIR__ . '/Fixture/AvoidPrivatePropertiesSniffTest.php.inc'), 1];
+    }
+
+    /**
+     * @dataProvider providerTestSniff()
+     */
+    public function testSniff(SmartFileInfo $smartFileInfo, int $expectedErrorCount): void
+    {
+        $this->doTestFileInfoWithErrorCountOf($smartFileInfo, $expectedErrorCount);
+    }
+
+    protected function getCheckerClass(): string
+    {
+        return AvoidPublicPropertiesSniff::class;
+    }
+}
