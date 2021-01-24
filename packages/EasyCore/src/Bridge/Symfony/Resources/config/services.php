080a3a08546d498bd8a3e263aef5f689b88cb8a1
Nathan Page <nathan.page@eonx.com>
2021-01-25 08:46:37 +1100

    [EasyStandard] Stop messenger worker if em closed (#472)
    Co-authored-by: Dmitry Kapenkin <dkapenkin@rambler.ru>
    Co-authored-by: Roman <roman.ovchinnikov@eonx.com>

--- packages/EasyCore/src/Bridge/Symfony/Resources/config/services.php
+++ packages/EasyCore/src/Bridge/Symfony/Resources/config/services.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use EonX\EasyCore\Bridge\Symfony\Env\ForBuildEnvVarProcessor;
+use EonX\EasyCore\Bridge\Symfony\Messenger\StopWorkerOnEmClosedEventListener;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -11,6 +12,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->defaults()
         ->autowire()
         ->autoconfigure();
+
+    // Messenger
+    $services
+        ->set(StopWorkerOnEmClosedEventListener::class)
+        ->tag('kernel.event_listener');
 
     $services->set(ForBuildEnvVarProcessor::class);
 };
