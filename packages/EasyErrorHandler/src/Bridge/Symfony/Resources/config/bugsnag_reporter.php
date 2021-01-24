7b61853b22844facd1064f7bda29dcfc44ac5aa4
Nathan Page <nathan.page@eonx.com>
2021-01-25 08:50:15 +1100

    [EasyErrorHandler] Implement severity aware exception + bugsnag bridge (#468)

--- packages/EasyErrorHandler/src/Bridge/Symfony/Resources/config/bugsnag_reporter.php
+++ packages/EasyErrorHandler/src/Bridge/Symfony/Resources/config/bugsnag_reporter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use EonX\EasyErrorHandler\Bridge\BridgeConstantsInterface;
 use EonX\EasyErrorHandler\Bridge\Bugsnag\BugsnagReporterProvider;
 use EonX\EasyErrorHandler\Bridge\Bugsnag\ErrorDetailsClientConfigurator;
+use EonX\EasyErrorHandler\Bridge\Bugsnag\SeverityClientConfigurator;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -18,5 +19,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->arg('$threshold', '%' . BridgeConstantsInterface::PARAM_BUGSNAG_THRESHOLD . '%')
         ->arg('$ignoredExceptions', '%' . BridgeConstantsInterface::PARAM_BUGSNAG_IGNORED_EXCEPTIONS . '%');
 
-    $services->set(ErrorDetailsClientConfigurator::class);
+    $services
+        ->set(ErrorDetailsClientConfigurator::class)
+        ->set(SeverityClientConfigurator::class);
 };
