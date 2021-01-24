f12e063bd91352af225d4567a7b56177dc4f1976
Ivan Torgov <likecod@gmail.com>
2021-01-25 00:39:54 +0300

    [EasyErrorHandler] Introduced ApiPlatformBuilderProvider and ApiPlatformValidationExceptionBuilder (#461)

--- packages/EasyErrorHandler/src/Bridge/Symfony/DependencyInjection/Configuration.php
+++ packages/EasyErrorHandler/src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -23,6 +23,7 @@ final class Configuration implements ConfigurationInterface
                     ->scalarPrototype()->end()
                 ->end()
                 ->booleanNode('verbose')->defaultFalse()->end()
+                ->booleanNode('override_api_platform_listener')->defaultTrue()->end()
                 ->booleanNode('use_default_builders')->defaultTrue()->end()
                 ->booleanNode('use_default_reporters')->defaultTrue()->end()
                 ->scalarNode('translation_domain')->defaultValue('EasyErrorHandlerBundle')->end()
