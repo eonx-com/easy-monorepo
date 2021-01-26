b54cc8921dee3459ae367ffc82f15f9ea2e0badc
natepage <nathan.page86@gmail.com>
2021-01-27 09:24:45 +1100

    [EasyErrorHandler] Automatically set severity based on log level

--- packages/EasyErrorHandler/src/Bridge/Bugsnag/SeverityClientConfigurator.php
+++ packages/EasyErrorHandler/src/Bridge/Bugsnag/SeverityClientConfigurator.php
@@ -9,9 +9,25 @@ use Bugsnag\Middleware\CallbackBridge;
 use Bugsnag\Report;
 use EonX\EasyBugsnag\Configurators\AbstractClientConfigurator;
 use EonX\EasyErrorHandler\Interfaces\Exceptions\SeverityAwareExceptionInterface;
+use EonX\EasyErrorHandler\Traits\DealsWithErrorLogLevelTrait;
+use Monolog\Logger;
 
 final class SeverityClientConfigurator extends AbstractClientConfigurator
 {
+    use DealsWithErrorLogLevelTrait;
+
+    /**
+     * @var int
+     */
+    private $threshold;
+
+    public function __construct(?int $threshold = null, ?int $priority = null)
+    {
+        $this->threshold = $threshold ?? Logger::ERROR;
+
+        parent::__construct($priority);
+    }
+
     public function configure(Client $bugsnag): void
     {
         $bugsnag
@@ -19,9 +35,24 @@ final class SeverityClientConfigurator extends AbstractClientConfigurator
             ->pipe(new CallbackBridge(function (Report $report): void {
                 $throwable = $report->getOriginalError();
 
-                $report->setSeverity(
-                    $throwable instanceof SeverityAwareExceptionInterface ? $throwable->getSeverity() : null
-                );
+                if ($throwable instanceof \Throwable) {
+                    $report->setSeverity($this->getSeverity($throwable));
+                }
             }));
+    }
+
+    private function getSeverity(\Throwable $throwable): ?string
+    {
+        // Allow to explicitly define the severity
+        $severity = $throwable instanceof SeverityAwareExceptionInterface ? $throwable->getSeverity() : null;
+
+        if ($severity !== null) {
+            return $severity;
+        }
+
+        $logLevel = $this->getLogLevel($throwable);
+
+        // If log level greater than threshold, severity to error, otherwise default to null
+        return $logLevel > $this->threshold ? SeverityAwareExceptionInterface::SEVERITY_ERROR : null;
     }
 }
