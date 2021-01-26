8817476193864542b64cb3c783c57f8ce3c6c91a
natepage <nathan.page86@gmail.com>
2021-01-27 08:01:37 +1100

    [First Try] Use dynamic packages in github action for split tests

---
+++ monorepo/Console/Commands/ExportPackagesAsJsonCommand.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyMonorepo\Console\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Symplify\SmartFileSystem\Finder\FinderSanitizer;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class ExportPackagesAsJsonCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected static $defaultName = 'export-packages';
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $composerFiles = $this->getComposerJsonFiles();
+        $packages = [];
+
+        foreach ($composerFiles as $composerFile) {
+            $repoName = $this->getRepoShortname($composerFile);
+
+            $packages[$repoName] = [
+                'dir' => $this->getDir($composerFile),
+                'repo' => $repoName,
+            ];
+        }
+
+        \ksort($packages);
+
+        // Remove keys
+        $noKeys = [];
+        foreach ($packages as $package) {
+            $noKeys[] = $package;
+        }
+
+        $output->write((string)\json_encode($noKeys));
+
+        return 0;
+    }
+
+    /**
+     * @return \Symplify\SmartFileSystem\SmartFileInfo[]
+     */
+    private function getComposerJsonFiles(): array
+    {
+        $finder = (new Finder())
+            ->in([__DIR__ . '/../../../packages'])
+            ->name('composer.json');
+
+        return (new FinderSanitizer())->sanitize($finder);
+    }
+
+    private function getDir(SmartFileInfo $composerJson): string
+    {
+        return \last(\explode('/', $composerJson->getRelativeDirectoryPath()));
+    }
+
+    private function getRepoShortname(SmartFileInfo $composerJson): string
+    {
+        $json = \json_decode($composerJson->getContents(), true);
+
+        return \str_replace('eonx-com/', '', $json['name']);
+    }
+}
