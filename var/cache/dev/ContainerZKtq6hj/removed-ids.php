67b66875e7c6bdc1e2054c5a84241201db4fea27
natepage <nathan.page86@gmail.com>
2021-01-27 08:42:52 +1100

    prepare release

---
+++ var/cache/dev/ContainerZKtq6hj/removed-ids.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '.abstract.instanceof.EonX\\EasyMonorepo\\Console\\Commands\\ExportPackagesAsJsonCommand' => true,
+    '.instanceof.Symfony\\Component\\Console\\Command\\Command.0.EonX\\EasyMonorepo\\Console\\Commands\\ExportPackagesAsJsonCommand' => true,
+    'Psr\\Container\\ContainerInterface' => true,
+    'Symfony\\Component\\DependencyInjection\\ContainerInterface' => true,
+];
