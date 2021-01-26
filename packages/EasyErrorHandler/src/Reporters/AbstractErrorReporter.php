627e9dc639a549ecc2d70cfb980313476ccedcd9
Nathan Page <nathan.page@eonx.com>
2021-01-27 09:43:11 +1100

    [EasyErrorHandler] Automatically set severity based on log level (#475)

--- packages/EasyErrorHandler/src/Reporters/AbstractErrorReporter.php
+++ packages/EasyErrorHandler/src/Reporters/AbstractErrorReporter.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace EonX\EasyErrorHandler\Reporters;
 
 use EonX\EasyErrorHandler\Interfaces\ErrorReporterInterface;
-use EonX\EasyErrorHandler\Interfaces\Exceptions\LogLevelAwareExceptionInterface;
-use Monolog\Logger;
-use Throwable;
+use EonX\EasyErrorHandler\Traits\DealsWithErrorLogLevelTrait;
 
 abstract class AbstractErrorReporter implements ErrorReporterInterface
 {
+    use DealsWithErrorLogLevelTrait;
+
     /**
      * @var int
      */
@@ -24,15 +24,5 @@ abstract class AbstractErrorReporter implements ErrorReporterInterface
     public function getPriority(): int
     {
         return $this->priority;
-    }
-
-    protected function getLogLevel(Throwable $throwable): int
-    {
-        if ($throwable instanceof LogLevelAwareExceptionInterface) {
-            return $throwable->getLogLevel();
-        }
-
-        // Default to error as exceptions not aware of their log level is unexpected
-        return Logger::ERROR;
     }
 }
