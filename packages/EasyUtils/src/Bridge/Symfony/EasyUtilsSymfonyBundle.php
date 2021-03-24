aabae3633a86d4f17c38759d0f762c3f5a35e102
natepage <nathan.page@eonx.com>
2021-03-25 09:23:42 +1100

    [EasyUtils] Flex integration

---
+++ packages/EasyUtils/src/Bridge/Symfony/EasyUtilsSymfonyBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyUtils\Bridge\Symfony;
+
+use EonX\EasyUtils\Bridge\Symfony\DependencyInjection\EasyUtilsExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class EasyUtilsSymfonyBundle extends Bundle
+{
+    public function getContainerExtension(): ExtensionInterface
+    {
+        return new EasyUtilsExtension();
+    }
+}
