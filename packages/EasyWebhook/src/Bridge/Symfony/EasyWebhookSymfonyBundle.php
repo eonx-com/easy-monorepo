c5af04f0c400f0fe5ea35edbc5f6ab194f3cccd0
natepage <nathan.page@eonx.com>
2021-03-25 14:53:12 +1100

    [EasyWebhook] Override getContainerExtension for EasyWebhookSymfonyBundle

--- packages/EasyWebhook/src/Bridge/Symfony/EasyWebhookSymfonyBundle.php
+++ packages/EasyWebhook/src/Bridge/Symfony/EasyWebhookSymfonyBundle.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace EonX\EasyWebhook\Bridge\Symfony;
 
 use EonX\EasyWebhook\Bridge\Symfony\DependencyInjection\Compiler\AddMessengerMiddlewarePass;
+use EonX\EasyWebhook\Bridge\Symfony\DependencyInjection\EasyWebhookExtension;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class EasyWebhookSymfonyBundle extends Bundle
@@ -16,5 +18,10 @@ final class EasyWebhookSymfonyBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new AddMessengerMiddlewarePass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, -10);
+    }
+
+    public function getContainerExtension(): ExtensionInterface
+    {
+        return new EasyWebhookExtension();
     }
 }
