40f8291a7ad90aa22975457237a64f2a9be04652
natepage <nathan.page@eonx.com>
2021-04-14 04:09:16 +1000

    [EasyEventDispatcher] Refactor Laravel ServiceProvider Closures

--- packages/EasyEventDispatcher/src/Bridge/Laravel/EasyEventDispatcherServiceProvider.php
+++ packages/EasyEventDispatcher/src/Bridge/Laravel/EasyEventDispatcherServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace EonX\EasyEventDispatcher\Bridge\Laravel;
 
 use EonX\EasyEventDispatcher\Interfaces\EventDispatcherInterface;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Events\Dispatcher as IlluminateDispatcherContract;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,8 +13,11 @@ final class EasyEventDispatcherServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(EventDispatcherInterface::class, function (): EventDispatcherInterface {
-            return new EventDispatcher($this->app->make(IlluminateDispatcherContract::class));
-        });
+        $this->app->singleton(
+            EventDispatcherInterface::class,
+            static function (Container $app): EventDispatcherInterface {
+                return new EventDispatcher($app->make(IlluminateDispatcherContract::class));
+            }
+        );
     }
 }
