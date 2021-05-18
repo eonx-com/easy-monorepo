8ada92ad00599a5ebb6c2c6388eddb653ca3483a
Evgeny Ruban <ev.ruban@gmail.com>
2021-05-18 20:05:33 +0400

    feature/remove-easy-standard
    - Try to add easy-quality

--- packages/EasyEventDispatcher/tests/Bridge/Symfony/Stubs/SymfonyEventDispatcherStub.php
+++ packages/EasyEventDispatcher/tests/Bridge/Symfony/Stubs/SymfonyEventDispatcherStub.php
@@ -14,11 +14,15 @@ final class SymfonyEventDispatcherStub implements EventDispatcherInterface
     private $dispatched = [];
 
     /**
-     * @param object $event The event to pass to the event handlers/listeners
+     * Dispatches an event to all registered listeners.
+     *
+     * @param object      $event     The event to pass to the event handlers/listeners
+     * @param string|null $eventName The name of the event to dispatch. If not supplied,
+     *                               the class of $event should be used instead.
      *
      * @return object The passed $event MUST be returned
      */
-    public function dispatch($event)
+    public function dispatch(object $event, string $eventName = null): object
     {
         $this->dispatched[] = $event;
 
