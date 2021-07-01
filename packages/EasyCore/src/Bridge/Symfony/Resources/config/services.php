d45cab58f570cd49d4d7559182d6cbb01d9ca7e0
Vadzim Lukiashka <vlukiashka.eonx@gmail.com>
2021-07-01 10:32:23 +0300

    [EasyCore] DeferredEntityEventDispatcher service

--- packages/EasyCore/src/Bridge/Symfony/Resources/config/services.php
+++ packages/EasyCore/src/Bridge/Symfony/Resources/config/services.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use EonX\EasyCore\Bridge\Symfony\Env\ForBuildEnvVarProcessor;
 use EonX\EasyCore\Bridge\Symfony\Messenger\StopWorkerOnEmClosedEventListener;
+use EonX\EasyCore\Doctrine\Dispatchers\DeferredEntityEventDispatcher;
+use EonX\EasyCore\Doctrine\Dispatchers\DeferredEntityEventDispatcherInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -19,4 +21,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->tag('kernel.event_listener');
 
     $services->set(ForBuildEnvVarProcessor::class);
+
+    $services->set(DeferredEntityEventDispatcherInterface::class, DeferredEntityEventDispatcher::class);
 };
