b8d9cc0150abfc44b269513d3c810bd6e6e8cac6
natepage <nathan.page@eonx.com>
2021-07-05 12:39:15 +1000

    [EasyHttpClient] Add Symfony config to decorate default http client

--- packages/EasyHttpClient/src/Bridge/Symfony/EasyHttpClientSymfonyBundle.php
+++ packages/EasyHttpClient/src/Bridge/Symfony/EasyHttpClientSymfonyBundle.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace EonX\EasyHttpClient\Bridge\Symfony;
 
+use EonX\EasyHttpClient\Bridge\Symfony\DependencyInjection\Compiler\DecorateDefaultClientPass;
 use EonX\EasyHttpClient\Bridge\Symfony\DependencyInjection\EasyHttpClientExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class EasyHttpClientSymfonyBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new DecorateDefaultClientPass());
+    }
+
     public function getContainerExtension(): ExtensionInterface
     {
         return new EasyHttpClientExtension();
