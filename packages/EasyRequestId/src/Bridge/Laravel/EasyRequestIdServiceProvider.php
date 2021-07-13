998bf7c4fb7b59549098550cf9d194a0be0cd5e7
natepage <nathan.page@eonx.com>
2021-07-13 11:05:09 +1000

    [EasyRequestId] Add support for laravel queue job

--- packages/EasyRequestId/src/Bridge/Laravel/EasyRequestIdServiceProvider.php
+++ packages/EasyRequestId/src/Bridge/Laravel/EasyRequestIdServiceProvider.php
@@ -15,15 +15,55 @@ use EonX\EasyRequestId\RequestIdService;
 use EonX\EasyRequestId\UuidV4FallbackResolver;
 use EonX\EasyWebhook\Bridge\BridgeConstantsInterface as EasyWebhookBridgeConstantsInterface;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Queue\Queue;
 use Illuminate\Support\ServiceProvider;
 
 final class EasyRequestIdServiceProvider extends ServiceProvider
 {
+    /**
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
     public function boot(): void
     {
         $this->publishes([
             __DIR__ . '/config/easy-request-id.php' => \base_path('config/easy-request-id.php'),
         ]);
+
+        /** @var \EonX\EasyRequestId\Interfaces\RequestIdServiceInterface $requestIdService */
+        $requestIdService = $this->app->make(RequestIdServiceInterface::class);
+
+        // Queue
+        // Add IDs to jobs pushed to the queue
+        Queue::createPayloadUsing(static function () use ($requestIdService): array {
+            return [
+                'easy_request_id' => [
+                    $requestIdService->getCorrelationIdHeaderName() => $requestIdService->getCorrelationId(),
+                    $requestIdService->getRequestIdHeaderName() => $requestIdService->getRequestId(),
+                ],
+            ];
+        });
+
+        // Resolve IDs from jobs from the queue
+        $this->app->make('events')->listen(
+            JobProcessing::class,
+            static function (JobProcessing $event) use ($requestIdService): void {
+                $body = \json_decode($event->job->getRawBody(), true);
+
+                if (\is_array($body) === false) {
+                    return;
+                }
+
+                $requestIdService->setResolver(static function () use ($body, $requestIdService): array {
+                    $ids = $body['easy_request_id'] ?? [];
+
+                    return [
+                        RequestIdServiceInterface::KEY_RESOLVED_CORRELATION_ID => $ids[$requestIdService->getCorrelationIdHeaderName()] ?? null,
+                        RequestIdServiceInterface::KEY_RESOLVED_REQUEST_ID => $ids[$requestIdService->getRequestIdHeaderName()] ?? null,
+                    ];
+                });
+            }
+        );
     }
 
     public function register(): void
--- packages/EasyRequestId/tests/Bridge/Laravel/EasyRequestIdServiceProviderTest.php
+++ packages/EasyRequestId/tests/Bridge/Laravel/EasyRequestIdServiceProviderTest.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 namespace EonX\EasyRequestId\Tests\Bridge\Laravel;
 
+use EonX\EasyRequestId\Bridge\Laravel\Bus\DispatcherDecorator;
 use EonX\EasyRequestId\Interfaces\RequestIdServiceInterface;
+use Illuminate\Contracts\Bus\QueueingDispatcher;
 use Ramsey\Uuid\Uuid;
 
 final class EasyRequestIdServiceProviderTest extends AbstractLumenTestCase
 {
     public function testSanity(): void
     {
-        $requestIdService = $this->getApplication()
-            ->make(RequestIdServiceInterface::class);
+        $app = $this->getApplication();
+        $dispatcher = $app->make(QueueingDispatcher::class);
+        $requestIdService = $app->make(RequestIdServiceInterface::class);
 
+        self::assertInstanceOf(DispatcherDecorator::class, $dispatcher);
         self::assertTrue(Uuid::isValid($requestIdService->getCorrelationId()));
         self::assertTrue(Uuid::isValid($requestIdService->getRequestId()));
     }
