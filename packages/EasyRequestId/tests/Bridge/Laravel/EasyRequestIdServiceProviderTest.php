37cc25b1542fe7a411570f81697dfd675e82e313
natepage <nathan.page@eonx.com>
2021-07-13 11:11:31 +1000

    [EasyRequestId] Fix phpstan

--- packages/EasyRequestId/tests/Bridge/Laravel/EasyRequestIdServiceProviderTest.php
+++ packages/EasyRequestId/tests/Bridge/Laravel/EasyRequestIdServiceProviderTest.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace EonX\EasyRequestId\Tests\Bridge\Laravel;
 
-use EonX\EasyRequestId\Bridge\Laravel\Bus\DispatcherDecorator;
 use EonX\EasyRequestId\Interfaces\RequestIdServiceInterface;
-use Illuminate\Contracts\Bus\QueueingDispatcher;
 use Ramsey\Uuid\Uuid;
 
 final class EasyRequestIdServiceProviderTest extends AbstractLumenTestCase
@@ -14,10 +12,8 @@ final class EasyRequestIdServiceProviderTest extends AbstractLumenTestCase
     public function testSanity(): void
     {
         $app = $this->getApplication();
-        $dispatcher = $app->make(QueueingDispatcher::class);
         $requestIdService = $app->make(RequestIdServiceInterface::class);
 
-        self::assertInstanceOf(DispatcherDecorator::class, $dispatcher);
         self::assertTrue(Uuid::isValid($requestIdService->getCorrelationId()));
         self::assertTrue(Uuid::isValid($requestIdService->getRequestId()));
     }
