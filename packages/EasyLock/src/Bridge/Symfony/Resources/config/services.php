06e11f7dbe3108706dde2e01fcca99978dea1139
natepage <nathan.page@eonx.com>
2021-07-15 19:10:50 +1000

    [EasyLock] Fix logger di for log channel

--- packages/EasyLock/src/Bridge/Symfony/Resources/config/services.php
+++ packages/EasyLock/src/Bridge/Symfony/Resources/config/services.php
@@ -7,13 +7,12 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use EonX\EasyLock\Bridge\BridgeConstantsInterface;
 use EonX\EasyLock\Interfaces\LockServiceInterface;
 use EonX\EasyLock\LockService;
-use Psr\Log\LoggerInterface;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
 
     $services
         ->set(LockServiceInterface::class, LockService::class)
-        ->args([ref(BridgeConstantsInterface::SERVICE_STORE), ref(LoggerInterface::class)])
+        ->arg('$store', ref(BridgeConstantsInterface::SERVICE_STORE))
         ->tag('monolog.logger', ['channel' => BridgeConstantsInterface::LOG_CHANNEL]);
 };
