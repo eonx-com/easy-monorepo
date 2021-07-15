ee42c7a3cd4f7e59ee47733b8ecb6914461e9ae5
Nathan Page <nathan.page@eonx.com>
2021-07-15 10:36:52 +1000

    [EasyRequestId] Make RequestIdStamp getters nullable + Implement FromArrayResolver (#659)

---
+++ packages/EasyRequestId/src/Resolvers/FromArrayResolver.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyRequestId\Resolvers;
+
+use EonX\EasyRequestId\Interfaces\RequestIdServiceInterface;
+
+final class FromArrayResolver
+{
+    /**
+     * @var mixed[]
+     */
+    private $array;
+
+    /**
+     * @var \EonX\EasyRequestId\Interfaces\RequestIdServiceInterface
+     */
+    private $requestIdService;
+
+    /**
+     * @param mixed[] $array
+     */
+    public function __construct(array $array, RequestIdServiceInterface $requestIdService)
+    {
+        $this->array = $array;
+        $this->requestIdService = $requestIdService;
+    }
+
+    /**
+     * @return null[]|string[]
+     */
+    public function __invoke(): array
+    {
+        return [
+            RequestIdServiceInterface::KEY_RESOLVED_CORRELATION_ID => $this->getIdValue($this->requestIdService->getCorrelationIdHeaderName()),
+            RequestIdServiceInterface::KEY_RESOLVED_REQUEST_ID => $this->getIdValue($this->requestIdService->getRequestIdHeaderName()),
+        ];
+    }
+
+    private function getIdValue(string $id): ?string
+    {
+        $value = $this->array[$id] ?? null;
+
+        return \is_string($value) && $value !== '' ? $value : null;
+    }
+}
