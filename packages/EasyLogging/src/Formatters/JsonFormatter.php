cb133d5e1b028022a6121afc388acb7476f84408
natepage <nathan.page@eonx.com>
2021-07-27 13:32:53 +1000

    [EasyLogging] JsonFormatter which explicitly formats DateTimes

---
+++ packages/EasyLogging/src/Formatters/JsonFormatter.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyLogging\Formatters;
+
+use Monolog\Formatter\JsonFormatter as BaseJsonFormatter;
+
+final class JsonFormatter extends BaseJsonFormatter
+{
+    /**
+     * @param mixed $data
+     * @param null|int $depth
+     *
+     * @return mixed
+     */
+    protected function normalize($data, $depth = null)
+    {
+        return parent::normalize($this->formatDateTimes($data), $depth);
+    }
+
+    /**
+     * @param mixed $data
+     *
+     * @return mixed
+     */
+    private function formatDateTimes($data)
+    {
+        if (\is_array($data)) {
+            foreach ($data as $key => $value) {
+                $data[$key] = $this->formatDateTimes($value);
+            }
+        }
+
+        if ($data instanceof \DateTimeInterface) {
+            return $data->format('Y-m-d\TH:i:sP');
+        }
+
+        return $data;
+    }
+}
