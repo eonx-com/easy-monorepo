81e274316067031421ac5a6e7676ca3fcd78ce21
Nathan Page <nathan.page@eonx.com>
2021-08-12 22:47:29 +1000

    [EasyCore] Improve search service in migration implementation for Symfony (#696)
    * [EasyCore] Make search service public
    
    * [EasyCore] Create abstract migration with delete indices for symfony

---
+++ packages/EasyCore/src/Bridge/Symfony/Doctrine/AbstractDoctrineMigration.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyCore\Bridge\Symfony\Doctrine;
+
+use Doctrine\Migrations\AbstractMigration;
+use EonX\EasyCore\Search\SearchServiceInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+abstract class AbstractDoctrineMigration extends AbstractMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @param string[] $indices
+     */
+    protected function deleteIndices(array $indices): void
+    {
+        if ($this->container === null) {
+            $this->warnIf(true, 'Container is null, do not delete indices');
+
+            return;
+        }
+
+        try {
+            $this->container
+                ->get(SearchServiceInterface::class)
+                ->deleteIndices($indices);
+        } catch (\Throwable $throwable) {
+            $this->warnIf(true, $throwable->getMessage());
+        }
+    }
+}
