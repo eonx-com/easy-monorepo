f2e8a8399543236c6e05de238221a419aaccaf1d
Nathan Page <nathan.page@eonx.com>
2021-08-17 08:44:43 +1000

    [EasySecurity] Support new symfony security system (#701)
    * [EasySecurity] Support new symfony security system
    
    * [EasySecurity] Fix bridge implementation

---
+++ packages/EasySecurity/src/Bridge/Symfony/Security/SecurityContextAuthenticator.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasySecurity\Bridge\Symfony\Security;
+
+use EonX\EasySecurity\Bridge\Symfony\Interfaces\AuthenticationFailureResponseFactoryInterface;
+use EonX\EasySecurity\Interfaces\SecurityContextResolverInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+final class SecurityContextAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
+{
+    /**
+     * @var \EonX\EasySecurity\Bridge\Symfony\Interfaces\AuthenticationFailureResponseFactoryInterface
+     */
+    private $responseFactory;
+
+    /**
+     * @var \EonX\EasySecurity\Interfaces\SecurityContextResolverInterface
+     */
+    private $securityContextResolver;
+
+    public function __construct(
+        SecurityContextResolverInterface $securityContextResolver,
+        AuthenticationFailureResponseFactoryInterface $respFactory
+    ) {
+        $this->securityContextResolver = $securityContextResolver;
+        $this->responseFactory = $respFactory;
+    }
+
+    public function authenticate(Request $request): PassportInterface
+    {
+        try {
+            $user = $this->securityContextResolver->resolveContext()->getUserOrFail();
+        } catch (\Throwable $throwable) {
+            throw new AuthenticationException($throwable->getMessage());
+        }
+
+        // From here we know we have a user, simply fake it in symfony
+        return new SelfValidatingPassport(
+            new UserBadge((string)$user->getUniqueId(), static function (): UserInterface {
+                return new FakeUser();
+            })
+        );
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return $this->responseFactory->create($request, $exception);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return null;
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null): Response
+    {
+        return $this->responseFactory->create($request, $authException);
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return true;
+    }
+}
