32892ca50229826eb6ffae8b34c9583687facd6a
Nathan Page <nathan.page@eonx.com>
2021-08-17 10:09:11 +1000

    [EasyBatch] Fix datetime type with microseconds (#702)
    * [EasyBatch] Fix datetime type with microseconds
    
    * Remove phpstan message for new symfony security class for now
    
    * Remove phpstan message for new symfony security class for now - 1
    
    * Remove phpstan message for new symfony security class for now - 2

---
+++ packages/EasyBatch/src/Bridge/Doctrine/DateTimeWithMicroSeconds.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyBatch\Bridge\Doctrine;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+final class DateTimeWithMicroSeconds extends Type
+{
+    /**
+     * @var string
+     */
+    public const NAME = 'datetime_with_microseconds';
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    /**
+     * @param mixed[] $column
+     *
+     * @throws \Doctrine\DBAL\Exception
+     */
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        switch ($platform->getName()) {
+            case 'mysql':
+                return 'DATETIME(6)';
+            case 'postgresql':
+                return 'TIMESTAMP(6) WITHOUT TIME ZONE';
+            default:
+                return $platform->getDateTimeTypeDeclarationSQL($column);
+        }
+    }
+}
