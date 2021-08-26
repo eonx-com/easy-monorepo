01bab779d76dff26acb81809cd0070cd1dc9b3ff
natepage <nathan.page@eonx.com>
2021-08-26 15:05:28 +1000

    [EasySecurity] Make sure configurators are ordered properly

--- packages/EasySecurity/src/Configurators/FromRequestConfigurator.php
+++ packages/EasySecurity/src/Configurators/FromRequestConfigurator.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace EonX\EasySecurity\Configurators;
 
+use EonX\EasySecurity\Interfaces\SecurityContextConfiguratorInterface;
 use EonX\EasySecurity\Interfaces\SecurityContextInterface;
+use EonX\EasyUtils\CollectorHelper;
 use Symfony\Component\HttpFoundation\Request;
 
 final class FromRequestConfigurator
@@ -25,7 +27,9 @@ final class FromRequestConfigurator
     public function __construct(Request $request, iterable $configurators)
     {
         $this->request = $request;
-        $this->configurators = $configurators;
+        $this->configurators = CollectorHelper::orderLowerPriorityFirstAsArray(
+            CollectorHelper::filterByClass($configurators, SecurityContextConfiguratorInterface::class)
+        );
     }
 
     public function __invoke(SecurityContextInterface $securityContext): SecurityContextInterface
