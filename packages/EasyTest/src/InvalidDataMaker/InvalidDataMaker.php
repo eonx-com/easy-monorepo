4ad19dee06b72b99d2be3dceee6f8baf1e6e059b
Aleksey Polyvanyi <alexndlm@gmail.com>
2021-09-05 14:05:03 +0300

    -add @todo

--- packages/EasyTest/src/InvalidDataMaker/InvalidDataMaker.php
+++ packages/EasyTest/src/InvalidDataMaker/InvalidDataMaker.php
@@ -322,6 +322,18 @@ class InvalidDataMaker extends AbstractInvalidDataMaker
         $value = ($integerPart ?? 0) + \round(1 / 3, $precision + 1);
 
         yield from $this->create("{$this->property} has invalid precision", $value);
+
+        /*
+         * @todo add types validation and invalid relationships validation cases if/when API Platform starts
+         * to return 400 instead of 500
+         * @see https://github.com/api-platform/api-platform/issues/788
+         *
+         * $value = 'abc';
+         * yield from $this->create("{$this->property} is a string", $value);
+         *
+         * $value = 10;
+         * yield from $this->create("{$this->property} is an integer", $value);
+         */
     }
 
     /**
