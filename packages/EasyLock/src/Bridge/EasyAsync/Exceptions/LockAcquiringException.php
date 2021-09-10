96d82f0ce99ebaec05085fddf8f9b193d0ef6377
natepage <nathan.page@eonx.com>
2021-09-11 00:31:09 +1000

    [EasyLock] Fix ecs

--- packages/EasyLock/src/Bridge/EasyAsync/Exceptions/LockAcquiringException.php
+++ packages/EasyLock/src/Bridge/EasyAsync/Exceptions/LockAcquiringException.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace EonX\EasyLock\Bridge\EasyAsync\Exceptions;
@@ -6,7 +7,8 @@ namespace EonX\EasyLock\Bridge\EasyAsync\Exceptions;
 use EonX\EasyAsync\Interfaces\ShouldKillWorkerExceptionInterface;
 use EonX\EasyLock\Interfaces\EasyLockExceptionInterface;
 
-final class LockAcquiringException extends \RuntimeException implements EasyLockExceptionInterface, ShouldKillWorkerExceptionInterface
+final class LockAcquiringException extends \RuntimeException
+    implements EasyLockExceptionInterface, ShouldKillWorkerExceptionInterface
 {
     // No body needed.
 }
