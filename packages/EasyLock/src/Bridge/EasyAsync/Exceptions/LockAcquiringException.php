1bf49a1e8bf3842963c998a75199cdaf45581a20
natepage <nathan.page@eonx.com>
2021-09-11 00:27:38 +1000

    [EasyLock] Implement easy-async bridge for exception to kill worker

---
+++ packages/EasyLock/src/Bridge/EasyAsync/Exceptions/LockAcquiringException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyLock\Bridge\EasyAsync\Exceptions;
+
+use EonX\EasyAsync\Interfaces\ShouldKillWorkerExceptionInterface;
+use EonX\EasyLock\Interfaces\EasyLockExceptionInterface;
+
+final class LockAcquiringException extends \RuntimeException implements EasyLockExceptionInterface, ShouldKillWorkerExceptionInterface
+{
+    // No body needed.
+}
