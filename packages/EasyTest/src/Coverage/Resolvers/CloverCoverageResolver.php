c2cc342618a1dec1b076e2f53c4c7725a052be4e
Andreev <randreev@131.ru>
2021-09-22 16:31:22 +0300

    [EasyTest] Add coverage resolver for *.clover format.
    - small refactoring

--- packages/EasyTest/src/Coverage/Resolvers/CloverCoverageResolver.php
+++ packages/EasyTest/src/Coverage/Resolvers/CloverCoverageResolver.php
@@ -12,6 +12,21 @@ use SimpleXMLElement;
 
 final class CloverCoverageResolver implements CoverageResolverInterface
 {
+    /**
+     * @var string
+     */
+    private const ATTRIBUTE_NAME_COVERED_ELEMENTS = 'coveredelements';
+
+    /**
+     * @var string
+     */
+    private const ATTRIBUTE_NAME_ELEMENTS = 'elements';
+
+    /**
+     * @var string
+     */
+    private const ATTRIBUTE_NAME_FILE_NAME = 'name';
+
     public function resolve(string $coverageOutput): CoverageReportDto
     {
         $violations = [];
@@ -28,14 +43,12 @@ final class CloverCoverageResolver implements CoverageResolverInterface
         $metrics = $xml->xpath('//file/metrics');
 
         foreach ($metrics as $metric) {
-            $elements = (int)$metric->attributes()
-                ->elements;
-            $coveredElements = (int)$metric->attributes()
-                ->coveredelements;
+            $elements = (int)$this->extractXMLAttribute($metric, self::ATTRIBUTE_NAME_ELEMENTS);
+            $coveredElements = (int)$this->extractXMLAttribute($metric, self::ATTRIBUTE_NAME_COVERED_ELEMENTS);
 
             if ($elements !== $coveredElements) {
                 $file = $metric->xpath('parent::*')[0];
-                $violations[] = (string)$file->attributes()->name;
+                $violations[] = $this->extractXMLAttribute($file, self::ATTRIBUTE_NAME_FILE_NAME);
             }
         }
 
@@ -44,5 +57,26 @@ final class CloverCoverageResolver implements CoverageResolverInterface
         $coverage = ($totalCoveredElements / $totalElements) * 100;
 
         return new CoverageReportDto($coverage, $violations);
+    }
+
+    private function extractXMLAttribute(SimpleXMLElement $element, string $attributeName): string
+    {
+        $attr = $element->attributes();
+
+        if ($attr === null) {
+            throw new UnableToResolveCoverageException(\sprintf(
+                '[%s] Given output could not be parsed',
+                static::class
+            ));
+        }
+
+        if (isset($attr[$attributeName]) === false) {
+            throw new UnableToResolveCoverageException(\sprintf(
+                '[%s] Given output could not be parsed',
+                static::class
+            ));
+        }
+
+        return (string)$attr[$attributeName];
     }
 }
