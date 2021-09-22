dce08b55e827d191143f9f84e72fa8030b944377
natepage <nathan.page@eonx.com>
2021-09-22 16:53:55 +1000

    Modify every composer.json files

--- monorepo/Console/Commands/ReplaceAsteriskWithDevMasterCommand.php
+++ monorepo/Console/Commands/ReplaceAsteriskWithDevMasterCommand.php
@@ -4,37 +4,43 @@ declare(strict_types=1);
 namespace EonX\EasyMonorepo\Console\Commands;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symplify\SmartFileSystem\Finder\FinderSanitizer;
 
 final class ReplaceAsteriskWithDevMasterCommand extends Command
 {
     protected static $defaultName = 'replace-asterisk-with-dev-master';
 
-    protected function configure(): void
-    {
-        $this->addArgument('path', InputArgument::REQUIRED);
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $filesystem = new Filesystem();
-        $filename = \sprintf('%s/composer.json', $input->getArgument('path'));
 
-        if ($filesystem->exists($filename) === false) {
-            $output->writeln(\sprintf('File %s does not exist', $filename));
+        foreach ($this->getComposerJsonFiles() as $composerJsonFile) {
+            $filename = $composerJsonFile->getFilename();
 
-            return self::FAILURE;
+            $updatedContents = \str_replace('*', 'dev-feature/php8-the-better-way', \file_get_contents($filename));
+            $updatedContents = \str_replace('"canonical": false', '"canonical": true', $updatedContents);
+
+            $filesystem->dumpFile($filename, $updatedContents);
+
+            $output->writeln(\sprintf('Successfully updated %s', $filename));
         }
 
-        $updatedContents = \str_replace('*', 'dev-feature/php8-the-better-way', \file_get_contents($filename));
-        $updatedContents = \str_replace('"canonical": false', '"canonical": true', $updatedContents);
-        $filesystem->dumpFile($filename, $updatedContents);
-
-        $output->writeln(\sprintf('Successfully updated %s', $filename));
-
         return self::SUCCESS;
+    }
+
+    /**
+     * @return \Symplify\SmartFileSystem\SmartFileInfo[]
+     */
+    private function getComposerJsonFiles(): array
+    {
+        $finder = (new Finder())
+            ->in([__DIR__ . '/../../../packages'])
+            ->name('composer.json');
+
+        return (new FinderSanitizer())->sanitize($finder);
     }
 }
