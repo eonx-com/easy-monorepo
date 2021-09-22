281cca2835019f9b8d2b42c54a539a5f8a22e4d9
natepage <nathan.page@eonx.com>
2021-09-22 23:25:17 +1000

    Implement our own localize packages path for split testing

---
+++ monorepo/Console/Commands/LocalizePackageRepositoriesCommand.php
@@ -0,0 +1,116 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyMonorepo\Console\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symplify\MonorepoBuilder\Split\Git\GitManager;
+use Symplify\SmartFileSystem\Finder\FinderSanitizer;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class LocalizePackageRepositoriesCommand extends Command
+{
+    protected static $defaultName = 'localize-monorepo-packages';
+
+    /**
+     * @var \Symplify\MonorepoBuilder\Split\Git\GitManager
+     */
+    private $gitManager;
+
+    public function __construct(GitManager $gitManager)
+    {
+        $this->gitManager = $gitManager;
+
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $composerJsonFiles = $this->getComposerJsonFiles();
+        $devVersion = \sprintf('dev-%s', $this->gitManager->getCurrentBranch());
+        $filesystem = new Filesystem();
+        $monorepoPackages = $this->getMonorepoPackages($composerJsonFiles);
+        $monorepoPackageNames = \array_keys($monorepoPackages);
+
+        foreach ($composerJsonFiles as $composerJsonFile) {
+            $filename = $composerJsonFile->getRealPath();
+            $localMonorepoPackages = [];
+            $repositories = [];
+            $composerJsonFileContents = $this->getComposerJsonFileContents($composerJsonFile);
+
+            // Replace monorepo packages version with dev one
+            foreach (['require', 'require-dev'] as $section) {
+                foreach (\array_keys($composerJsonFileContents[$section]) as $package) {
+                    if (\in_array($package, $monorepoPackageNames, true)) {
+                        $localMonorepoPackages[] = $package;
+                        $composerJsonFileContents[$section][$package] = $devVersion;
+                    }
+                }
+            }
+
+            // Add local repositories for monorepo packages
+            foreach ($localMonorepoPackages as $package) {
+                $repositories[] = [
+                    'type' => 'path',
+                    'url' => \sprintf('../%s', $monorepoPackages[$package]),
+                ];
+            }
+
+            $composerJsonFileContents['repositories'] = $repositories;
+            $composerJsonFileContents['minimum-stability'] = 'dev';
+            $composerJsonFileContents['prefer-stable'] = true;
+
+            $filesystem->dumpFile($filename, \json_encode($composerJsonFileContents, \JSON_PRETTY_PRINT));
+
+            $output->writeln(\sprintf('Successfully updated %s', $filename));
+        }
+
+        return self::SUCCESS;
+    }
+
+    private function getComposerJsonFileContents(SmartFileInfo $composerJsonFile): array
+    {
+        return \json_decode($composerJsonFile->getContents(), true);
+    }
+
+    /**
+     * @return \Symplify\SmartFileSystem\SmartFileInfo[]
+     */
+    private function getComposerJsonFiles(): array
+    {
+        $finder = (new Finder())
+            ->in([__DIR__ . '/../../../packages'])
+            ->name('composer.json');
+
+        return (new FinderSanitizer())->sanitize($finder);
+    }
+
+    private function getDir(SmartFileInfo $composerJson): string
+    {
+        return \last(\explode('/', $composerJson->getRelativeDirectoryPath()));
+    }
+
+    /**
+     * @param \Symplify\SmartFileSystem\SmartFileInfo[] $composerJsonFiles
+     *
+     * @return string[]
+     */
+    private function getMonorepoPackages(array $composerJsonFiles): array
+    {
+        $packages = [];
+
+        foreach ($composerJsonFiles as $composerJsonFile) {
+            $composerJsonContents = $this->getComposerJsonFileContents($composerJsonFile);
+
+            $packages[$composerJsonContents['name']] = $this->getDir($composerJsonFile);
+        }
+
+        \ksort($packages);
+
+        return $packages;
+    }
+}
