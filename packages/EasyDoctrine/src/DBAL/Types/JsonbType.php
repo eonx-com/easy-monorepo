7392852e51b7b88d942e34b52106c881be6c568d
Ivan Torgov <ivan.torgov@eonx.com>
2021-12-16 10:52:16 +0300

    [EasyDoctrine] Sort an array (decoded JSON data) by keys in the `JsonbType` DBAL type.

--- packages/EasyDoctrine/src/DBAL/Types/JsonbType.php
+++ packages/EasyDoctrine/src/DBAL/Types/JsonbType.php
@@ -5,27 +5,93 @@ declare(strict_types=1);
 namespace EonX\EasyDoctrine\DBAL\Types;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Types\JsonType;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+use JsonException;
 
-final class JsonbType extends JsonType
+final class JsonbType extends Type
 {
     /**
      * @var string
      */
-    public const TYPE_NAME = 'JSONB';
+    public const JSONB = 'JSONB';
 
-    public function getName(): string
+    /**
+     * @param mixed $value
+     *
+     * @throws \Doctrine\DBAL\Types\ConversionException
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
     {
-        return static::TYPE_NAME;
+        if ($value === null) {
+            return null;
+        }
+
+        try {
+            return \json_encode($value, \JSON_THROW_ON_ERROR);
+        } catch (JsonException $exception) {
+            throw ConversionException::conversionFailedSerialization(
+                $value,
+                $this->getName(),
+                $exception->getMessage()
+            );
+        }
     }
 
     /**
-     * @param mixed[] $fieldDeclaration
+     * @param mixed $value
      *
-     * @throws \Doctrine\DBAL\DBALException
+     * @return mixed[]|null
+     *
+     * @throws \Doctrine\DBAL\Types\ConversionException
      */
-    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?array
     {
-        return $platform->getDoctrineTypeMapping(static::TYPE_NAME);
+        if ($value === null || $value === '') {
+            return null;
+        }
+
+        if (\is_resource($value)) {
+            $value = \stream_get_contents($value);
+        }
+
+        try {
+            return $this->sortByKey(\json_decode($value, true, 512, \JSON_THROW_ON_ERROR));
+        } catch (JsonException $exception) {
+            throw ConversionException::conversionFailed($value, $this->getName(), $exception);
+        }
+    }
+
+    public function getName(): string
+    {
+        return self::JSONB;
+    }
+
+    /**
+     * @param mixed[] $column
+     *
+     * @throws \Doctrine\DBAL\Exception
+     */
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return $platform->getDoctrineTypeMapping(self::JSONB);
+    }
+
+    /**
+     * @param mixed[] $array
+     *
+     * @return mixed[]
+     */
+    private function sortByKey(array $array): array
+    {
+        \ksort($array);
+
+        foreach ($array as $key => $value) {
+            if (\is_array($value)) {
+                $array[$key] = $this->sortByKey($value);
+            }
+        }
+
+        return $array;
     }
 }
