76ab93ee0ba2129d36d47bacdd2f3ab83bfa71a6
Anton Popov <anton.popov@eonx.com>
2022-01-22 20:45:08 +0700

    feature/PLU-3995-update-carbon-normalizer - updated tests

--- packages/EasyCore/tests/Bridge/Symfony/ApiPlatform/Normalizers/CarbonImmutableNormalizerTest.php
+++ packages/EasyCore/tests/Bridge/Symfony/ApiPlatform/Normalizers/CarbonImmutableNormalizerTest.php
@@ -4,61 +4,24 @@ declare(strict_types=1);
 
 namespace EonX\EasyCore\Tests\Bridge\Symfony\ApiPlatform\Normalizers;
 
-use Carbon\Carbon;
 use Carbon\CarbonImmutable;
-use Carbon\CarbonInterface;
-use Carbon\CarbonTimeZone;
 use EonX\EasyCore\Bridge\Symfony\ApiPlatform\Normalizers\CarbonImmutableNormalizer;
 use EonX\EasyCore\Tests\Bridge\Symfony\AbstractSymfonyTestCase;
-use stdClass;
-use Symfony\Component\Serializer\Exception\InvalidArgumentException;
 
 /**
  * @covers \EonX\EasyCore\Bridge\Symfony\ApiPlatform\Normalizers\CarbonImmutableNormalizer
  */
 final class CarbonImmutableNormalizerTest extends AbstractSymfonyTestCase
 {
-    /**
-     * @var string
-     */
-    private const FORMAT_KEY = 'datetime_format';
-
-    /**
-     * @var string
-     */
-    private const TIMEZONE_KEY = 'datetime_timezone';
-
-    /**
-     * @return iterable<mixed>
-     *
-     * @see testNormalizeSucceeds
-     */
-    public function provideDataForNormalize(): iterable
+    public function testDenormalizeSucceeds(): void
     {
-        CarbonImmutable::setTestNow('2021-12-16 10:00:00');
-        yield 'with empty context' => [
-            'expectedResult' => '2021-12-16T10:00:00+00:00',
-            'carbon' => CarbonImmutable::now(),
-            'context' => [],
-        ];
+        $data = '2021-12-16T17:00:00+07:00';
+        $normalizer = new CarbonImmutableNormalizer();
 
-        yield 'with CarbonTimeZone timezone in context' => [
-            'expectedResult' => '2021-12-16T17:00:00+07:00',
-            'carbon' => CarbonImmutable::now(),
-            'context' => [self::TIMEZONE_KEY => CarbonTimeZone::create('Asia/Krasnoyarsk')],
-        ];
+        $result = $normalizer->denormalize($data, 'some-json');
 
-        yield 'with string timezone in context' => [
-            'expectedResult' => '2021-12-16T17:00:00+07:00',
-            'carbon' => CarbonImmutable::now(),
-            'context' => [self::TIMEZONE_KEY => 'Asia/Krasnoyarsk'],
-        ];
-
-        yield 'with format in context' => [
-            'expectedResult' => '2021-12-16T10:00:00.000+00:00',
-            'carbon' => CarbonImmutable::now(),
-            'context' => [self::FORMAT_KEY => CarbonInterface::RFC3339_EXTENDED],
-        ];
+        self::assertInstanceOf(CarbonImmutable::class, $result);
+        self::assertEquals(new CarbonImmutable($data), $result);
     }
 
     public function testHasCacheableSupportsMethodSucceeds(): void
@@ -66,49 +29,6 @@ final class CarbonImmutableNormalizerTest extends AbstractSymfonyTestCase
         $normalizer = new CarbonImmutableNormalizer();
 
         $result = $normalizer->hasCacheableSupportsMethod();
-
-        self::assertTrue($result);
-    }
-
-    /**
-     * @param mixed[] $context
-     *
-     * @dataProvider provideDataForNormalize
-     */
-    public function testNormalizeSucceeds(string $expectedResult, CarbonInterface $carbon, array $context): void
-    {
-        $normalizer = new CarbonImmutableNormalizer($context);
-
-        $result = $normalizer->normalize($carbon, null, $context);
-
-        self::assertSame($expectedResult, $result);
-    }
-
-    public function testNormalizeThrowsException(): void
-    {
-        $normalizer = new CarbonImmutableNormalizer();
-
-        $this->safeCall(static function () use ($normalizer) {
-            $normalizer->normalize(new stdClass());
-        });
-
-        $this->assertThrownException(InvalidArgumentException::class, 0);
-    }
-
-    public function testSupportsNormalizationReturnsFalse(): void
-    {
-        $normalizer = new CarbonImmutableNormalizer();
-
-        $result = $normalizer->supportsNormalization(new stdClass());
-
-        self::assertFalse($result);
-    }
-
-    public function testSupportsNormalizationReturnsTrue(): void
-    {
-        $normalizer = new CarbonImmutableNormalizer();
-
-        $result = $normalizer->supportsNormalization(new Carbon());
 
         self::assertTrue($result);
     }
