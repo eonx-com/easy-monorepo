35167af93841f9ca12fc153fe56f55db76b26fd5
nathanpage <nathan.page@eonx.com>
2022-02-10 11:48:29 +1100

    [EasyAsync] Implement randomized stop worker limits for messages and time

---
+++ packages/EasyAsync/src/Bridge/Symfony/Resources/config/messenger_stop_on_messages_limit.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use EonX\EasyAsync\Bridge\BridgeConstantsInterface;
+use EonX\EasyAsync\Bridge\Symfony\Messenger\StopWorkerOnMessagesLimitSubscriber;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+    $services
+        ->set(StopWorkerOnMessagesLimitSubscriber::class)
+        ->arg('$minMessages', '%' . BridgeConstantsInterface::PARAM_MESSENGER_WORKER_STOP_MIN_MESSAGES . '%')
+        ->arg('$maxMessages', '%' . BridgeConstantsInterface::PARAM_MESSENGER_WORKER_STOP_MAX_MESSAGES . '%')
+        ->tag('monolog.logger', ['channel' => BridgeConstantsInterface::LOG_CHANNEL]);
+};
--- packages/EasyAsync/tests/Bridge/Symfony/EasyAsyncBundleTest.php
+++ packages/EasyAsync/tests/Bridge/Symfony/EasyAsyncBundleTest.php
@@ -4,15 +4,74 @@ declare(strict_types=1);
 
 namespace EonX\EasyAsync\Tests\Bridge\Symfony;
 
+use EonX\EasyAsync\Bridge\Symfony\Messenger\StopWorkerOnMessagesLimitSubscriber;
+use EonX\EasyAsync\Bridge\Symfony\Messenger\StopWorkerOnTimeLimitSubscriber;
 use EonX\EasyAsync\Interfaces\Batch\BatchFactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 final class EasyAsyncBundleTest extends AbstractSymfonyTestCase
 {
+    /**
+     * @return iterable<mixed>
+     */
+    public function providerTestMessengerConfig(): iterable
+    {
+        yield 'no config - no subscribers' => [
+            static function (ContainerInterface $container): void {
+                self::assertFalse($container->has(StopWorkerOnMessagesLimitSubscriber::class));
+                self::assertFalse($container->has(StopWorkerOnTimeLimitSubscriber::class));
+            },
+        ];
+
+        yield 'messages only' => [
+            static function (ContainerInterface $container): void {
+                self::assertTrue($container->has(StopWorkerOnMessagesLimitSubscriber::class));
+                self::assertFalse($container->has(StopWorkerOnTimeLimitSubscriber::class));
+            },
+            [
+                __DIR__ . '/Fixtures/config/messages_limit_only.yaml',
+            ]
+        ];
+
+        yield 'time only' => [
+            static function (ContainerInterface $container): void {
+                self::assertFalse($container->has(StopWorkerOnMessagesLimitSubscriber::class));
+                self::assertTrue($container->has(StopWorkerOnTimeLimitSubscriber::class));
+            },
+            [
+                __DIR__ . '/Fixtures/config/time_limit_only.yaml',
+            ]
+        ];
+
+        yield 'both limits' => [
+            static function (ContainerInterface $container): void {
+                self::assertTrue($container->has(StopWorkerOnMessagesLimitSubscriber::class));
+                self::assertTrue($container->has(StopWorkerOnTimeLimitSubscriber::class));
+            },
+            [
+                __DIR__ . '/Fixtures/config/both_limits.yaml',
+            ]
+        ];
+    }
+
     public function testSanity(): void
     {
         $container = $this->getKernel()
             ->getContainer();
 
         self::assertInstanceOf(BatchFactoryInterface::class, $container->get(BatchFactoryInterface::class));
+    }
+
+    /**
+     * @dataProvider providerTestMessengerConfig
+     *
+     * @param null|string[] $configs
+     */
+    public function testMessengerConfig(callable $assert, ?array $configs = null): void
+    {
+        $container = $this->getKernel($configs)
+            ->getContainer();
+
+        \call_user_func($assert, $container);
     }
 }
