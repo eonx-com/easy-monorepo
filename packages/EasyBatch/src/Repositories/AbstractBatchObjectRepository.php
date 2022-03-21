f484a1fb7bbae97c3e62b8ffe1229bb62070eb82
nathanpage <nathan.page@eonx.com>
2022-03-21 11:20:02 +1100

    [EasyBatch] Prevent extra columns to break database interactions

--- packages/EasyBatch/src/Repositories/AbstractBatchObjectRepository.php
+++ packages/EasyBatch/src/Repositories/AbstractBatchObjectRepository.php
@@ -14,42 +14,18 @@ use EonX\EasyBatch\Interfaces\BatchObjectTransformerInterface;
 abstract class AbstractBatchObjectRepository
 {
     /**
-     * @var \Doctrine\DBAL\Connection
+     * @var null|string[]
      */
-    protected $conn;
-
-    /**
-     * @var \EonX\EasyBatch\Interfaces\BatchObjectFactoryInterface
-     */
-    protected $factory;
-
-    /**
-     * @var string
-     */
-    protected $table;
-
-    /**
-     * @var \EonX\EasyBatch\Interfaces\BatchObjectTransformerInterface
-     */
-    private $transformer;
-
-    /**
-     * @var \EonX\EasyBatch\Interfaces\BatchObjectIdStrategyInterface
-     */
-    private $idStrategy;
+    private ?array $tableColumns = null;
 
     public function __construct(
-        BatchObjectFactoryInterface $factory,
-        BatchObjectIdStrategyInterface $idStrategy,
-        BatchObjectTransformerInterface $transformer,
-        Connection $conn,
-        string $table
+        protected BatchObjectFactoryInterface $factory,
+        protected BatchObjectIdStrategyInterface $idStrategy,
+        protected BatchObjectTransformerInterface $transformer,
+        protected Connection $conn,
+        protected string $table
     ) {
-        $this->factory = $factory;
-        $this->idStrategy = $idStrategy;
-        $this->transformer = $transformer;
-        $this->conn = $conn;
-        $this->table = $table;
+        // No body needed.
     }
 
     /**
@@ -65,6 +41,9 @@ abstract class AbstractBatchObjectRepository
         $batchObject->setUpdatedAt($now);
 
         $data = $this->transformer->transformToArray($batchObject);
+        foreach(\array_diff(\array_keys($data), $this->resolveTableColumns()) as $toRemove) {
+            unset($data[$toRemove]);
+        }
 
         $this->has($batchObjectId) === false
             ? $this->conn->insert($this->table, $data)
@@ -106,5 +85,24 @@ abstract class AbstractBatchObjectRepository
         $result = $this->conn->fetchAssociative($sql, ['id' => $id]);
 
         return \is_array($result) ? $result : null;
+    }
+
+    /**
+     * @return string[]
+     *
+     * @throws \Doctrine\DBAL\Exception
+     */
+    private function resolveTableColumns(): array
+    {
+        if ($this->tableColumns !== null) {
+            return $this->tableColumns;
+        }
+
+        $sql = $this->conn->getDatabasePlatform()
+            ->getListTableColumnsSQL($this->table);
+
+        $columns = $this->conn->fetchAllAssociative($sql);
+
+        return $this->tableColumns = \array_column($columns, 'name');
     }
 }
