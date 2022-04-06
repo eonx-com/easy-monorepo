d3571ee97d377dbf3525b6aa3b9d3024acdfbfbf
Aleksey Polyvanyi <aleksey.polyvanyi@eonx.com>
2022-04-06 13:43:51 +0200

    -refactoring

--- packages/EasyDoctrine/src/Utils/ObjectCopier.php
+++ packages/EasyDoctrine/src/Utils/ObjectCopier.php
@@ -7,7 +7,7 @@ namespace EonX\EasyDoctrine\Utils;
 use DeepCopy\DeepCopy;
 use EonX\EasyDoctrine\Interfaces\ObjectCopierInterface;
 
-class ObjectCopier implements ObjectCopierInterface
+final class ObjectCopier implements ObjectCopierInterface
 {
     public function __construct(private DeepCopy $deepCopy)
     {
