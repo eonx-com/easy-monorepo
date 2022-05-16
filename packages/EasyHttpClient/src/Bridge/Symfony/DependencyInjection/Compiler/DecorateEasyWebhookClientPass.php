3ddde2f50eb6a65c8f409803e2d90ee68366d000
Nathan Page <nathan.page@eonx.com>
2022-05-17 02:07:53 +1000

    [EasyHttpClient] Use default http client for SQS transport (#936)
    * [EasyHttpClient] Use default http client for SQS transport
    
    * [EasyHttpClient] Fix ecs

--- packages/EasyHttpClient/src/Bridge/Symfony/DependencyInjection/Compiler/DecorateEasyWebhookClientPass.php
+++ packages/EasyHttpClient/src/Bridge/Symfony/DependencyInjection/Compiler/DecorateEasyWebhookClientPass.php
@@ -5,42 +5,30 @@ declare(strict_types=1);
 namespace EonX\EasyHttpClient\Bridge\Symfony\DependencyInjection\Compiler;
 
 use EonX\EasyHttpClient\Bridge\BridgeConstantsInterface;
-use EonX\EasyHttpClient\Implementations\Symfony\WithEventsHttpClient;
 use EonX\EasyWebhook\Bridge\BridgeConstantsInterface as EasyWebhookBridgeConstantsInterface;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 
-final class DecorateEasyWebhookClientPass implements CompilerPassInterface
+final class DecorateEasyWebhookClientPass extends AbstractEasyHttpClientCompilerPass
 {
-    /**
-     * @var string
-     */
     private const DECORATION_SERVICE_ID = 'easy_http_client.decorate_easy_webhook';
 
-    public function process(ContainerBuilder $container): void
+    protected function doProcess(ContainerBuilder $container): void
     {
         // Apply only if enabled, easy-webhook is installed and client definition exists
-        if ($this->isEnabled($container) === false
-            || \interface_exists(EasyWebhookBridgeConstantsInterface::class) === false
+        if (\interface_exists(EasyWebhookBridgeConstantsInterface::class) === false
             || $container->has(EasyWebhookBridgeConstantsInterface::HTTP_CLIENT) === false) {
             return;
         }
 
-        $def = (new Definition(WithEventsHttpClient::class))
-            ->setAutowired(true)
-            ->setAutoconfigured(true)
-            ->setDecoratedService(EasyWebhookBridgeConstantsInterface::HTTP_CLIENT);
-
-        $container->setDefinition(self::DECORATION_SERVICE_ID, $def);
+        $this->decorateHttpClient(
+            $container,
+            EasyWebhookBridgeConstantsInterface::HTTP_CLIENT,
+            self::DECORATION_SERVICE_ID
+        );
     }
 
-    private function isEnabled(ContainerBuilder $container): bool
+    protected function getEnableParamName(): string
     {
-        if ($container->hasParameter(BridgeConstantsInterface::PARAM_DECORATE_EASY_WEBHOOK_CLIENT) === false) {
-            return false;
-        }
-
-        return (bool)$container->getParameter(BridgeConstantsInterface::PARAM_DECORATE_EASY_WEBHOOK_CLIENT);
+        return BridgeConstantsInterface::PARAM_DECORATE_EASY_WEBHOOK_CLIENT;
     }
 }
--- packages/EasyHttpClient/src/Bridge/Symfony/EasyHttpClientSymfonyBundle.php
+++ packages/EasyHttpClient/src/Bridge/Symfony/EasyHttpClientSymfonyBundle.php
@@ -6,6 +6,7 @@ namespace EonX\EasyHttpClient\Bridge\Symfony;
 
 use EonX\EasyHttpClient\Bridge\Symfony\DependencyInjection\Compiler\DecorateDefaultClientPass;
 use EonX\EasyHttpClient\Bridge\Symfony\DependencyInjection\Compiler\DecorateEasyWebhookClientPass;
+use EonX\EasyHttpClient\Bridge\Symfony\DependencyInjection\Compiler\DecorateMessengerSqsClientPass;
 use EonX\EasyHttpClient\Bridge\Symfony\DependencyInjection\EasyHttpClientExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
@@ -15,8 +16,10 @@ final class EasyHttpClientSymfonyBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
-        $container->addCompilerPass(new DecorateDefaultClientPass());
-        $container->addCompilerPass(new DecorateEasyWebhookClientPass());
+        $container
+            ->addCompilerPass(new DecorateDefaultClientPass())
+            ->addCompilerPass(new DecorateEasyWebhookClientPass())
+            ->addCompilerPass(new DecorateMessengerSqsClientPass());
     }
 
     public function getContainerExtension(): ExtensionInterface
