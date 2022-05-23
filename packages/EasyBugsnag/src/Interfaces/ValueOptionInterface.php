e304751307f9dc7dffda67d14f572dda348f4128
nathanpage <nathan.page@eonx.com>
2022-05-23 14:07:04 +1000

    [EasyBugsnag] Allow to resolve request in cli

---
+++ packages/EasyBugsnag/src/Interfaces/ValueOptionInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyBugsnag\Interfaces;
+
+interface ValueOptionInterface
+{
+    /**
+     * @var string
+     */
+    public const RESOLVE_REQUEST_IN_CLI = 'easy_bugsnag.resolve_request_in_cli';
+}
