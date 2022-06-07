d149579a34f5cf5e1adf0de507779f2e8e98e1fb
nathanpage <nathan.page@eonx.com>
2022-06-07 13:10:18 +1000

    [EasySwoole] Fix access log integration

--- packages/EasySwoole/src/AccessLog/HttpFoundationAccessLogFormatter.php
+++ packages/EasySwoole/src/AccessLog/HttpFoundationAccessLogFormatter.php
@@ -11,16 +11,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class HttpFoundationAccessLogFormatter implements HttpFoundationAccessLogFormatterInterface
 {
-    public function __construct(private readonly string $timezone = 'UTC')
-    {
-    }
-
     public function formatAccessLog(Request $request, Response $response): string
     {
         return \sprintf(
-            '%s - [%s] "%s %s%s %s" %d "%s"',
+            '%s - "%s %s%s %s" %d "%s"',
             $request->getClientIp(),
-            (Carbon::now($this->timezone))->toIso8601ZuluString('microsecond'),
             $request->getMethod(),
             $request->getPathInfo(),
             $request->getQueryString() ? '?' . $request->getQueryString() : null,
