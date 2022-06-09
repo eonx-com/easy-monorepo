2882ff8a92ade0c43cab7e38a5c0e81a335533d7
Nathan Page <nathan.page@eonx.com>
2022-06-09 13:09:59 +1000

    [EasyBatch] Unset current batch objects on message after processing it (#957)

--- packages/EasyBatch/src/Interfaces/CurrentBatchAwareInterface.php
+++ packages/EasyBatch/src/Interfaces/CurrentBatchAwareInterface.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace EonX\EasyBatch\Interfaces;
 
+/**
+ * @deprecated since 4.1, will be removed in 5.0. Use EonX\EasyBatch\Interfaces\CurrentBatchObjectsAwareInterface instead.
+ */
 interface CurrentBatchAwareInterface
 {
     public function setCurrentBatch(BatchInterface $batch): void;
