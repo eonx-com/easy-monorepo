78cc4208c2ae0b1013d3b9c4a1d1a2c9da363425
nathanpage <nathan.page@eonx.com>
2022-06-14 09:21:51 +1000

    [EasyBatch] Add more checks around db interactions

---
+++ packages/EasyBatch/tests/Bridge/Symfony/EasyBatchTestContext.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyBatch\Tests\Bridge\Symfony;
+
+use Doctrine\DBAL\Connection;
+use EonX\EasyBatch\Interfaces\BatchItemFactoryInterface;
+use EonX\EasyBatch\Interfaces\BatchItemRepositoryInterface;
+use EonX\EasyBatch\Interfaces\BatchObjectManagerInterface;
+use EonX\EasyBatch\Interfaces\BatchRepositoryInterface;
+use EonX\EasyBatch\Tests\Bridge\Symfony\Stubs\SymfonyEventDispatcherStub;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+final class EasyBatchTestContext
+{
+    public function __construct(private readonly ContainerInterface $container)
+    {
+    }
+
+    public function getBatchItemFactory(): BatchItemFactoryInterface
+    {
+        return $this->container->get(BatchItemFactoryInterface::class);
+    }
+
+    public function getBatchItemRepository(): BatchItemRepositoryInterface
+    {
+        return $this->container->get(BatchItemRepositoryInterface::class);
+    }
+
+    public function getBatchRepository(): BatchRepositoryInterface
+    {
+        return $this->container->get(BatchRepositoryInterface::class);
+    }
+
+    public function getBatchObjectManager(): BatchObjectManagerInterface
+    {
+        return $this->container->get(BatchObjectManagerInterface::class);
+    }
+
+    public function getConnection(): Connection
+    {
+        return $this->container->get(Connection::class);
+    }
+
+    public function getEventDispatcher(): SymfonyEventDispatcherStub
+    {
+        return $this->container->get(EventDispatcherInterface::class);
+    }
+}
