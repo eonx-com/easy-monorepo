95ec2c8289aa2c171065b26e0b796a3d7c8d73e5
Evgeny Ruban <evgeny.ruban@eonx.com>
2022-06-16 15:35:06 +0400

    feature/PBA-632-add-http-client-stubs
    - [EasyTest] Add HttpClient stubs.

---
+++ packages/EasyTest/src/Stub/HttpClient/HttpClientRequestStub.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyTest\Stub\HttpClient;
+
+use Closure;
+use Symfony\Component\HttpClient\Response\MockResponse;
+
+final class HttpClientRequestStub
+{
+    private ?string $hash = null;
+
+    /**
+     * @param mixed[]|null $options
+     */
+    public function __construct(
+        private Closure $addResponseClosure,
+        private string $method,
+        private string $url,
+        private ?array $options = null
+    ) {
+    }
+
+    /**
+     * @param mixed[]|string $data
+     * @param mixed[]|null $info
+     */
+    public function addResponse(array|string $data, ?array $info = null): HttpClientStub
+    {
+        $addResponseClosure = $this->addResponseClosure;
+
+        $body = \is_array($data) ? (string)\json_encode($data) : $data;
+
+        return $addResponseClosure(new MockResponse($body, $info ?? []), $this->getHash());
+    }
+
+    public function getHash(): string
+    {
+        $headers = $this->options['headers'] ?? [];
+        \sort($headers);
+
+        return $this->hash ??= \sha1((string)\json_encode([
+            $this->method,
+            $this->url,
+            $headers,
+            $this->options['body'] ?? null,
+        ]));
+    }
+}
