725d5bf04911d01f7336a2f41532794d71164b0c
Evgeny Ruban <evgeny.ruban@eonx.com>
2022-06-21 06:16:54 +0400

    [EasyCore] Add Integer constraint. (#966)
    * feature/PBA-630-add-integer-constraint
    
    - [EasyCore] Add Integer constraint.
    
    * feature/PBA-630-add-integer-constraint
    
    - [EasyCore] Add Integer constraint.

---
+++ packages/EasyCore/src/Bridge/Symfony/Validator/Constraints/Integer.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyCore\Bridge\Symfony\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD)]
+final class Integer extends Constraint
+{
+    /**
+     * @var string
+     */
+    public const INVALID_INTEGER_ERROR = 'INVALID_INTEGER_ERROR';
+
+    public string $message = 'This value should be of type integer.';
+}
---
+++ packages/EasyCore/src/Bridge/Symfony/Validator/Constraints/IntegerValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyCore\Bridge\Symfony\Validator\Constraints;
+
+use EonX\EasyCore\Bridge\Symfony\Validator\Constraints\Integer as IntegerConstraint;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+final class IntegerValidator extends ConstraintValidator
+{
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        if ($constraint instanceof IntegerConstraint === false) {
+            throw new UnexpectedTypeException($constraint, IntegerConstraint::class);
+        }
+
+        if ($value === null || $value === '') {
+            return;
+        }
+
+        if (\filter_var($value, \FILTER_VALIDATE_INT) === false) {
+            $this->context->buildViolation($constraint->message)
+                ->setCode(IntegerConstraint::INVALID_INTEGER_ERROR)
+                ->addViolation();
+        }
+    }
+}
