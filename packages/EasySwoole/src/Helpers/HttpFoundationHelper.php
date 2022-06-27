04af65d27414a5c19e78d029525fc33b4498f108
Nathan Page <nathan.page@eonx.com>
2022-06-27 13:26:11 +1000

    [EasySwoole] Improve HTTP response handling (#973)
    * [EasySwoole] Improver output/response handling
    
    * [EasySwoole] Improve headers and cookie handling

--- packages/EasySwoole/src/Helpers/HttpFoundationHelper.php
+++ packages/EasySwoole/src/Helpers/HttpFoundationHelper.php
@@ -33,31 +33,79 @@ final class HttpFoundationHelper
         return $hfRequest;
     }
 
-    public static function reflectHttpFoundationResponse(HttpFoundationResponse $hfResponse, Response $response): void
-    {
-        foreach ($hfResponse->headers->all() as $name => $values) {
-            $response->header((string) $name, $values);
-        }
-
+    public static function reflectHttpFoundationResponse(
+        HttpFoundationResponse $hfResponse,
+        Response $response,
+        int $chunkSize,
+        ?string $bufferedOutput = null
+    ): void {
         $response->status($hfResponse->getStatusCode());
 
-        switch (true) {
-            case $hfResponse instanceof BinaryFileResponse && $hfResponse->headers->has('Content-Range'):
-            case $hfResponse instanceof StreamedResponse:
-                \ob_start(static function ($buffer) use ($response) {
-                    $response->write($buffer);
-
-                    return '';
-                }, 4096);
-                $hfResponse->sendContent();
-                \ob_end_clean();
-                $response->end();
-                break;
-            case $hfResponse instanceof BinaryFileResponse:
-                $response->sendfile($hfResponse->getFile()->getPathname());
-                break;
-            default:
-                $response->end($hfResponse->getContent());
+        // Reflect headers, use swoole response methods for cookie
+        foreach ($hfResponse->headers->allPreserveCaseWithoutCookies() as $name => $values) {
+            foreach ($values as $value) {
+                $response->header((string)$name, $value);
+            }
         }
+
+        foreach ($hfResponse->headers->getCookies() as $cookie) {
+            $response->{$cookie->isRaw() ? 'rawcookie' : 'cookie'}(
+                $cookie->getName(),
+                $cookie->getValue(),
+                $cookie->getExpiresTime(),
+                $cookie->getPath(),
+                $cookie->getDomain(),
+                $cookie->isSecure(),
+                $cookie->isHttpOnly()
+            );
+        }
+
+        // Support streamed responses
+        if (($hfResponse instanceof BinaryFileResponse && $hfResponse->headers->has('Content-Range'))
+            || $hfResponse instanceof StreamedResponse) {
+            // Start output buffering, write buffer to swoole response
+            \ob_start(static function ($buffer) use ($response): string {
+                if (\is_string($buffer) && $buffer !== '') {
+                    $response->write($buffer);
+                }
+
+                return '';
+            }, 4096);
+
+            // Send streamed content to buffer
+            $hfResponse->sendContent();
+
+            // Stop output buffering
+            \ob_end_clean();
+
+            // End swoole response
+            $response->end();
+        }
+
+        // Support "simple" binary file response
+        if ($hfResponse instanceof BinaryFileResponse) {
+            $response->sendfile($hfResponse->getFile()->getPathname());
+
+            return;
+        }
+
+        // From here we have a normal response, simply return the content
+        // Prepend buffered output to response content (echo, var_dump, etc)
+        $content = $bufferedOutput . $hfResponse->getContent();
+        $length = \mb_strlen($content);
+
+        // Do not chunk response if not needed
+        if ($length <= $chunkSize) {
+            $response->end($content);
+
+            return;
+        }
+
+        // Otherwise chunk content
+        for ($offset = 0; $offset < $length; $offset += $chunkSize) {
+            $response->write(\substr($content, $offset, $chunkSize));
+        }
+
+        $response->end();
     }
 }
