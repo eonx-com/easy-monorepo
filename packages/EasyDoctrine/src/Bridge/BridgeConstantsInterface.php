af34de2b14157a8f367f42685f8841a9c62983e9
Nathan Page <nathan.page@eonx.com>
2022-06-29 13:46:04 +1000

    [EasyDoctrine] Implement support for AWS RDS IAM auth (#977)
    * [EasyDoctrine] Implement support for AWS RDS IAM auth
    
    * [EasyDoctrine] Fix cs
    
    * [EasyDoctrine] Fix cs - 1
    
    * [EasyDoctrine] Fix cs - 2

--- packages/EasyDoctrine/src/Bridge/BridgeConstantsInterface.php
+++ packages/EasyDoctrine/src/Bridge/BridgeConstantsInterface.php
@@ -9,5 +9,25 @@ interface BridgeConstantsInterface
     /**
      * @var string
      */
+    public const PARAM_AWS_RDS_IAM_REGION = 'easy_doctrine.aws_rds_iam_region';
+
+    /**
+     * @var string
+     */
+    public const PARAM_AWS_RDS_IAM_USERNAME = 'easy_doctrine.aws_rds_iam_username';
+
+    /**
+     * @var string
+     */
+    public const PARAM_AWS_RDS_IAM_CACHE_EXPIRY_IN_SECONDS = 'easy_doctrine.aws_rds_iam_cache_expiry_in_seconds';
+
+    /**
+     * @var string
+     */
     public const PARAM_DEFERRED_DISPATCHER_ENTITIES = 'easy_doctrine.deferred_dispatcher_entities';
+
+    /**
+     * @var string
+     */
+    public const SERVICE_AWS_RDS_IAM_CACHE = 'easy_doctrine.aws_rds_iam_cache';
 }
