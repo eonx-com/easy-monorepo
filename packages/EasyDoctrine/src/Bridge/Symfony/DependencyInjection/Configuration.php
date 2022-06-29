0203539bb284d17818c58abcbba37a86025c3764
Nathan Page <nathan.page@eonx.com>
2022-06-29 15:24:25 +1000

    [EasyDoctrine] Change default AWS RDS IAM username to disabled (#979)

--- packages/EasyDoctrine/src/Bridge/Symfony/DependencyInjection/Configuration.php
+++ packages/EasyDoctrine/src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -20,7 +20,7 @@ final class Configuration implements ConfigurationInterface
                     ->children()
                         ->booleanNode('enabled')->defaultFalse()->end()
                         ->scalarNode('aws_region')->defaultValue('ap-southeast-2')->end()
-                        ->scalarNode('aws_username')->defaultValue('username')->end()
+                        ->scalarNode('aws_username')->defaultValue('disabled')->end()
                         ->integerNode('cache_expiry_in_seconds')->defaultValue(600)->end()
                         ->scalarNode('disabled_username')->defaultValue('disabled')->end()
                     ->end()
