f8ead34d6e04a0cdf2b57dc1ec19877b77a0d581
Nathan Page <nathan.page@eonx.com>
2022-07-13 12:28:41 +1000

    [EasyApiToken] Allow to reset api token decoder factory (#995)

--- packages/EasySecurity/src/Bridge/Symfony/Resources/config/default_configurators.php
+++ packages/EasySecurity/src/Bridge/Symfony/Resources/config/default_configurators.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use EonX\EasyApiToken\Interfaces\Factories\ApiTokenDecoderFactoryInterface;
 use EonX\EasySecurity\Bridge\BridgeConstantsInterface;
 use EonX\EasySecurity\Configurators\ApiTokenConfigurator;
 use EonX\EasySecurity\Interfaces\SecurityContextConfiguratorInterface;
@@ -16,6 +17,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services
         ->set(ApiTokenConfigurator::class)
-        ->arg('$apiTokenDecoder', service(BridgeConstantsInterface::SERVICE_API_TOKEN_DECODER))
+        ->arg('$apiTokenDecoderFactory', service(ApiTokenDecoderFactoryInterface::class))
+        ->arg('$apiTokenDecoder', param(BridgeConstantsInterface::PARAM_TOKEN_DECODER))
         ->arg('$priority', SecurityContextConfiguratorInterface::SYSTEM_PRIORITY);
 };
--- packages/EasySecurity/src/Configurators/ApiTokenConfigurator.php
+++ packages/EasySecurity/src/Configurators/ApiTokenConfigurator.php
@@ -4,26 +4,24 @@ declare(strict_types=1);
 
 namespace EonX\EasySecurity\Configurators;
 
-use EonX\EasyApiToken\Interfaces\ApiTokenDecoderInterface;
+use EonX\EasyApiToken\Interfaces\Factories\ApiTokenDecoderFactoryInterface;
 use EonX\EasySecurity\Interfaces\SecurityContextInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 final class ApiTokenConfigurator extends AbstractSecurityContextConfigurator
 {
-    /**
-     * @var \EonX\EasyApiToken\Interfaces\ApiTokenDecoderInterface
-     */
-    private $apiTokenDecoder;
-
-    public function __construct(ApiTokenDecoderInterface $apiTokenDecoder, ?int $priority = null)
-    {
-        $this->apiTokenDecoder = $apiTokenDecoder;
-
+    public function __construct(
+        private readonly ApiTokenDecoderFactoryInterface $apiTokenDecoderFactory,
+        private readonly ?string $apiTokenDecoder = null,
+        ?int $priority = null
+    ) {
         parent::__construct($priority);
     }
 
     public function configure(SecurityContextInterface $context, Request $request): void
     {
-        $context->setToken($this->apiTokenDecoder->decode($request));
+        $decoder = $this->apiTokenDecoderFactory->build($this->apiTokenDecoder);
+
+        $context->setToken($decoder->decode($request));
     }
 }
