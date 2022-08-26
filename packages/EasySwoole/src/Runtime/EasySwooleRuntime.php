5cd27b5cda217d168d0f6d34ed1aaa587d7e700d
Nathan Page <nathan.page@eonx.com>
2022-08-26 11:43:03 +1000

    [EasySwoole] Allow individual setting to be defined using env variables (#1027)
    * [EasySwoole] Allow individual setting to be defined using env variables
    
    * [EasySwoole] Fix dependencies and phpstan

--- packages/EasySwoole/src/Runtime/EasySwooleRuntime.php
+++ packages/EasySwoole/src/Runtime/EasySwooleRuntime.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\Runtime\RunnerInterface;
 use Symfony\Component\Runtime\SymfonyRuntime;
 
+use function Symfony\Component\String\u;
+
 final class EasySwooleRuntime extends SymfonyRuntime
 {
     /**
@@ -19,7 +21,7 @@ final class EasySwooleRuntime extends SymfonyRuntime
     {
         if ($application instanceof HttpKernelInterface) {
             $options = $this->options;
-            $options['settings'] = \array_merge([
+            $options['settings'] = $this->resolveSwooleSettings(\array_merge([
                 // Process
                 Constant::OPTION_DAEMONIZE => 0,
                 Constant::OPTION_GROUP => 'www-data',
@@ -30,7 +32,7 @@ final class EasySwooleRuntime extends SymfonyRuntime
                 // Processes number
                 Constant::OPTION_REACTOR_NUM => \swoole_cpu_num() * 2,
                 Constant::OPTION_WORKER_NUM => \swoole_cpu_num() * 2,
-            ], $options['settings'] ?? []);
+            ], $options['settings'] ?? []));
 
             // Bridge for eonx-com/easy-bugsnag to resolve request in CLI
             if (\interface_exists(EasyBugsnagValueOptionInterface::class)) {
@@ -41,5 +43,38 @@ final class EasySwooleRuntime extends SymfonyRuntime
         }
 
         return parent::getRunner($application);
+    }
+
+    /**
+     * Allows application to define individual Swoole settings using env variables.
+     * Any option defined on \Swoole\Constant as a constant can be set using an env variable.
+     * Simply replace the OPTION_ from the constant name with SWOOLE_SETTING_ in the env variable name.
+     *
+     * @param mixed[] $settings
+     *
+     * @return mixed[]
+     */
+    private function resolveSwooleSettings(array $settings): array
+    {
+        $reflection = new \ReflectionClass(Constant::class);
+        $constants = $reflection->getConstants();
+
+        foreach ($constants as $constant => $constantValue) {
+            $constantName = u($constant);
+
+            if ($constantName->startsWith('OPTION_')) {
+                $constantName = $constantName
+                    ->replace('OPTION_', 'SWOOLE_SETTING_')
+                    ->toString();
+
+                $value = $_SERVER[$constantName] ?? $_ENV[$constantName] ?? null;
+
+                if ($value !== null && $value !== '') {
+                    $settings[$constantValue] = $value;
+                }
+            }
+        }
+
+        return $settings;
     }
 }
