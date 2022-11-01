cac81eeeacad6e42003b1fab991fbd4fbbc3c6b6
Aleksey Polyvanyi <alexndlm@gmail.com>
2022-11-01 14:19:58 +0100

    Introduce getUserIdentifier method

--- packages/EasySecurity/src/Interfaces/UserInterface.php
+++ packages/EasySecurity/src/Interfaces/UserInterface.php
@@ -8,6 +8,10 @@ interface UserInterface
 {
     /**
      * @return null|int|string
+     * @deprecated Will be removed in 5.0.0
+     *
      */
     public function getUniqueId();
+
+    public function getUserIdentifier(): string;
 }
