be7e659278e88f2cb8b078443310ef1cbeb829a3
dextercampos <dexterjohncampos@gmail.com>
2022-11-02 14:19:52 +0800

    feature/easy-async-doctrine-managers-close-connection-middleware
    - add DoctrineManagersCloseConnectionMiddleware to close entity manager connections every after a message is processed to save number of connections

---
+++ packages/EasyAsync/src/Bridge/Symfony/Messenger/DoctrineManagersCloseConnectionMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyAsync\Bridge\Symfony\Messenger;
+
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
+use Symfony\Component\Messenger\Middleware\StackInterface;
+use Symfony\Component\Messenger\Stamp\ConsumedByWorkerStamp;
+use EonX\EasyAsync\Doctrine\ManagersCloser;
+
+class DoctrineManagersCloseConnectionMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private readonly ManagersCloser $managersCloser,
+        private readonly ?array $managers = null
+    ) {
+    }
+
+    public function handle(Envelope $envelope, StackInterface $stack): Envelope
+    {
+        try {
+            return $stack->next()->handle($envelope, $stack);
+        } finally {
+            if ($envelope->last(ConsumedByWorkerStamp::class) !== null) {
+                $this->managersCloser->close($this->managers);
+            }
+        }
+    }
+}
