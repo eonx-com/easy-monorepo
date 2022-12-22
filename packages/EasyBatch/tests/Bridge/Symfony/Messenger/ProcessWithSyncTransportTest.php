4c696f5299dbfb3bb253dde984831da445a1f121
Aleksey Polyvanyi <alexndlm@gmail.com>
2022-12-22 19:26:14 +0100

    -refactoring

--- packages/EasyBatch/tests/Bridge/Symfony/Messenger/ProcessWithSyncTransportTest.php
+++ packages/EasyBatch/tests/Bridge/Symfony/Messenger/ProcessWithSyncTransportTest.php
@@ -29,7 +29,7 @@ final class ProcessWithSyncTransportTest extends AbstractSymfonyTestCase
         self::assertSame(1, self::getContainer()->get(SyncMessageHandler::class)->getInvokeCount());
         $batchRepository = self::getContainer()->get(BatchRepositoryInterface::class);
         $batchRepository->reset();
-        $batch = $batchRepository->findOrFail($batch->getId());
+        $batch = $batchRepository->findOrFail((string)$batch->getId());
         self::assertSame('succeeded', $batch->getStatus());
     }
 }
