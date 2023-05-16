089574886d532bf483a94e85bd651599bf31dddc
Nathan Page <nathan.page@eonx.com>
2023-05-16 10:38:48 +1000

    [Monorepo] Use GitHub API to create release instead of gh (#1124)
    * Use GitHub API to create release instead of gh
    
    * Fix action version in php-ci.yml

--- monorepo/Release/TagVersionReleaseWorker.php
+++ monorepo/Release/TagVersionReleaseWorker.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace EonX\EasyMonorepo\Release;
 
 use EonX\EasyMonorepo\Git\GitManager;
+use GuzzleHttp\ClientInterface;
 use PharIo\Version\Version;
 use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
 use Symplify\MonorepoBuilder\Release\Process\ProcessRunner;
@@ -12,8 +13,11 @@ use Throwable;
 
 final class TagVersionReleaseWorker implements ReleaseWorkerInterface
 {
-    public function __construct(private GitManager $gitManager, private ProcessRunner $processRunner)
-    {
+    public function __construct(
+        private readonly GitManager $gitManager,
+        private readonly ClientInterface $httpClient,
+        private readonly ProcessRunner $processRunner
+    ) {
         // The body is not required
     }
 
@@ -22,10 +26,15 @@ final class TagVersionReleaseWorker implements ReleaseWorkerInterface
         return sprintf('Add local tag "%s"', $version->getVersionString());
     }
 
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function work(Version $version): void
     {
+        // Commit changes, push them, create tag and push tag
         $cmd = \sprintf(
-            'git add . && git commit -m "Prepare Release %s" && git push',
+            'git add . && git commit -m "Prepare Release %s" && git push && git tag %s && git push --tags',
+            $version->getVersionString(),
             $version->getVersionString()
         );
 
@@ -35,18 +44,24 @@ final class TagVersionReleaseWorker implements ReleaseWorkerInterface
             // nothing to commit
         }
 
-        $currentBranch = $this->gitManager->getCurrentBranch();
-        $notesFilename = \sprintf(__DIR__ . '/../../secret/release_%s.md', $version->getVersionString());
+        // Create Release in GitHub
+        $url = 'https://api.github.com/repos/eonx-com/easy-monorepo/releases';
+        $options = [
+            'headers' => [
+                'accept' => 'application/vnd.github.v3+json',
+                'authorization' => \sprintf('Token %s', \getenv('GITHUB_TOKEN')),
+            ],
+            'body' => \json_encode([
+                'name' => $version->getVersionString(),
+                'draft' => false,
+                'prerelease' => false,
+                'generate_release_notes' => true,
+                'make_latest' => 'legacy',
+                'tag_name' => $version->getVersionString(),
+                'target_commitish' => $this->gitManager->getCurrentBranch(),
+            ]),
+        ];
 
-        // Create Release in Github
-        $cmd = \sprintf(
-            'gh release create %s -F %s -t %s --target %s',
-            $version->getVersionString(),
-            $notesFilename,
-            $version->getVersionString(),
-            $currentBranch
-        );
-
-        $this->processRunner->run($cmd);
+        $this->httpClient->request('POST', $url, $options);
     }
 }
