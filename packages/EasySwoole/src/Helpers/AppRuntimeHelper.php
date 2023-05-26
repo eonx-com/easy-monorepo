3ce634127795bd1371e758b6f300143d4d983d4e
Nathan Page <nathan.page@eonx.com>
2023-05-26 16:39:39 +1000

    [EasySwoole] Improve AppRuntimeHelper with explicit setters for options (#1138)
    * [EasySwoole] Improve AppRuntimeHelper with explicit setters for options
    
    * [EasySwoole] Fix cs

--- packages/EasySwoole/src/Helpers/AppRuntimeHelper.php
+++ packages/EasySwoole/src/Helpers/AppRuntimeHelper.php
@@ -10,11 +10,6 @@ final class AppRuntimeHelper
 
     private const APP_RUNTIME_OPTIONS = 'APP_RUNTIME_OPTIONS';
 
-    public static function setRuntime(string $runtime): void
-    {
-        $_SERVER[self::APP_RUNTIME] = $runtime;
-    }
-
     /**
      * @param mixed[] $options
      */
@@ -24,5 +19,90 @@ final class AppRuntimeHelper
             $_SERVER[self::APP_RUNTIME_OPTIONS] ?? [],
             $options
         );
+    }
+
+    public static function getOption(string $name, mixed $default = null): mixed
+    {
+        return $_SERVER[self::APP_RUNTIME_OPTIONS][$name] ?? $default;
+    }
+
+    /**
+     * @param mixed[] $cacheTables
+     */
+    public static function setCacheTables(array $cacheTables): void
+    {
+        self::addOptions(['cache_tables' => $cacheTables]);
+    }
+
+    /**
+     * @param callable[] $callbacks
+     */
+    public static function setCallbacks(array $callbacks): void
+    {
+        self::addOptions(['callbacks' => $callbacks]);
+    }
+
+    public static function setHost(string $host): void
+    {
+        self::addOptions(['host' => $host]);
+    }
+
+    /**
+     * @param string[] $hotReloadDirs
+     */
+    public static function setHotReloadDirs(array $hotReloadDirs): void
+    {
+        self::addOptions(['hot_reload_dirs' => $hotReloadDirs]);
+    }
+
+    public static function setHotReloadEnabled(bool $hotReloadEnabled): void
+    {
+        self::addOptions(['hot_reload_enabled' => $hotReloadEnabled]);
+    }
+
+    /**
+     * @param string[] $hotReloadExtensions
+     */
+    public static function setHotReloadExtensions(array $hotReloadExtensions): void
+    {
+        self::addOptions(['hot_reload_extensions' => $hotReloadExtensions]);
+    }
+
+    public static function setMode(int $mode): void
+    {
+        self::addOptions(['mode' => $mode]);
+    }
+
+    public static function setPort(int $port): void
+    {
+        self::addOptions(['port' => $port]);
+    }
+
+    public static function setResponseChunkSize(int $responseChunkSize): void
+    {
+        self::addOptions(['response_chunk_size' => $responseChunkSize]);
+    }
+
+    /**
+     * @param mixed[] $settings
+     */
+    public static function setSettings(array $settings): void
+    {
+        self::addOptions(['settings' => $settings]);
+    }
+
+    public static function setSockType(int $sockType): void
+    {
+        self::addOptions(['sock_type' => $sockType]);
+    }
+
+    public static function setUseDefaultCallbacks(bool $useDefaultCallbacks): void
+    {
+        self::addOptions(['use_default_callbacks' => $useDefaultCallbacks]);
+    }
+
+    public static function setRuntime(string $runtime): void
+    {
+        $_SERVER[self::APP_RUNTIME] = $runtime;
     }
 }
