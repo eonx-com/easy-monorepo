67660b3aeb644dc625f66b11441734df12f51ba0
Ivan Torgov <ivan.torgov@eonx.com>
2023-07-03 13:38:17 +0200

    [EasyMonorepo] Added support of `doctrine/dbal` v3. (#1161)

--- packages/EasyBatch/src/Bridge/Symfony/Serializers/MessageSerializerDecorator.php
+++ packages/EasyBatch/src/Bridge/Symfony/Serializers/MessageSerializerDecorator.php
@@ -6,6 +6,7 @@ namespace EonX\EasyBatch\Bridge\Symfony\Serializers;
 
 use EonX\EasyBatch\Interfaces\MessageSerializerInterface;
 use Symfony\Component\Messenger\Exception\HandlerFailedException;
+use Symfony\Component\Messenger\Stamp\AckStamp;
 
 final class MessageSerializerDecorator implements MessageSerializerInterface
 {
@@ -22,9 +23,8 @@ final class MessageSerializerDecorator implements MessageSerializerInterface
     public function serialize(object $message): string
     {
         if ($message instanceof HandlerFailedException) {
-            // @todo Use AckStamp::class when drop support Symfony lower than 5.4
             $envelope = $message->getEnvelope()
-                ->withoutAll('Symfony\Component\Messenger\Stamp\AckStamp');
+                ->withoutAll(AckStamp::class);
             $message = new HandlerFailedException($envelope, $message->getNestedExceptions());
         }
 
