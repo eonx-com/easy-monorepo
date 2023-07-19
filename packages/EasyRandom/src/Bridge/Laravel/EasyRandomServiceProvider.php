9d7f4886a033b36ef9dd76c4fc425d456a86568f
Ivan Torgov <ivan.torgov@eonx.com>
2023-07-19 13:27:02 +0200

    [EasyRandom] Improvements.

--- packages/EasyRandom/src/Bridge/Laravel/EasyRandomServiceProvider.php
+++ packages/EasyRandom/src/Bridge/Laravel/EasyRandomServiceProvider.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace EonX\EasyRandom\Bridge\Laravel;
 
-use EonX\EasyRandom\Bridge\Laravel\Exceptions\UnsupportedUuidVersion;
-use EonX\EasyRandom\Bridge\Laravel\Exceptions\UuidLibraryNotFound;
+use EonX\EasyRandom\Bridge\Laravel\Exceptions\UnsupportedUuidVersionException;
 use EonX\EasyRandom\Bridge\Ramsey\Generators\RamseyUuidV4Generator;
 use EonX\EasyRandom\Bridge\Ramsey\Generators\RamseyUuidV6Generator;
 use EonX\EasyRandom\Bridge\Symfony\Generators\SymfonyUuidV4Generator;
@@ -37,33 +36,29 @@ final class EasyRandomServiceProvider extends ServiceProvider
         $uuidVersion = (int)\config('easy-random.uuid_version', 6);
 
         if (\in_array($uuidVersion, [4, 6], true) === false) {
-            UnsupportedUuidVersion::throw($uuidVersion);
-        }
-
-        if (\class_exists(RamseyUuid::class) === false && \class_exists(SymfonyUuid::class) === false) {
-            UuidLibraryNotFound::throw();
+            throw UnsupportedUuidVersionException::create($uuidVersion);
         }
 
         $this->app->singleton(RandomStringGeneratorInterface::class, RandomStringGenerator::class);
         $this->app->singleton(RandomIntegerGeneratorInterface::class, RandomIntegerGenerator::class);
 
         if ($uuidVersion === 4) {
-            if (\class_exists(RamseyUuid::class)) {
-                $this->app->singleton(UuidGeneratorInterface::class, RamseyUuidV4Generator::class);
+            if (\class_exists(SymfonyUuid::class)) {
+                $this->app->singleton(UuidGeneratorInterface::class, SymfonyUuidV4Generator::class);
             }
 
-            if (\class_exists(RamseyUuid::class) === false) {
-                $this->app->singleton(UuidGeneratorInterface::class, SymfonyUuidV4Generator::class);
+            if (\class_exists(RamseyUuid::class)) {
+                $this->app->singleton(UuidGeneratorInterface::class, RamseyUuidV4Generator::class);
             }
         }
 
         if ($uuidVersion === 6) {
-            if (\class_exists(RamseyUuid::class)) {
-                $this->app->singleton(UuidGeneratorInterface::class, RamseyUuidV6Generator::class);
+            if (\class_exists(SymfonyUuid::class)) {
+                $this->app->singleton(UuidGeneratorInterface::class, SymfonyUuidV6Generator::class);
             }
 
-            if (\class_exists(RamseyUuid::class) === false) {
-                $this->app->singleton(UuidGeneratorInterface::class, SymfonyUuidV6Generator::class);
+            if (\class_exists(RamseyUuid::class)) {
+                $this->app->singleton(UuidGeneratorInterface::class, RamseyUuidV6Generator::class);
             }
         }
 
--- packages/EasyRandom/src/Bridge/Symfony/DependencyInjection/EasyRandomExtension.php
+++ packages/EasyRandom/src/Bridge/Symfony/DependencyInjection/EasyRandomExtension.php
@@ -5,14 +5,18 @@ declare(strict_types=1);
 namespace EonX\EasyRandom\Bridge\Symfony\DependencyInjection;
 
 use EonX\EasyRandom\Bridge\BridgeConstantsInterface;
+use EonX\EasyRandom\Bridge\Ramsey\Generators\RamseyUuidV4Generator;
+use EonX\EasyRandom\Bridge\Ramsey\Generators\RamseyUuidV6Generator;
 use EonX\EasyRandom\Bridge\Symfony\Generators\SymfonyUuidV4Generator;
 use EonX\EasyRandom\Bridge\Symfony\Generators\SymfonyUuidV6Generator;
 use EonX\EasyRandom\Interfaces\UuidGeneratorInterface;
+use Ramsey\Uuid\Uuid as RamseyUuid;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\Uid\Uuid as SymfonyUuid;
 
 final class EasyRandomExtension extends Extension
 {
@@ -43,11 +47,23 @@ final class EasyRandomExtension extends Extension
         $uuidVersion = $container->getParameter(BridgeConstantsInterface::PARAM_UUID_VERSION);
 
         if ($uuidVersion === 4) {
-            $container->setDefinition(UuidGeneratorInterface::class, new Definition(SymfonyUuidV4Generator::class));
+            if (\class_exists(RamseyUuid::class)) {
+                $container->setDefinition(UuidGeneratorInterface::class, new Definition(RamseyUuidV4Generator::class));
+            }
+
+            if (\class_exists(SymfonyUuid::class)) {
+                $container->setDefinition(UuidGeneratorInterface::class, new Definition(SymfonyUuidV4Generator::class));
+            }
         }
 
         if ($uuidVersion === 6) {
-            $container->setDefinition(UuidGeneratorInterface::class, new Definition(SymfonyUuidV6Generator::class));
+            if (\class_exists(RamseyUuid::class)) {
+                $container->setDefinition(UuidGeneratorInterface::class, new Definition(RamseyUuidV6Generator::class));
+            }
+
+            if (\class_exists(SymfonyUuid::class)) {
+                $container->setDefinition(UuidGeneratorInterface::class, new Definition(SymfonyUuidV6Generator::class));
+            }
         }
     }
 }
