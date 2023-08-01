544ed01af1d4f6a09b33347df7e7d62d9969de86
Ivan Torgov <ivan.torgov@eonx.com>
2023-08-01 16:05:52 +0200

    [EasyApiPlatform] Added an additional ReadListener (fixes the issue with non-existent URI IDs in POST requests) (#1217)

--- packages/EasyApiPlatform/src/Bridge/Symfony/Resources/config/services.php
+++ packages/EasyApiPlatform/src/Bridge/Symfony/Resources/config/services.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use EonX\EasyApiPlatform\Bridge\Symfony\Listeners\ReadListener;
 use EonX\EasyApiPlatform\Routing\IriConverter;
 
 return static function (ContainerConfigurator $container): void {
@@ -15,4 +16,14 @@ return static function (ContainerConfigurator $container): void {
     $services->set(IriConverter::class)
         ->decorate('api_platform.iri_converter')
         ->arg('$decorated', service('.inner'));
+
+    $services->set(ReadListener::class)
+        ->arg(
+            '$resourceMetadataCollectionFactory',
+            service('api_platform.metadata.resource.metadata_collection_factory')
+        )
+        ->tag('kernel.event_listener', [
+            'event' => 'kernel.request',
+            'priority' => 4,
+        ]);
 };
