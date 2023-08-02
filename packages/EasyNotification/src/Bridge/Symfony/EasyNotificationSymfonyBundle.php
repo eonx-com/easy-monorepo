44e2b5fb4e76277cd47d3b0ea8535e33d45501f6
Shevelev Vladimir <v.varitweb@gmail.comy>
2023-08-02 16:09:49 +0700

    feature/refactor-easy-notification
    - Refactor EasyNotification

--- packages/EasyNotification/src/Bridge/Symfony/EasyNotificationSymfonyBundle.php
+++ packages/EasyNotification/src/Bridge/Symfony/EasyNotificationSymfonyBundle.php
@@ -3,14 +3,42 @@ declare(strict_types=1);
 
 namespace EonX\EasyNotification\Bridge\Symfony;
 
-use EonX\EasyNotification\Bridge\Symfony\DependencyInjection\EasyNotificationExtension;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use EonX\EasyNotification\Bridge\BridgeConstantsInterface;
+use EonX\EasyNotification\Interfaces\QueueMessageConfiguratorInterface;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-final class EasyNotificationSymfonyBundle extends Bundle
+final class EasyNotificationSymfonyBundle extends AbstractBundle
 {
-    public function getContainerExtension(): ExtensionInterface
+    protected string $extensionAlias = 'easy_notification';
+
+    /**
+ * @var string[]
+ */
+    private static array $configToParam = [
+        'api_url' => BridgeConstantsInterface::PARAM_API_URL,
+        'config_expires_after' => BridgeConstantsInterface::PARAM_CONFIG_CACHE_EXPIRES_AFTER,
+    ];
+
+    public function configure(DefinitionConfigurator $definition): void
     {
-        return new EasyNotificationExtension();
+        $definition->import(__DIR__ . '/Resources/config/definition.php');
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import(__DIR__ . '/Resources/config/services.php');
+
+        $builder
+            ->registerForAutoconfiguration(QueueMessageConfiguratorInterface::class)
+            ->addTag(BridgeConstantsInterface::TAG_QUEUE_MESSAGE_CONFIGURATOR);
+
+        foreach (self::$configToParam as $configName => $param) {
+            $container
+                ->parameters()
+                ->set($param, $config[$configName]);
+        }
     }
 }
