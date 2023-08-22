ba0aea532dfa2bafbfb726f7ec9debe2ffae5ad6
nathanpage <nathan.page@eonx.com>
2023-08-22 15:49:20 +1000

    [EasyDoctrine][EasySwoole] Extend DoctrineBundle ConnectionFactory to support multiple decorators

--- packages/EasyDoctrine/src/Bridge/Symfony/Aws/Rds/AuthTokenConnectionFactory.php
+++ packages/EasyDoctrine/src/Bridge/Symfony/Aws/Rds/AuthTokenConnectionFactory.php
@@ -10,12 +10,16 @@ use Doctrine\DBAL\Connection;
 use EonX\EasyDoctrine\Bridge\AwsRds\AwsRdsConnectionParamsResolver;
 use EonX\EasyDoctrine\Bridge\AwsRds\Drivers\DbalDriver;
 
-final class AuthTokenConnectionFactory
+final class AuthTokenConnectionFactory extends ConnectionFactory
 {
     public function __construct(
         private readonly ConnectionFactory $factory,
         private readonly AwsRdsConnectionParamsResolver $connectionParamsResolver,
     ) {
+        // Call parent constructor with empty values as we only extend the factory from
+        // doctrine bundle as it does not implement an interface allowing us to have multiple decoration
+        // and inject the inner connection in the decorator.
+        parent::__construct([], null);
     }
 
     /**
