3cc4e42ff2ff6c961603104ca7be97cafc804895
Anton Popov <anton.popov@eonx.com>
2023-09-06 23:38:14 +0700

    feature/update-number-to-money-string-in-easy-utils - ecs

--- packages/EasyUtils/src/ValueObjects/Number.php
+++ packages/EasyUtils/src/ValueObjects/Number.php
@@ -31,6 +31,11 @@ final class Number implements Stringable
         $this->precision = $precision ?? self::DEFAULT_PRECISION;
     }
 
+    public function __toString(): string
+    {
+        return $this->round($this->value);
+    }
+
     public static function max(?self ...$values): ?self
     {
         return \array_reduce(
@@ -63,11 +68,6 @@ final class Number implements Stringable
                 return $leftOperand->isLessThan($rightOperand) ? $leftOperand : $rightOperand;
             }
         );
-    }
-
-    public function __toString(): string
-    {
-        return $this->round($this->value);
     }
 
     public function abs(): self
