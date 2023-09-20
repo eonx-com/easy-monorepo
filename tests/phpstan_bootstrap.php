574e26209e5b26d3d16bb0e2c6dd8db136ae1922
Evgeny Ruban <ev.ruban@gmail.com>
2023-09-20 12:06:53 +0400

    [CI] Fix false phpstan error on safe static call (#1278)

--- tests/phpstan_bootstrap.php
+++ tests/phpstan_bootstrap.php
@@ -4,7 +4,3 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
 require_once __DIR__ . '/../vendor/symfony/symfony/src/Symfony/Component/DependencyInjection/Loader/PhpFileLoader.php';
-
-// enables mocking of final classes
-// @see https://tomasvotruba.com/blog/2019/03/28/how-to-mock-final-classes-in-phpunit/
-DG\BypassFinals::enable();
