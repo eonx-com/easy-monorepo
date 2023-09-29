d0a7c13bc7f022674d1d41c401d5570cd304ec94
voodooism <delicatessen.dev@gmail.com>
2023-09-29 14:04:19 +0300

    feature/utils-add-normalizer-object-transformer
    - set priority to normalizer object transformer

--- packages/EasyUtils/src/Bridge/Symfony/Resources/config/sensitive_data_default_object_transformers.php
+++ packages/EasyUtils/src/Bridge/Symfony/Resources/config/sensitive_data_default_object_transformers.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use EonX\EasyUtils\SensitiveData\ObjectTransformers\DefaultObjectTransformer;
+use EonX\EasyUtils\SensitiveData\ObjectTransformers\NormalizerObjectTransformer;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
@@ -14,4 +15,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services
         ->set(DefaultObjectTransformer::class)
         ->arg('$priority', 10000);
+
+    $services
+        ->set(NormalizerObjectTransformer::class)
+        ->arg('$priority', 20000);
 };
