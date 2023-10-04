726c5685ce535618e6515f0a4e75b30f5f466b23
voodooism <delicatessen.dev@gmail.com>
2023-10-04 15:32:51 +0300

    feature/utils-add-normalizer-object-transformer
    - fix ecs

--- packages/EasyUtils/src/Bridge/Symfony/Resources/config/sensitive_data_default_object_transformers.php
+++ packages/EasyUtils/src/Bridge/Symfony/Resources/config/sensitive_data_default_object_transformers.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use EonX\EasyUtils\Bridge\Symfony\SensitiveData\ObjectTransformers\NormalizerObjectTransformer;
 use EonX\EasyUtils\SensitiveData\ObjectTransformers\DefaultObjectTransformer;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
