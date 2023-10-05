b1cb0bbbcb81e6db165dac8afd63f38b14253b28
voodooism <delicatessen.dev@gmail.com>
2023-10-05 15:10:24 +0300

    feature/add-exception-object-transformer
    - introduce exception object transformer

--- packages/EasyUtils/src/Bridge/Symfony/Resources/config/sensitive_data_default_object_transformers.php
+++ packages/EasyUtils/src/Bridge/Symfony/Resources/config/sensitive_data_default_object_transformers.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use EonX\EasyUtils\SensitiveData\ObjectTransformers\DefaultObjectTransformer;
+use EonX\EasyUtils\SensitiveData\ObjectTransformers\ExceptionObjectTransformer;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
     $services->defaults()
         ->autowire()
         ->autoconfigure();
+
+    $services
+        ->set(ExceptionObjectTransformer::class)
+        ->arg('$priority', 100);
 
     $services
         ->set(DefaultObjectTransformer::class)
