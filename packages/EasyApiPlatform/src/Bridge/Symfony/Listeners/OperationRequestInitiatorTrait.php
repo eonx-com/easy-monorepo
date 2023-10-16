52576731f663ab7ea16eb04eccb64330fbf3aea7
Aleksey Polyvanyi <alexndlm@gmail.com>
2023-10-16 08:57:25 +0200

    - fix CI

--- packages/EasyApiPlatform/src/Bridge/Symfony/Listeners/OperationRequestInitiatorTrait.php
+++ packages/EasyApiPlatform/src/Bridge/Symfony/Listeners/OperationRequestInitiatorTrait.php
@@ -6,7 +6,7 @@ namespace EonX\EasyApiPlatform\Bridge\Symfony\Listeners;
 use ApiPlatform\State\Util\OperationRequestInitiatorTrait as NewOperationRequestInitiatorTrait;
 use ApiPlatform\Util\OperationRequestInitiatorTrait as OldOperationRequestInitiatorTrait;
 
-if (trait_exists(OldOperationRequestInitiatorTrait::class)) {
+if (\trait_exists(OldOperationRequestInitiatorTrait::class)) {
     /**
      * @deprecated Remove when ApiPlatform 3.2 is required
      */
@@ -16,7 +16,7 @@ if (trait_exists(OldOperationRequestInitiatorTrait::class)) {
     }
 }
 
-if (trait_exists(NewOperationRequestInitiatorTrait::class)) {
+if (\trait_exists(NewOperationRequestInitiatorTrait::class)) {
     /**
      * @deprecated Remove when ApiPlatform 3.2 is required
      */
