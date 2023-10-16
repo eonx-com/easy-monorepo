7981f2f9d325623afb75452ec722e47142987709
nathanpage <nathan.page@eonx.com>
2023-10-16 12:52:30 +1100

    [EasyPagination] Fix cs - 1

--- packages/EasyPagination/src/Traits/DoctrineCommonPaginatorTrait.php
+++ packages/EasyPagination/src/Traits/DoctrineCommonPaginatorTrait.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 use Doctrine\DBAL\Platforms\SqlitePlatform;
 use Doctrine\DBAL\Query\QueryBuilder as DbalQueryBuilder;
 use Doctrine\ORM\QueryBuilder as OrmQueryBuilder;
+
 use function Symfony\Component\String\u;
 
 trait DoctrineCommonPaginatorTrait
