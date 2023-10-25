29e1df3d432ec2337562c571ea05b18a32c84771
nathanpage <nathan.page@eonx.com>
2023-10-25 11:40:56 +1100

    [EasySwoole] Improve access log to include when request was received and duration

--- packages/EasySwoole/src/AccessLog/HttpFoundationAccessLogFormatter.php
+++ packages/EasySwoole/src/AccessLog/HttpFoundationAccessLogFormatter.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace EonX\EasySwoole\AccessLog;
 
+use Carbon\CarbonImmutable;
+use DateTimeInterface;
 use EonX\EasySwoole\Interfaces\HttpFoundationAccessLogFormatterInterface;
+use EonX\EasySwoole\Interfaces\RequestAttributesInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -11,15 +14,27 @@ final class HttpFoundationAccessLogFormatter implements HttpFoundationAccessLogF
 {
     public function formatAccessLog(Request $request, Response $response): string
     {
+        $durationInMs = '';
+        $startTime = $request->attributes->get(RequestAttributesInterface::EASY_SWOOLE_REQUEST_START_TIME);
+
+        if ($startTime instanceof CarbonImmutable) {
+            $durationInMs = \sprintf(
+                ' - ReceivedAt: %s (%sms)',
+                $startTime->format(DateTimeInterface::RFC3339),
+                $startTime->diffInMilliseconds(CarbonImmutable::now('UTC'))
+            );
+        }
+
         return \sprintf(
-            '%s - "%s %s%s %s" %d "%s"',
+            '%s - "%s %s%s %s" %d "%s"%s',
             $request->getClientIp(),
             $request->getMethod(),
             $request->getPathInfo(),
             $request->getQueryString() ? '?' . $request->getQueryString() : null,
             $request->getProtocolVersion(),
             $response->getStatusCode(),
-            $request->headers->get('user-agent', '<no_user_agent>')
+            $request->headers->get('user-agent', '<no_user_agent>'),
+            $durationInMs
         );
     }
 }
