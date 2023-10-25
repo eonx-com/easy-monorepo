b5868e1e063ca40f572c99404b7c409f439dd96b
Nathan Page <nathan.page@eonx.com>
2023-10-25 20:11:44 +1100

    [EasySwoole] Improve logging in Coroutine PDO implementation (#1305)

--- packages/EasySwoole/src/Bridge/Doctrine/Coroutine/PDO/PDOClientConfig.php
+++ packages/EasySwoole/src/Bridge/Doctrine/Coroutine/PDO/PDOClientConfig.php
@@ -5,13 +5,20 @@ namespace EonX\EasySwoole\Bridge\Doctrine\Coroutine\PDO;
 
 use EonX\EasyDoctrine\Bridge\AwsRds\AwsRdsConnectionParamsResolver;
 use OpenSwoole\Core\Coroutine\Client\ClientConfigInterface;
+use Psr\Log\LoggerInterface;
 
 final class PDOClientConfig implements ClientConfigInterface
 {
     public function __construct(
         private readonly array $params,
         private readonly ?AwsRdsConnectionParamsResolver $connectionParamsResolver = null,
+        private readonly ?LoggerInterface $logger = null,
     ) {
+    }
+
+    public function getLogger(): ?LoggerInterface
+    {
+        return $this->logger;
     }
 
     /**
