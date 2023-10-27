c19852c683a35a25ab796605e222598e62d889cc
Evgeny Ruban <ev.ruban@gmail.com>
2023-10-27 13:36:21 +0400

    feature/add-enums-support-for-role-and-permission-expression-function
    -

--- packages/EasySecurity/tests/Bridge/Symfony/Security/PermissionExpressionFunctionProviderTest.php
+++ packages/EasySecurity/tests/Bridge/Symfony/Security/PermissionExpressionFunctionProviderTest.php
@@ -20,14 +20,6 @@ final class PermissionExpressionFunctionProviderTest extends AbstractTestCase
         self::assertSame(self::PERMISSION_VALID, $function->getEvaluator()([], 'PERMISSION_VALID'));
     }
 
-    public function testPermissionExpressionFunctionNotFoundWithConstant(): void
-    {
-        $this->expectException(PermissionConstantNotFoundException::class);
-
-        $function = (new PermissionExpressionFunctionProvider([self::class]))->getFunctions()[0];
-        $function->getEvaluator()([], 'PERMISSION_INVALID');
-    }
-
     public function testPermissionExpressionFunctionFoundWithEnum(): void
     {
         $function = (new PermissionExpressionFunctionProvider([PermissionEnum::class]))->getFunctions()[0];
@@ -37,6 +29,14 @@ final class PermissionExpressionFunctionProviderTest extends AbstractTestCase
         self::assertSame(PermissionEnum::PermissionValid->value, $function->getEvaluator()([], 'PermissionValid'));
         // Using cached permission
         self::assertSame(PermissionEnum::PermissionValid->value, $function->getEvaluator()([], 'PermissionValid'));
+    }
+
+    public function testPermissionExpressionFunctionNotFoundWithConstant(): void
+    {
+        $this->expectException(PermissionConstantNotFoundException::class);
+
+        $function = (new PermissionExpressionFunctionProvider([self::class]))->getFunctions()[0];
+        $function->getEvaluator()([], 'PERMISSION_INVALID');
     }
 
     public function testPermissionExpressionFunctionNotFoundWithEnum(): void
