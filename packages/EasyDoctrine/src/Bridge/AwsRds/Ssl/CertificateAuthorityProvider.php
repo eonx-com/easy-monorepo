ab7efb830f67b9413e7bd90f214cef9ab81f8fbb
nathanpage <nathan.page@eonx.com>
2023-11-01 14:44:28 +1100

    [EasyDoctrine] Improve logging around AWS RDS SSL

--- packages/EasyDoctrine/src/Bridge/AwsRds/Ssl/CertificateAuthorityProvider.php
+++ packages/EasyDoctrine/src/Bridge/AwsRds/Ssl/CertificateAuthorityProvider.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace EonX\EasyDoctrine\Bridge\AwsRds\Ssl;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
 final class CertificateAuthorityProvider
@@ -16,20 +17,29 @@ final class CertificateAuthorityProvider
 
     public function __construct(
         private readonly string $caPath,
+        private readonly ?LoggerInterface $logger = null,
     ) {
         $this->filesystem = new Filesystem();
     }
 
     public function getCertificateAuthorityPath(): string
     {
+        $this->logger?->debug('Resolving AWS RDS CA path');
+
         if ($this->filesystem->exists($this->caPath) === false) {
+            $this->logger?->debug('CA file does not exist');
+
             $caContents = \file_get_contents(self::RDS_COMBINED_CA_URL);
+
+            $this->logger?->debug('Downloaded RDS CA from public URL');
 
             if (\is_string($caContents) === false || $caContents === '') {
                 throw new CouldNotDownloadRdsCombinedCaException('Could not download RDS Combined CA');
             }
 
             $this->filesystem->dumpFile($this->caPath, $caContents);
+
+            $this->logger?->debug('Created CA file');
         }
 
         return $this->caPath;
