01f7aed5972db9cdc4d611cdd9b3feccb52b15b0
Nathan Page <nathan.page@eonx.com>
2023-11-02 09:55:47 +1100

    [EasyDoctrine] RDS CA cache warmer (#1311)
    * [EasyDoctrine] RDS CA cache warmer
    
    * [EasyDoctrine] Fix cs

--- packages/EasyDoctrine/src/Bridge/Symfony/Resources/config/aws_rds_ssl.php
+++ packages/EasyDoctrine/src/Bridge/Symfony/Resources/config/aws_rds_ssl.php
@@ -5,6 +5,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use EonX\EasyDoctrine\Bridge\AwsRds\Ssl\CertificateAuthorityProvider;
 use EonX\EasyDoctrine\Bridge\BridgeConstantsInterface;
+use EonX\EasyDoctrine\Bridge\Symfony\Aws\Rds\CertificateAuthorityCacheWarmer;
 use Psr\Log\LoggerInterface;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -18,4 +19,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->set(CertificateAuthorityProvider::class)
         ->arg('$caPath', param(BridgeConstantsInterface::PARAM_AWS_RDS_SSL_CA_PATH))
         ->arg('$logger', service(LoggerInterface::class)->nullOnInvalid());
+
+    $services->set(CertificateAuthorityCacheWarmer::class);
 };
