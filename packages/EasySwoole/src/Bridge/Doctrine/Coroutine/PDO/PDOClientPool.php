1deaa4faca4ad21342c43dc29c7afea98ef3d1ed
Nathan Page <nathan.page@eonx.com>
2023-12-04 13:22:33 +1100

    [EasySwoole] Create our own implementation of PDOClientPool (#1332)
    * [EasySwoole] Create our own implementation of PDOClientPool
    
    * [EasySwoole] Fix cs
    
    * [EasySwoole] Fix cs - 1
    
    * [EasySwoole] Push split packages to use in AHI
    
    * [EasySwoole] Revert composer.json to prepare for release
    
    * Fix split_packages.yml
    
    * [EasySwoole] Fixed the DbalConnection's constructor.
    
    ---------
    
    Co-authored-by: Ivan Torgov <ivan.torgov@eonx.com>

--- packages/EasySwoole/src/Bridge/Doctrine/Coroutine/PDO/PDOClientPool.php
+++ packages/EasySwoole/src/Bridge/Doctrine/Coroutine/PDO/PDOClientPool.php
@@ -4,107 +4,100 @@ declare(strict_types=1);
 namespace EonX\EasySwoole\Bridge\Doctrine\Coroutine\PDO;
 
 use co;
-use OpenSwoole\Core\Coroutine\Pool\ClientPool;
 use OpenSwoole\Coroutine;
-use ReflectionClass;
+use OpenSwoole\Coroutine\Channel;
 use Throwable;
-use UnexpectedValueException;
 
-final class PDOClientPool extends ClientPool
+final class PDOClientPool
 {
-    /**
-     * @var \ReflectionClass<\OpenSwoole\Core\Coroutine\Pool\ClientPool>|null
-     */
-    private ?ReflectionClass $parentReflection = null;
+    private int $connectionCount = 0;
+
+    private Channel $pool;
 
     public function __construct(
-        PDOClientFactory $factory,
-        PDOClientConfig $config,
-        int $size,
+        private readonly PDOClientFactory $factory,
+        private readonly PDOClientConfig $config,
+        private readonly int $size,
         bool $heartbeat,
         private readonly float $maxIdleTime,
     ) {
-        parent::__construct($factory, $config, $size, $heartbeat);
+        $this->pool = new Channel($this->size);
+
+        if ($heartbeat) {
+            $this->heartbeat();
+        }
     }
 
-    /**
-     * @throws \ReflectionException
-     */
-    protected function heartbeat(): void
+    public function get(): PDOClient
     {
-        $poolProperty = $this->getParentReflectionClass()
-            ->getProperty('pool');
+        $pdo = null;
 
-        /** @var \OpenSwoole\Coroutine\Channel $pool */
-        $pool = $poolProperty->getValue($this);
+        // If no available connection in the pool, and not reached maximum size, then create new PDOClient
+        if ($this->pool->isEmpty() && $this->connectionCount < $this->size) {
+            $pdo = $this->factory::make($this->config);
 
-        Coroutine::create(function () use ($pool): void {
+            // Increment count of connections only after make(), so we are sure we have successfully created
+            // the new PDOClient
+            $this->connectionCount++;
+        }
+
+        // If we just created a new PDOClient return it, otherwise get one from the pool
+        return $pdo ?? $this->pool->pop();
+    }
+
+    public function put(PDOClient $client): void
+    {
+        $this->pool->push($client);
+    }
+
+    private function heartbeat(): void
+    {
+        Coroutine::create(function (): void {
             while (true) {
+                // Trigger every 3 seconds only to save CPU usage
                 co::sleep(3);
 
-                if ($pool->isEmpty()) {
+                // If pool is empty, then no need to check
+                if ($this->pool->isEmpty()) {
                     continue;
                 }
 
-                /** @var \EonX\EasySwoole\Bridge\Doctrine\Coroutine\PDO\PDOClient $client */
-                $client = $this->get();
+                $pdo = $this->get();
 
-                $shouldClose = $client->getLastUsedTime() !== null
-                    && $client->getLastUsedTime() + $this->maxIdleTime < \microtime(true);
+                // If PDOClient was never used, trigger fake usage, so it gets recycled in next idle cycle
+                if ($pdo->getLastUsedTime() === null) {
+                    $pdo->triggerLastUsedTime();
 
-                if ($shouldClose) {
-                    unset($client);
+                    $this->put($pdo);
 
                     continue;
                 }
 
-                $client->heartbeat();
-                $this->put($client);
+                // If PDOClient was idle for too long, close the connection
+                if ($pdo->getLastUsedTime() + $this->maxIdleTime < \microtime(true)) {
+                    unset($pdo);
+
+                    // Decrease the pool connections count
+                    $this->connectionCount--;
+
+                    continue;
+                }
+
+                try {
+                    $pdo->heartbeat();
+                } catch (Throwable) {
+                    // If the PDOClient is compromised, close the connection
+                    unset($pdo);
+
+                    // Decrease the pool connections count
+                    $this->connectionCount--;
+
+                    continue;
+                }
+
+                // Otherwise put PDOClient back in the pool
+                $this->put($pdo);
             }
         });
-    }
-
-    /**
-     * @throws \ReflectionException
-     * @throws \Throwable
-     */
-    protected function make(): void
-    {
-        $numProperty = $this->getParentReflectionClass()
-            ->getProperty('num');
-
-        $originalNumValue = $numProperty->getValue($this);
-
-        try {
-            parent::make();
-        } catch (Throwable $throwable) {
-            $newNumValue = $numProperty->getValue($this);
-
-            // If num value was increased and not decreased again because of the exception
-            // then decrease it to keep the pool state correct, looks like a bug in openswoole
-            if ($newNumValue > 0 && $newNumValue > $originalNumValue) {
-                $numProperty->setValue($newNumValue - 1);
-            }
-
-            throw $throwable;
-        }
-    }
-
-    /**
-     * @return \ReflectionClass<\OpenSwoole\Core\Coroutine\Pool\ClientPool>
-     */
-    private function getParentReflectionClass(): ReflectionClass
-    {
-        if ($this->parentReflection !== null) {
-            return $this->parentReflection;
-        }
-
-        $reflectionParentClass = (new ReflectionClass($this))->getParentClass();
-
-        if ($reflectionParentClass === false) {
-            throw new UnexpectedValueException('Unable to get the parent class.');
-        }
-
-        return $this->parentReflection = $reflectionParentClass;
     }
 }
