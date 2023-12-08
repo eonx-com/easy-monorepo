0c9ff35b3db53e1fe0fc70d98b872c9549e81253
Aleksey Polyvanyi <alexndlm@gmail.com>
2023-12-08 10:38:42 +0100

    [EasyApiPlatform] Fix CarbonImmutableNormalizerTest (#1329)

--- packages/EasyApiPlatform/tests/Normalizers/CarbonImmutableNormalizerTest.php
+++ packages/EasyApiPlatform/tests/Normalizers/CarbonImmutableNormalizerTest.php
@@ -14,7 +14,7 @@ final class CarbonImmutableNormalizerTest extends AbstractTestCase
         $data = '2021-12-16T17:00:00+07:00';
         $normalizer = new CarbonImmutableNormalizer();
 
-        $result = $normalizer->denormalize($data, 'some-json');
+        $result = $normalizer->denormalize($data, CarbonImmutable::class);
 
         self::assertEquals(new CarbonImmutable($data), $result);
     }
