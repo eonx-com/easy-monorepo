d91be17281e9858ada92b02e4bf68ee8a56c935d
voodooism <delicatessen.dev@gmail.com>
2024-04-03 13:49:49 +0300

    [EasyUtils] Add curl cert opts to sanitizer

--- packages/EasyUtils/src/SensitiveData/SensitiveDataSanitizerInterface.php
+++ packages/EasyUtils/src/SensitiveData/SensitiveDataSanitizerInterface.php
@@ -5,6 +5,12 @@ namespace EonX\EasyUtils\SensitiveData;
 
 interface SensitiveDataSanitizerInterface
 {
+    public const CURLOPT_CAINFO_BLOB = 40309;
+
+    public const CURLOPT_SSLCERT_BLOB = 40291;
+
+    public const CURLOPT_SSLKEY_BLOB = 40292;
+
     public const DEFAULT_KEYS_TO_MASK = [
         'access_key',
         'access_secret',
@@ -27,6 +33,9 @@ interface SensitiveDataSanitizerInterface
         'token',
         'verificationcode',
         'x-shared-key',
+        self::CURLOPT_CAINFO_BLOB,
+        self::CURLOPT_SSLCERT_BLOB,
+        self::CURLOPT_SSLKEY_BLOB,
     ];
 
     public const DEFAULT_MASK_PATTERN = '*REDACTED*';
