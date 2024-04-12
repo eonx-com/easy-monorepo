807b89ff89fc714ffe7ed97253b8825d673052ea
voodooism <delicatessen.dev@gmail.com>
2024-04-12 14:05:32 +0300

    - delete unused constants

--- packages/EasyUtils/src/Bridge/Symfony/EasyUtilsSymfonyBundle.php
+++ packages/EasyUtils/src/Bridge/Symfony/EasyUtilsSymfonyBundle.php
@@ -21,35 +21,6 @@ final class EasyUtilsSymfonyBundle extends AbstractBundle
         'scale' => BridgeConstantsInterface::PARAM_MATH_SCALE,
     ];
 
-    public const SENSITIVE_DATA_DEFAULT_KEYS_TO_MASK = [
-        'access_key',
-        'access_secret',
-        'access_token',
-        'apikey',
-        'auth_basic',
-        'auth_bearer',
-        'authorization',
-        'card_number',
-        'cert',
-        'cvc',
-        'cvv',
-        'number',
-        'password',
-        'php-auth-pw',
-        'php_auth_pw',
-        'php-auth-user',
-        'php_auth_user',
-        'securitycode',
-        'token',
-        'verificationcode',
-        'x-shared-key',
-        '40309', // Value of the CURLOPT_CAINFO_BLOB constant
-        '40291', // Value of the CURLOPT_SSLCERT_BLOB constant
-        '40292', // Value of the CURLOPT_SSLKEY_BLOB constant
-    ];
-
-    public const SENSITIVE_DATA_DEFAULT_MASK_PATTERN = '*REDACTED*';
-
     private const SENSITIVE_DATA_CONFIG = [
         'keys_to_mask' => BridgeConstantsInterface::PARAM_SENSITIVE_DATA_KEYS_TO_MASK,
     ];
