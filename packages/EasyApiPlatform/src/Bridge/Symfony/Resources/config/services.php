2db4243034f07d053e6102af8d3b17b627cc2581
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-05-30 16:39:12 +0200

    feature/easy-api-platform-remove-deprecations

--- packages/EasyApiPlatform/src/Bridge/Symfony/Resources/config/services.php
+++ packages/EasyApiPlatform/src/Bridge/Symfony/Resources/config/services.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use EonX\EasyApiPlatform\Bridge\Symfony\Listeners\ReadListener;
-use EonX\EasyApiPlatform\Routing\IriConverter;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
@@ -12,10 +11,6 @@ return static function (ContainerConfigurator $container): void {
     $services->defaults()
         ->autowire()
         ->autoconfigure();
-
-    $services->set(IriConverter::class)
-        ->decorate('api_platform.iri_converter')
-        ->arg('$decorated', service('.inner'));
 
     $services->set(ReadListener::class)
         ->arg(
