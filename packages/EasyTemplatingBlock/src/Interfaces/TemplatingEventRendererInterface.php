662408e861ba0bb5e23a18ccac09cd8bc8c1070c
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-06-23 14:03:14 +0200

    feature/MTCE-240-Monorepo-v6-refactoring-EasyTemplatingBlock

--- packages/EasyTemplatingBlock/src/Interfaces/TemplatingEventRendererInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace EonX\EasyTemplatingBlock\Interfaces;
-
-interface TemplatingEventRendererInterface
-{
-    public function renderEvent(string $event, ?array $context = null): string;
-}
