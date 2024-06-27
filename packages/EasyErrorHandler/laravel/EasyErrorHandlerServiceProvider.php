569e47b82e8301d2f219da09e6a19b5c1f209ab1
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-06-27 15:52:30 +0200

    -refactoring

--- packages/EasyErrorHandler/laravel/EasyErrorHandlerServiceProvider.php
+++ packages/EasyErrorHandler/laravel/EasyErrorHandlerServiceProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace EonX\EasyErrorHandler\Laravel;
 
 use Bugsnag\Client;
-use EonX\EasyBugsnag\Bundle\Enum\ConfigTag;
+use EonX\EasyBugsnag\Bundle\Enum\ConfigTag as EasyBugsnagConfigTag;
 use EonX\EasyErrorHandler\Bugsnag\Configurator\ErrorDetailsClientConfigurator;
 use EonX\EasyErrorHandler\Bugsnag\Configurator\SeverityClientConfigurator;
 use EonX\EasyErrorHandler\Bugsnag\Configurator\UnhandledClientConfigurator;
@@ -184,7 +184,7 @@ final class EasyErrorHandlerServiceProvider extends ServiceProvider
 
             foreach (self::BUGSNAG_CONFIGURATORS as $configurator) {
                 $this->app->singleton($configurator);
-                $this->app->tag($configurator, [ConfigTag::ClientConfigurator->value]);
+                $this->app->tag($configurator, [EasyBugsnagConfigTag::ClientConfigurator->value]);
             }
 
             $this->app->singleton(
