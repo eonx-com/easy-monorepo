467cf098ff300e83c785af0edd422449a3ad9415
Ivan Torgov <ivan.torgov@eonx.com>
2024-06-28 16:16:44 +1000

    [EasyEncryption] Fix an issue in the `AwsCloudHsmSdkConfigurator` service (#1458)
    * [EasyEncryption] Make the `--server-client-cert-file` and the `--server-client-key-file` options optional in the `AwsCloudHsmSdkConfigurator` service.
    
    * [EasyEncryption] Resolve code review concerns.

--- packages/EasyEncryption/src/Configurator/AwsCloudHsmSdkConfigurator.php
+++ packages/EasyEncryption/src/Configurator/AwsCloudHsmSdkConfigurator.php
@@ -65,16 +65,20 @@ final class AwsCloudHsmSdkConfigurator
     private function configureAwsCloudHsmSdkUsingAwsSdk(array $options): void
     {
         $cluster = [
-            'client_cert_path' => $options['--server-client-cert-file'],
-            'client_key_path' => $options['--server-client-key-file'],
             'hsm_ca_file' => $options['--hsm-ca-cert'],
             'options' => [
                 'disable_key_availability_check' => $options['--disable-key-availability-check'],
             ],
         ];
+
+        if (isset($options['--server-client-cert-file']) && isset($options['--server-client-key-file'])) {
+            $cluster['client_cert_path'] = $options['--server-client-cert-file'];
+            $cluster['client_key_path'] = $options['--server-client-key-file'];
+        }
+
         $servers = [];
 
-        if (\array_key_exists('-a', $options)) {
+        if (isset($options['-a'])) {
             $hsmIpAddresses = \explode(' ', (string)$options['-a']);
 
             foreach ($hsmIpAddresses as $hsmIpAddress) {
@@ -86,7 +90,7 @@ final class AwsCloudHsmSdkConfigurator
             }
         }
 
-        if (\array_key_exists('--cluster-id', $options)) {
+        if (isset($options['--cluster-id'])) {
             $awsCredentials = null;
             if ($this->awsRoleArn !== null) {
                 $stsClient = new StsClient([
