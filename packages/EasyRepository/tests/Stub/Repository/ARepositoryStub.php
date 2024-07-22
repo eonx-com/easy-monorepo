ef2af8d9a7815e2b60c57bd6487903a09725746e
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-07-22 10:51:31 +0200

    -refactoring

--- packages/EasyRepository/tests/Stub/Repository/ARepositoryStub.php
+++ packages/EasyRepository/tests/Stub/Repository/ARepositoryStub.php
@@ -3,6 +3,6 @@ declare(strict_types=1);
 
 namespace EonX\EasyRepository\Tests\Stub\Repository;
 
-final class RepositoryBStub
+final class ARepositoryStub
 {
 }
--- packages/EasyRepository/tests/Unit/laravel/EasyRepositoryServiceProviderTest.php
+++ packages/EasyRepository/tests/Unit/laravel/EasyRepositoryServiceProviderTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 namespace EonX\EasyRepository\Tests\Unit\Laravel;
 
 use EonX\EasyRepository\Laravel\EasyRepositoryServiceProvider;
-use EonX\EasyRepository\Tests\Stub\Repository\RepositoryAStub;
-use EonX\EasyRepository\Tests\Stub\Repository\RepositoryBStub;
+use EonX\EasyRepository\Tests\Stub\Repository\ARepositoryStub;
+use EonX\EasyRepository\Tests\Stub\Repository\BRepositoryStub;
 use LogicException;
 
 final class EasyRepositoryServiceProviderTest extends AbstractLumenTestCase
@@ -26,15 +26,15 @@ final class EasyRepositoryServiceProviderTest extends AbstractLumenTestCase
         $app = $this->getApplication();
         \config()
             ->set('easy-repository.repositories', [
-                'interface-1' => RepositoryAStub::class,
-                'interface-2' => RepositoryBStub::class,
+                'interface-1' => ARepositoryStub::class,
+                'interface-2' => BRepositoryStub::class,
             ]);
 
         $provider = new EasyRepositoryServiceProvider($app);
         $provider->boot();
         $provider->register();
 
-        $this->assertInstanceInApp(RepositoryAStub::class, 'interface-1');
-        $this->assertInstanceInApp(RepositoryBStub::class, 'interface-2');
+        $this->assertInstanceInApp(ARepositoryStub::class, 'interface-1');
+        $this->assertInstanceInApp(BRepositoryStub::class, 'interface-2');
     }
 }
