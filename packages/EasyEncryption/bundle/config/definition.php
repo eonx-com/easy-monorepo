de8c60f1fa5a2cd013d6224d1ef8a472692f77d2
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-07-29 11:36:48 +0200

    -refactoring

--- packages/EasyEncryption/bundle/config/definition.php
+++ packages/EasyEncryption/bundle/config/definition.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
 
-use EonX\EasyEncryption\Common\Encryptor\EncryptorInterface;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 
 return static function (DefinitionConfigurator $definition) {
