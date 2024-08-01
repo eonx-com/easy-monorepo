fe33d94fc0e4d4d53c57acf600ec9c3ed9c46769
Shevelev Vladimir <vladimir.shevelev@eonx.com>
2024-08-01 13:40:49 +0700

    feature/PBAO-243-Move-all-encryption-infrastructure-classes-into-EasyEncryption
    - Add default implementation for EncryptorInterface and HashCalculatorInterface

--- packages/EasyEncryption/src/Bridge/Symfony/Resources/config/services.php
+++ packages/EasyEncryption/src/Bridge/Symfony/Resources/config/services.php
@@ -6,7 +6,10 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use EonX\EasyEncryption\Bridge\BridgeConstantsInterface;
 use EonX\EasyEncryption\Encryptor;
 use EonX\EasyEncryption\Encryptors\ObjectEncryptor;
+use EonX\EasyEncryption\Encryptors\StringEncryptor;
 use EonX\EasyEncryption\Factories\DefaultEncryptionKeyFactory;
+use EonX\EasyEncryption\HashCalculators\HashCalculatorInterface;
+use EonX\EasyEncryption\HashCalculators\HmacSha512HashCalculator;
 use EonX\EasyEncryption\Interfaces\EncryptionKeyFactoryInterface;
 use EonX\EasyEncryption\Interfaces\EncryptionKeyProviderInterface;
 use EonX\EasyEncryption\Interfaces\EncryptorInterface;
@@ -32,6 +35,13 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services
         ->set(EncryptorInterface::class, Encryptor::class)
         ->arg('$defaultKeyName', param(BridgeConstantsInterface::PARAM_DEFAULT_KEY_NAME));
+
+    $services->set(HashCalculatorInterface::class, HmacSha512HashCalculator::class);
+
+    $services->set(StringEncryptor::class)
+        ->arg('$encryptor', service(Encryptor::class))
+        ->arg('$encryptionKeyName', param(BridgeConstantsInterface::PARAM_DEFAULT_KEY_NAME))
+        ->arg('$maxChunkSize', param(BridgeConstantsInterface::PARAM_MAX_CHUNK_SIZE));
 
     $services->set(Encryptor::class);
     $services->set(EncryptableMetadata::class);
