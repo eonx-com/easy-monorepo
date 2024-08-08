6c85e96ac64b85963b0ff27c40c0ffaf18e68804
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-08-08 14:32:08 +0200

    -fix

--- packages/EasySecurity/src/SymfonySecurity/Entity/FakeUser.php
+++ packages/EasySecurity/src/SymfonySecurity/Entity/FakeUser.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface as SymfonyUserInterface;
  */
 final class FakeUser implements UserInterface, SymfonyUserInterface
 {
-    privates const ID_USERNAME = 'easy_security.fake_user';
+    private const ID_USERNAME = 'easy_security.fake_user';
 
     public function eraseCredentials(): void
     {
