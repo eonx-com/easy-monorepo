96e2cf59a0959ffc7aceb5a4dced91f0cc04a120
Alexandr Tsepelev <alexandr.tsepelev@eonx.com>
2024-08-13 18:13:55 +0500

    fix

--- packages/EasyAsync/src/Bridge/Laravel/Providers/EasyAsyncServiceProvider.php
+++ packages/EasyAsync/src/Bridge/Laravel/Providers/EasyAsyncServiceProvider.php
@@ -55,22 +55,10 @@ final class EasyAsyncServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/easy-async.php', 'easy-async');
 
-        $this->clearDoctrineEmBeforeJob();
         $this->logQueueWorkerStopping();
         $this->registerAsyncLogger();
         $this->registerEasyErrorHandlerBridge();
         $this->registerQueueListeners();
-        $this->restartQueueOnEmClose();
-    }
-
-    private function clearDoctrineEmBeforeJob(): void
-    {
-        if ((bool)\config('easy-async.clear_doctrine_em_before_job', false) === false) {
-            return;
-        }
-
-        $this->app->get('events')
-            ->listen(JobProcessing::class, DoctrineManagersClearListener::class);
     }
 
     private function logQueueWorkerStopping(): void
@@ -147,15 +135,5 @@ final class EasyAsyncServiceProvider extends ServiceProvider
                 $app->make(BridgeConstantsInterface::SERVICE_LOGGER)
             )
         );
-    }
-
-    private function restartQueueOnEmClose(): void
-    {
-        if ((bool)\config('easy-async.restart_queue_on_doctrine_em_close', true) === false) {
-            return;
-        }
-
-        $this->app->get('events')
-            ->listen(JobExceptionOccurred::class, DoctrineManagersSanityCheckListener::class);
     }
 }
