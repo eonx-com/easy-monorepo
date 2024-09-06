0408dff97b81946f254b54ef7625b72f9ddc2835
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-09-06 12:21:44 +0200

    feature/easy-doctrine-add-AsDoctrineListener-attribute-to-FixPostgreSqlDefaultSchemaListener

--- packages/EasyDoctrine/src/Common/Listener/FixPostgreSqlDefaultSchemaListener.php
+++ packages/EasyDoctrine/src/Common/Listener/FixPostgreSqlDefaultSchemaListener.php
@@ -3,14 +3,17 @@ declare(strict_types=1);
 
 namespace EonX\EasyDoctrine\Common\Listener;
 
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\DBAL\Schema\PostgreSQLSchemaManager;
 use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+use Doctrine\ORM\Tools\ToolEvents;
 
 /**
  * Workaround for an issue: https://github.com/doctrine/dbal/issues/1110.
  *
  * @author https://github.com/vudaltsov
  */
+#[AsDoctrineListener(event: ToolEvents::postGenerateSchema)]
 final class FixPostgreSqlDefaultSchemaListener
 {
     /**
