1c34f7b8b7b7cdbb5e16a9b25fc5a807ba8d125e
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-09-23 13:01:55 +0200

    -refactoring

--- packages/EasyApiPlatform/bundle/config/state_provider.php
+++ packages/EasyApiPlatform/bundle/config/state_provider.php
@@ -13,6 +13,6 @@ return static function (ContainerConfigurator $container): void {
         ->autoconfigure();
 
     $services->set(ReadStateProvider::class)
-        ->decorate('api_platform.state_provider.read', null, 1)
+        ->decorate('api_platform.state_provider.read', priority: 1)
         ->arg('$decorated', service('.inner'));
 };
