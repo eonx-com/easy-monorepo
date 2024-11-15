d3f25a7991a1c03f6f1406980be933875a524855
Evgeny Ruban <ev.ruban@gmail.com>
2024-11-15 17:23:50 +0400

    [EasyErrorHandler] Fix not found translations when LoggingTranslator is used (#1601)

--- packages/EasyErrorHandler/src/Common/Translator/Translator.php
+++ packages/EasyErrorHandler/src/Common/Translator/Translator.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace EonX\EasyErrorHandler\Common\Translator;
 
+use Symfony\Component\Translation\TranslatorBagInterface;
 use Symfony\Contracts\Translation\TranslatorInterface as SymfonyTranslatorInterface;
 
 final readonly class Translator implements TranslatorInterface
@@ -10,17 +11,20 @@ final readonly class Translator implements TranslatorInterface
     private const DEFAULT_DOMAIN = 'EasyErrorHandlerBundle';
 
     public function __construct(
-        private SymfonyTranslatorInterface $decorated,
+        // @todo: Rename to `$translator` in next major release (7.0) as it's not a decoration
+        private SymfonyTranslatorInterface&TranslatorBagInterface $decorated,
         private ?string $domain = null,
     ) {
     }
 
     public function trans(string $message, array $parameters, ?string $locale = null): string
     {
-        $translation = $this->decorated->trans($message, $parameters, $this->domain, $locale);
-
-        if ($translation !== $message) {
-            return $translation;
+        $catalogue = $this->decorated->getCatalogue();
+        if (
+            $catalogue->has($message, self::DEFAULT_DOMAIN) === false
+            || $catalogue->has($message, $this->domain ?? 'messages')
+        ) {
+            return $this->decorated->trans($message, $parameters, $this->domain, $locale);
         }
 
         return $this->decorated->trans($message, $parameters, self::DEFAULT_DOMAIN, $locale);
