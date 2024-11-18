d35d0cdda0b7d951559767bf747014edf72449a8
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-11-18 17:23:44 +0100

    -refactoring

--- packages/EasyErrorHandler/bundle/translations/EasyErrorHandlerBundle.en.php
+++ packages/EasyErrorHandler/bundle/translations/EasyErrorHandlerBundle.en.php
@@ -16,11 +16,11 @@ return [
     'violations' => [
         // @todo Remove `another_iri` in 7.0
         'another_iri' => 'This value should be %iri% IRI.',
-        'invalid_type' => 'This value should be of type %expected_types%.',
         // @todo Remove `missing_constructor_argument` in 7.0
         'missing_constructor_argument' => 'This value should be present.',
         'invalid_datetime' => 'This value is not a valid date/time.',
         'invalid_iri' => 'This value should be an IRI.',
+        'invalid_type' => 'This value should be of type %expected_types%.',
         'not_encodable' => 'The input data is misformatted.',
     ],
 ];
