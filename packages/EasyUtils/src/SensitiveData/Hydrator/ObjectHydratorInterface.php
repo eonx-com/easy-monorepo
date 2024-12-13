3b0ddbbbde45b3f686d0a966ec4f4c7211cc73ff
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-12-13 10:15:05 +0100

    [EasyUtils] Add generics to SensitiveDataSanitizerInterface (#1616)

--- packages/EasyUtils/src/SensitiveData/Hydrator/ObjectHydratorInterface.php
+++ packages/EasyUtils/src/SensitiveData/Hydrator/ObjectHydratorInterface.php
@@ -5,5 +5,12 @@ namespace EonX\EasyUtils\SensitiveData\Hydrator;
 
 interface ObjectHydratorInterface
 {
+    /**
+     * @template T of object
+     *
+     * @param T $object
+     *
+     * @return T
+     */
     public function hydrate(object $object, array $sanitizedData): object;
 }
--- packages/EasyUtils/src/SensitiveData/Sanitizer/SensitiveDataSanitizerInterface.php
+++ packages/EasyUtils/src/SensitiveData/Sanitizer/SensitiveDataSanitizerInterface.php
@@ -5,5 +5,12 @@ namespace EonX\EasyUtils\SensitiveData\Sanitizer;
 
 interface SensitiveDataSanitizerInterface
 {
+    /**
+     * @template T
+     *
+     * @param T $data
+     *
+     * @return (T is object ? T|array : (T is array ? array : (T is string ? string : T)))
+     */
     public function sanitize(mixed $data): mixed;
 }
