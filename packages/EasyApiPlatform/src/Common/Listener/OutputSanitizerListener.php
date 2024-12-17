83c987b4b87e8bb9f8402f6ee4f4fe604968f24a
Evgeny Ruban <ev.ruban@gmail.com>
2024-12-17 17:17:08 +0400

    feature/PBAO-436-add-OutputSanitizerListener
    -

---
+++ packages/EasyApiPlatform/src/Common/Listener/OutputSanitizerListener.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyApiPlatform\Common\Listener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+#[AsEventListener]
+final class OutputSanitizerListener
+{
+    public function __invoke(ResponseEvent $event): void
+    {
+        if ($event->isMainRequest() === false) {
+            return;
+        }
+
+        $response = $event->getResponse();
+
+        if (\str_contains((string)$response->headers->get('Content-Type'), 'application/json') === false) {
+            return;
+        }
+
+        $content = $response->getContent();
+
+        if ($content === false) {
+            return;
+        }
+
+        $response->setContent(\htmlspecialchars($content, \ENT_QUOTES));
+    }
+}
