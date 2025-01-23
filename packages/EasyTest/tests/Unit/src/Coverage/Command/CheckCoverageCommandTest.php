f6e954d9afea917cc12cc0dd1cab25b3388579e1
Aleksey Polyvanyi <alexndlm@gmail.com>
2025-01-23 12:14:01 +0100

    -fix tests

--- packages/EasyTest/tests/Unit/src/Coverage/Command/CheckCoverageCommandTest.php
+++ packages/EasyTest/tests/Unit/src/Coverage/Command/CheckCoverageCommandTest.php
@@ -36,9 +36,15 @@ final class CheckCoverageCommandTest extends AbstractUnitTestCase
                 'file' => __DIR__ . '/../../../../Fixture/Coverage/coverage_70.clover',
                 '--coverage' => 71,
             ],
-            <<<TEXT
-[ERROR] Coverage "70%" is lower than expectation "71%"                                                                 
+            '[ERROR] Coverage "70%" is lower than expectation "71%"',
+        ];
 
+        yield 'Clover file but coverage too low and violations are shown' => [
+            [
+                'file' => __DIR__ . '/../../../../Fixture/Coverage/coverage_70.clover',
+                '--coverage' => 71,
+            ],
+            <<<TEXT
 Violations
 ----------
 
