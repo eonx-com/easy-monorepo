cebf872ad8108859db8d10743ae6fe1a1491de33
Alexandr Tsepelev <90907144+alexandrtspl@users.noreply.github.com>
2025-01-24 18:43:32 +0500

    * some methods added (#1629)

--- packages/EasyTest/src/Common/Trait/ContainerServiceTrait.php
+++ packages/EasyTest/src/Common/Trait/ContainerServiceTrait.php
@@ -4,15 +4,30 @@ declare(strict_types=1);
 namespace EonX\EasyTest\Common\Trait;
 
 use Doctrine\ORM\EntityManagerInterface;
+use EonX\EasyActivity\EasyDoctrine\Subscriber\EasyDoctrineEntityEventsSubscriberInterface;
+use Faker\Generator;
 
 /**
  * @mixin \Symfony\Bundle\FrameworkBundle\Test\KernelTestCase
  */
 trait ContainerServiceTrait
 {
+    protected static function enableActivityLogging(): void
+    {
+        self::getService(EasyDoctrineEntityEventsSubscriberInterface::class)->enable();
+    }
+
     protected static function getEntityManager(): EntityManagerInterface
     {
         return self::getService(EntityManagerInterface::class);
+    }
+
+    protected static function getFaker(): Generator
+    {
+        /** @var \Faker\Generator $service */
+        $service = self::getService('test.faker');
+
+        return $service;
     }
 
     /**
