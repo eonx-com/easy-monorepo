a5d4d9f88d3606bac5e75ba86aedc32d6662ae03
Alexandr Tsepelev <90907144+alexandrtspl@users.noreply.github.com>
2025-01-27 15:34:48 +0500

    feature/easy-error-handler-assertion-trait (#1632)

--- packages/EasyTest/src/EasyErrorHandler/Trait/EasyErrorHandlerAssertionTrait.php
+++ packages/EasyTest/src/EasyErrorHandler/Trait/EasyErrorHandlerAssertionTrait.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace EonX\EasyTest\EasyErrorHandler\Trait;
 
 use EonX\EasyErrorHandler\Common\ErrorHandler\ErrorHandlerInterface;
+use PHPUnit\Framework\Attributes\After;
+use PHPUnit\Framework\Attributes\Before;
 
 /**
  * @mixin \PHPUnit\Framework\TestCase
@@ -11,6 +13,52 @@ use EonX\EasyErrorHandler\Common\ErrorHandler\ErrorHandlerInterface;
  */
 trait EasyErrorHandlerAssertionTrait
 {
+    /**
+     * @var int[]|null
+     */
+    private static ?array $errorHandlerReportedErrors = null;
+
+    #[Before]
+    public function setUpEasyErrorHandler(): void
+    {
+        self::$errorHandlerReportedErrors = null;
+    }
+
+    #[After]
+    public function tearDownEasyErrorHandler(): void
+    {
+        self::checkErrorHandlerReportedErrorsAsserted();
+    }
+
+    protected static function assertErrorHandlerReportedError(?int $errorCode = null): void
+    {
+        if ($errorCode === null) {
+            return;
+        }
+
+        if ($errorCode === 0) {
+            self::fail('Error code 0 is not allowed to be asserted.');
+        }
+
+        foreach (self::getErrorHandlerReportedErrors() as $key => $reportedError) {
+            if ($reportedError === $errorCode) {
+                unset(self::$errorHandlerReportedErrors[$key]);
+
+                return;
+            }
+        }
+
+        self::fail(
+            \sprintf('There is no "%s" error code in reported errors.', $errorCode)
+            . (\count(self::getErrorHandlerReportedErrors()) > 0
+                ? \sprintf(' Not reported errors: %s.', \implode(', ', self::getErrorHandlerReportedErrors()))
+                : ' No reported errors.')
+        );
+    }
+
+    /**
+     * @deprecated
+     */
     protected function assertEmptyReportedErrors(): void
     {
         self::assertCount(
@@ -20,6 +68,9 @@ trait EasyErrorHandlerAssertionTrait
         );
     }
 
+    /**
+     * @deprecated
+     */
     protected function assertErrorCodeExistsInReportedErrors(int $errorCode): void
     {
         $errorCodeExists = false;
@@ -39,6 +90,8 @@ trait EasyErrorHandlerAssertionTrait
 
     /**
      * @return \Throwable[]
+     *
+     * @deprecated
      */
     protected function getErrorHandlerReportedThrowables(): array
     {
@@ -46,5 +99,35 @@ trait EasyErrorHandlerAssertionTrait
         $errorHandler = self::getService(ErrorHandlerInterface::class);
 
         return $errorHandler->getReportedErrors();
+    }
+
+    private static function checkErrorHandlerReportedErrorsAsserted(): void
+    {
+        self::assertEmpty(self::getErrorHandlerReportedErrors(), \sprintf(
+            'Not all reported errors were asserted: %s.',
+            \implode(', ', self::getErrorHandlerReportedErrors())
+        ));
+    }
+
+    /**
+     * @return array<int|class-string<\Throwable>>
+     */
+    private static function getErrorHandlerReportedErrors(): array
+    {
+        if (self::$errorHandlerReportedErrors !== null) {
+            return self::$errorHandlerReportedErrors;
+        }
+
+        /** @var \EonX\EasyErrorHandler\Common\ErrorHandler\TraceableErrorHandlerInterface $errorHandler */
+        $errorHandler = self::getService(ErrorHandlerInterface::class);
+        self::$errorHandlerReportedErrors = [];
+
+        foreach ($errorHandler->getReportedErrors() as $exception) {
+            if (\is_int($exception->getCode()) && $exception->getCode() !== 0) {
+                self::$errorHandlerReportedErrors[] = $exception->getCode();
+            }
+        }
+
+        return self::$errorHandlerReportedErrors;
     }
 }
