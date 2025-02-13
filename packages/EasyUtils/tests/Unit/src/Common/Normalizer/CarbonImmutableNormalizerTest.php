2fc1e4be16a8b3588715bce2e669389b663ddd32
Roman Andreev <31572316+voodooism@users.noreply.github.com>
2025-02-13 14:55:59 +0100

    [EasyUtils] Change type for carbon immutable denormalization. (#1634)

--- packages/EasyUtils/tests/Unit/src/Common/Normalizer/CarbonImmutableNormalizerTest.php
+++ packages/EasyUtils/tests/Unit/src/Common/Normalizer/CarbonImmutableNormalizerTest.php
@@ -6,18 +6,29 @@ namespace EonX\EasyUtils\Tests\Unit\Common\Normalizer;
 use Carbon\CarbonImmutable;
 use EonX\EasyUtils\Common\Normalizer\CarbonImmutableNormalizer;
 use EonX\EasyUtils\Tests\Unit\AbstractUnitTestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 
 final class CarbonImmutableNormalizerTest extends AbstractUnitTestCase
 {
-    public function testDenormalizeSucceeds(): void
+    /**
+     * @see testDenormalizeSucceeds
+     */
+    public static function provideDateTimeFormats(): iterable
     {
-        $data = '2021-12-16T17:00:00+07:00';
+        yield 'Y-m-d H:i:s' => ['dateTime' => '2021-12-16 17:00:00'];
+
+        yield 'Y-m-d\TH:i:sP' => ['dateTime' => '2021-12-16T17:00:00+07:00'];
+    }
+
+    #[DataProvider('provideDateTimeFormats')]
+    public function testDenormalizeSucceeds(string $dateTime): void
+    {
         $normalizer = new CarbonImmutableNormalizer(new DateTimeNormalizer());
 
-        $result = $normalizer->denormalize($data, CarbonImmutable::class);
+        $result = $normalizer->denormalize($dateTime, CarbonImmutable::class);
 
-        self::assertEquals(new CarbonImmutable($data), $result);
+        self::assertEquals(new CarbonImmutable($dateTime), $result);
     }
 
     public function testHasCacheableSupportsMethodSucceeds(): void
