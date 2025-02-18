f0d94f40229d1fa880f557a21ad2bd812b8d653a
Alexandr Tsepelev <alexandr.tsepelev@eonx.com>
2025-02-18 11:07:23 +0500

    feature/MTCE-357-fix-results-count-when-large-dataset-is-enabled
    * format total results extension

---
+++ packages/EasyPagination/src/Twig/FormatTotalResultsExtension.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyPagination\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class FormatTotalResultsExtension extends AbstractExtension
+{
+    private int $maxPreciseNumResults;
+
+    public function __construct(?int $maxPreciseNumResults = null)
+    {
+        $this->maxPreciseNumResults = $maxPreciseNumResults ?? 100_000;
+    }
+
+    /**
+     * @return \Twig\TwigFunction[]
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('formatTotalResults', $this->formatTotalResults(...)),
+        ];
+    }
+
+    /**
+     * @return array{numResults: string, prefix: ?string, suffix: ?string}
+     */
+    private function formatTotalResults(int $numResults): array
+    {
+        if($numResults <= $this->maxPreciseNumResults || $numResults <= 100_000){
+            return [
+                'numResults' => (string)$numResults,
+                'prefix' => null,
+                'suffix' => null,
+            ];
+        }
+
+        $suffix = '';
+        if($numResults > 100_000 && $numResults < 1000_000){
+            $numResults = \round($numResults / 1000);
+            $suffix = 'K';
+        }
+        if($numResults >= 1000_000 && $numResults < 1000_000_000){
+            $numResults = \round($numResults / 1000_000, 1);
+            $suffix = 'M';
+        }
+        if($numResults >= 1000_000_000){
+            $numResults = \round($numResults / 1000_000_000, 2);
+            $suffix = 'B';
+        }
+
+        $numResults = \rtrim((string)$numResults, '0');
+
+        return [
+            'numResults' => \rtrim($numResults, '.'),
+            'prefix' => 'About',
+            'suffix' => $suffix,
+        ];
+    }
+
+}
---
+++ packages/EasyPagination/tests/Unit/src/Twig/FormatTotalResultsExtensionTest.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyPagination\Tests\Unit\Twig;
+
+use EonX\EasyPagination\Resolver\FromHttpFoundationRequestPaginationResolver;
+use EonX\EasyPagination\Tests\Unit\AbstractUnitTestCase;
+use EonX\EasyPagination\Twig\FormatTotalResultsExtension;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+final class FormatTotalResultsExtensionTest extends AbstractUnitTestCase
+{
+    /**
+     * @see testFormatTotalResults
+     */
+    public static function provideNumResults(): iterable
+    {
+        yield 'NumResults lower than maxPreciseNumResults' => [
+            'numResults' => 109000,
+            'expectedResult' => [
+                'numResults' => '109000',
+                'prefix' => null,
+                'suffix' => null,
+            ],
+        ];
+
+        yield 'NumResults lower than 100_000' => [
+            'numResults' => 10000,
+            'expectedResult' => [
+                'numResults' => '10000',
+                'prefix' => null,
+                'suffix' => null,
+            ],
+        ];
+
+        yield 'NumResults greate than maxPreciseNumResults, between 100_000 and 1000_000' => [
+            'numResults' => 151467,
+            'expectedResult' => [
+                'numResults' => '151',
+                'prefix' => 'About',
+                'suffix' => 'K',
+            ],
+        ];
+
+        yield 'NumResults greate than maxPreciseNumResults, between 1000_000 and 1000_000_000' => [
+            'numResults' => 151467988,
+            'expectedResult' => [
+                'numResults' => '151.5',
+                'prefix' => 'About',
+                'suffix' => 'M',
+            ],
+        ];
+
+        yield 'NumResults greate than maxPreciseNumResults, greate than 1000_000_000' => [
+            'numResults' => 151467988344,
+            'expectedResult' => [
+                'numResults' => '151.47',
+                'prefix' => 'About',
+                'suffix' => 'B',
+            ],
+        ];
+    }
+
+    #[DataProvider('provideNumResults')]
+    public function testFormatTotalResults(int $numResults, array $expectedResult): void
+    {
+        $formatTotalResultsExtension = new FormatTotalResultsExtension(110_000);
+        $formatTotalResults = $formatTotalResultsExtension->getFunctions()[0]->getCallable();
+        $result = $formatTotalResults($numResults);
+
+        self::assertSame($expectedResult, $result);
+    }
+}
