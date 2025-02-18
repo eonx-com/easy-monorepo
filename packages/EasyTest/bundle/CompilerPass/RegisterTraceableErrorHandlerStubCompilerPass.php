0c8f5a54c59acca0731661a2ca02c27dbded7323
Aleksey Polyvanyi <alexndlm@gmail.com>
2025-02-18 12:24:41 +0100

    feature/easy-test-create-EasyTestBundle

--- packages/EasyTest/bundle/CompilerPass/RegisterTraceableErrorHandlerStubCompilerPass.php
+++ packages/EasyTest/bundle/CompilerPass/RegisterTraceableErrorHandlerStubCompilerPass.php
@@ -1,24 +1,30 @@
 <?php
 declare(strict_types=1);
 
-namespace EonX\EasyErrorHandler\Bundle\CompilerPass;
+namespace EonX\EasyTest\Bundle\CompilerPass;
 
 use EonX\EasyErrorHandler\Common\ErrorHandler\ErrorHandlerInterface;
 use EonX\EasyErrorHandler\Common\ErrorHandler\TraceableErrorHandler;
 use EonX\EasyErrorHandler\Common\ErrorHandler\TraceableErrorHandlerInterface;
+use EonX\EasyTest\EasyErrorHandler\ErrorHandler\TraceableErrorHandlerStub;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
-final class RegisterTraceableErrorHandlerCompilerPass implements CompilerPassInterface
+final class RegisterTraceableErrorHandlerStubCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
-        if (($container->hasParameter('kernel.debug') && $container->getParameter('kernel.debug'))) {
+        if ($container->hasDefinition(TraceableErrorHandlerInterface::class) === false) {
             $container
                 ->register(TraceableErrorHandlerInterface::class, TraceableErrorHandler::class)
                 ->setDecoratedService(ErrorHandlerInterface::class)
                 ->addArgument(new Reference(\sprintf('%s.inner', TraceableErrorHandlerInterface::class)));
         }
+
+        $container
+            ->register(TraceableErrorHandlerStub::class, TraceableErrorHandlerStub::class)
+            ->setDecoratedService(TraceableErrorHandlerInterface::class)
+            ->addArgument(new Reference(\sprintf('%s.inner', TraceableErrorHandlerStub::class)));
     }
 }
