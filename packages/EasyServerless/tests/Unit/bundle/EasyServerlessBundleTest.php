7ee9b5edaaaa3a4a33e93ad186dfb0053e7d6afd
nathanpage <nathan.page@eonx.com>
2025-03-27 09:33:50 +1100

    [EasyServerless] Fix cs

--- packages/EasyServerless/tests/Unit/bundle/EasyServerlessBundleTest.php
+++ packages/EasyServerless/tests/Unit/bundle/EasyServerlessBundleTest.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 namespace EonX\EasyServerless\Tests\Unit\Bundle;
 
-use EonX\EasyServerless\Tests\Unit\AbstractSymfonyTestCase;
 use EonX\EasyServerless\Bundle\Enum\ConfigParam;
+use EonX\EasyServerless\Tests\Unit\AbstractSymfonyTestCase;
 
 final class EasyServerlessBundleTest extends AbstractSymfonyTestCase
 {
