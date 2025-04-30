437f40732e32494ade5c9f830bb8b5c4ad4ce0db
nathanpage <nathan.page@eonx.com>
2025-04-30 12:29:35 +1000

    [EasyServerless] Supports resolving secrets from JSON files in the main load function

--- packages/EasyServerless/src/Aws/Helper/SecretsHelper.php
+++ packages/EasyServerless/src/Aws/Helper/SecretsHelper.php
@@ -10,7 +10,9 @@ final class SecretsHelper
 {
     private const ALREADY_LOADED = 'easy_serverless.secrets.already_loaded';
 
-    private const PREFIX_SECRETS_MANAGER = 'resolve:secretsmanager:';
+    private const PREFIX_JSON_FILES = 'resolve:json_files:';
+
+    private const PREFIX_SECRETS_MANAGER = 'resolve:secrets_manager:';
 
     private static ?SecretsManagerClient $secretsManager = null;
 
@@ -25,7 +27,14 @@ final class SecretsHelper
 
         self::doLoad(\array_filter(
             $_SERVER,
-            static fn ($value): bool => \is_string($value) && \str_starts_with($value, self::PREFIX_SECRETS_MANAGER)
+            static function ($value): bool {
+                if (\is_string($value) === false) {
+                    return false;
+                }
+
+                return \str_starts_with($value, self::PREFIX_SECRETS_MANAGER)
+                    || \str_starts_with($value, self::PREFIX_JSON_FILES);
+            }
         ));
 
         $_SERVER[self::ALREADY_LOADED] = true;
@@ -33,6 +42,10 @@ final class SecretsHelper
 
     public static function loadFromJsonFiles(string $dir): void
     {
+        if (\str_starts_with($dir, self::PREFIX_JSON_FILES)) {
+            $dir = \str_replace(self::PREFIX_JSON_FILES, '', $dir);
+        }
+
         $files = (new Finder())
             ->in($dir)
             ->files()
@@ -79,6 +92,14 @@ final class SecretsHelper
     private static function doLoad(array $envVars): void
     {
         foreach ($envVars as $key => $value) {
+            if (\is_string($value) && \str_starts_with($value, self::PREFIX_JSON_FILES)) {
+                self::logToStderr(\sprintf('Found secret to resolve from local filesystem: %s => %s', $key, $value));
+
+                self::loadFromJsonFiles($value);
+
+                continue;
+            }
+
             if (\is_string($value) && \str_starts_with($value, self::PREFIX_SECRETS_MANAGER)) {
                 self::logToStderr(\sprintf('Found secret to resolve from SecretsManager: %s => %s', $key, $value));
 
