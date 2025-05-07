61ddac93a4e02fe49738c175d709fa8affec6dd6
Shevelev Vladimir <vladimir.shevelev@eonx.com>
2025-05-07 19:49:57 +0700

    feature/Add-AcnValidator
    - Add AcnValidator

---
+++ packages/EasyUtils/src/Common/Constraint/Acn.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyUtils\Common\Constraint;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_METHOD)]
+final class Acn extends AbstractConstraint
+{
+    public const COMPLEMENT_CALCULATION_FAILED_ERROR = 'COMPLEMENT_CALCULATION_FAILED_ERROR';
+
+    public const INVALID_CHARACTERS_ERROR = 'INVALID_CHARACTERS_ERROR';
+
+    public const INVALID_LENGTH_ERROR = 'INVALID_LENGTH_ERROR';
+
+    public string $message = 'acn.not_valid';
+
+    public function __construct(?array $groups = null, ?string $message = null)
+    {
+        $this->message = $message ?? $this->message;
+
+        parent::__construct(groups: $groups);
+    }
+}
---
+++ packages/EasyUtils/src/Common/Validator/AcnValidator.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyUtils\Common\Validator;
+
+use EonX\EasyUtils\Common\Constraint\Acn;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+
+/**
+ * Validates an Australian Company Number.
+ *
+ * @link https://asic.gov.au/for-business/registering-a-company/steps-to-register-a-company/australian-company-numbers/australian-company-number-digit-check/
+ */
+final class AcnValidator extends ConstraintValidator
+{
+    private const ACN_LENGTH = 9;
+
+    private const MODULUS = 10;
+
+    private const WEIGHTS = [8, 7, 6, 5, 4, 3, 2, 1, 0];
+
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        if ($constraint instanceof Acn === false) {
+            throw new UnexpectedTypeException($constraint, Acn::class);
+        }
+
+        if ($value === null || $value === '') {
+            return;
+        }
+
+        if (\is_string($value) === false && (\is_object($value) && \method_exists($value, '__toString')) === false) {
+            throw new UnexpectedValueException($value, 'string');
+        }
+
+        $acn = (string)$value;
+
+        $violationCode = $this->getViolationCode($acn);
+        if ($violationCode !== null) {
+            $this->context->buildViolation($constraint->message)
+                ->setCode($violationCode)
+                ->addViolation();
+
+            return;
+        }
+
+        $sum = 0;
+        foreach (\str_split($acn) as $key => $digit) {
+            $sum += (int)$digit * self::WEIGHTS[$key];
+        }
+
+        $remainder = $sum % self::MODULUS;
+        $complement = (string)(self::MODULUS - $remainder);
+
+        if ($complement === '10') {
+            $complement = '0';
+        }
+
+        if ($acn[8] !== $complement) {
+            $this->context->buildViolation($constraint->message)
+                ->setCode(Acn::COMPLEMENT_CALCULATION_FAILED_ERROR)
+                ->addViolation();
+        }
+    }
+
+    private function getViolationCode(string $acn): ?string
+    {
+        if (\strlen($acn) !== self::ACN_LENGTH) {
+            return Acn::INVALID_LENGTH_ERROR;
+        }
+
+        if (\ctype_digit($acn) === false) {
+            return Acn::INVALID_CHARACTERS_ERROR;
+        }
+
+        return null;
+    }
+}
