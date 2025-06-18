77cfd091bb38555ee2c7b8328e4eb6ab407facf6
Aleksey Polyvanyi <alexndlm@gmail.com>
2025-06-18 12:10:55 +0200

    [EasyUtils] Sanitize URL with key in path (#1660)

--- packages/EasyUtils/src/SensitiveData/Sanitizer/UrlStringSanitizer.php
+++ packages/EasyUtils/src/SensitiveData/Sanitizer/UrlStringSanitizer.php
@@ -9,7 +9,7 @@ final class UrlStringSanitizer extends AbstractStringSanitizer
     {
         foreach ($keysToMask as $key) {
             $string = (string)\preg_replace(
-                \sprintf('/(%s=|\[%s\]=)([^&]+)/i', $key, $key),
+                \sprintf('/(%s=|\[%s\]=|\/%s\/)([^&\/?#]+)/i', $key, $key, $key),
                 '$1' . $maskPattern,
                 $string
             );
