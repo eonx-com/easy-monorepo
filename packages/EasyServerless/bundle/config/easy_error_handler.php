57ecffd26d603f48a75f9602a1d1d2c1081504b7
nathanpage <nathan.page@eonx.com>
2025-07-01 08:32:40 +1000

    [EasyServerless] Ensure errors are reported within bref messenger context

---
+++ packages/EasyServerless/bundle/config/easy_error_handler.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Bref\Symfony\Messenger\Service\BusDriver;
+use EasyServerless\EasyErrorHandler\BusDriver\ReportBusDriver;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->set(ReportBusDriver::class)
+        ->decorate(BusDriver::class, priority: -1000);
+};
---
+++ packages/EasyServerless/src/EasyErrorHandler/BusDriver/ReportBusDriver.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace EasyServerless\EasyErrorHandler\BusDriver;
+
+use Bref\Symfony\Messenger\Service\BusDriver;
+use EonX\EasyErrorHandler\Common\ErrorHandler\ErrorHandlerInterface;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Throwable;
+
+final readonly class ReportBusDriver implements BusDriver
+{
+    public function __construct(
+        private BusDriver $decorated,
+        private ErrorHandlerInterface $errorHandler,
+    ) {
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function putEnvelopeOnBus(MessageBusInterface $bus, Envelope $envelope, string $transportName): void
+    {
+        try {
+            $this->decorated->putEnvelopeOnBus($bus, $envelope, $transportName);
+        } catch (Throwable $throwable) {
+            $this->errorHandler->report($throwable);
+
+            throw $throwable;
+        }
+    }
+}
