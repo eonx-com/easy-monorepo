0f0de1dd40ccba89bd4529a6a972e03c92e2502c
Ivan Torgov <ivan.torgov@eonx.com>
2025-07-10 23:34:17 +1000

    [EasyUtils] Implement `NullableEnumEnvVarProcessor`

--- packages/EasyUtils/bundle/config/services.php
+++ packages/EasyUtils/bundle/config/services.php
@@ -5,6 +5,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use EonX\EasyUtils\Bundle\Enum\ConfigParam;
 use EonX\EasyUtils\Common\EnvVarProcessor\ForBuildEnvVarProcessor;
+use EonX\EasyUtils\Common\EnvVarProcessor\NullableEnumEnvVarProcessor;
 use EonX\EasyUtils\CreditCard\Validator\CreditCardNumberValidator;
 use EonX\EasyUtils\CreditCard\Validator\CreditCardNumberValidatorInterface;
 use EonX\EasyUtils\Csv\Parser\CsvWithHeadersParser;
@@ -20,7 +21,9 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(CreditCardNumberValidatorInterface::class, CreditCardNumberValidator::class);
     $services->set(CsvWithHeadersParserInterface::class, CsvWithHeadersParser::class);
+
     $services->set(ForBuildEnvVarProcessor::class);
+    $services->set(NullableEnumEnvVarProcessor::class);
 
     $services->set(MathHelperInterface::class, MathHelper::class)
         ->arg('$roundPrecision', param(ConfigParam::MathRoundPrecision->value))
