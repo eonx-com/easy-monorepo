0dab94ab16bed83a508cb621f362fd83026b8c3f
nathanpage <nathan.page@eonx.com>
2025-10-22 10:24:47 +1100

    [EasySwoole] Allow applications to implement custom logic on workerStart and workerStop

---
+++ packages/EasySwoole/src/Common/Event/WorkerStopEvent.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasySwoole\Common\Event;
+
+use Swoole\Http\Server;
+
+final readonly class WorkerStopEvent
+{
+    public function __construct(
+        private Server $server,
+        private int $workerId,
+    ) {
+    }
+
+    public function getServer(): Server
+    {
+        return $this->server;
+    }
+
+    public function getWorkerId(): int
+    {
+        return $this->workerId;
+    }
+}
