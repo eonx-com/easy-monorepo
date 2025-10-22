19062494fea0ed1b53ad8a73baf8d3afaa9bfb0d
nathanpage <nathan.page@eonx.com>
2025-10-23 09:00:37 +1100

    [EasySwoole] Fix listener binding

--- packages/EasySwoole/src/EasyEncryption/Listener/InitAwsCloudHsmEncryptorListener.php
+++ packages/EasySwoole/src/EasyEncryption/Listener/InitAwsCloudHsmEncryptorListener.php
@@ -4,17 +4,20 @@ declare(strict_types=1);
 namespace EonX\EasySwoole\EasyEncryption\Listener;
 
 use EonX\EasyEncryption\AwsCloudHsm\Encryptor\AwsCloudHsmEncryptorInterface;
+use EonX\EasyEncryption\Common\Encryptor\EncryptorInterface;
 use EonX\EasySwoole\Common\Event\WorkerStartEvent;
 
 final readonly class InitAwsCloudHsmEncryptorListener
 {
     public function __construct(
-        private AwsCloudHsmEncryptorInterface $encryptor,
+        private EncryptorInterface $encryptor,
     ) {
     }
 
     public function __invoke(WorkerStartEvent $event): void
     {
-        $this->encryptor->init();
+        if ($this->encryptor instanceof AwsCloudHsmEncryptorInterface) {
+            $this->encryptor->init();
+        }
     }
 }
