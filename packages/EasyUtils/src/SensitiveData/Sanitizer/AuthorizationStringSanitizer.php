c25ceb5c06bb4269f8637886765913ca1fe561ff
Aleksey Polyvanyi <alexndlm@gmail.com>
2025-10-28 16:00:02 +0100

    feature/easy-utils-MTCE-558-Fix-Authorization-token-sanitizing-in-logs (#1714)

--- packages/EasyUtils/src/SensitiveData/Sanitizer/AuthorizationStringSanitizer.php
+++ packages/EasyUtils/src/SensitiveData/Sanitizer/AuthorizationStringSanitizer.php
@@ -8,8 +8,8 @@ final class AuthorizationStringSanitizer extends AbstractStringSanitizer
     public function sanitizeString(string $string, string $maskPattern, array $keysToMask): string
     {
         return (string)\preg_replace(
-            '/(Authorization:)[A-Za-z0-9=_\-\. ]+/',
-            '$1' . $maskPattern,
+            '/(^|\b)(Authorization:)[^\r\n]+/mi',
+            '$2 ' . $maskPattern,
             $string
         );
     }
