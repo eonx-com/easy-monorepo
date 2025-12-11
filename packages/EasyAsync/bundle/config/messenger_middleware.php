0d9a1e700aa3a8d65a01d5f4212b9555b7e34115
zeromodule <anton.sharonov@eonx.com>
2025-12-11 12:18:38 +0100

    [EasyAsync] Fix manager services definition (#1721)

--- packages/EasyAsync/bundle/config/messenger_middleware.php
+++ packages/EasyAsync/bundle/config/messenger_middleware.php
@@ -3,9 +3,6 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use EonX\EasyAsync\Bundle\Enum\BundleParam;
-use EonX\EasyAsync\Doctrine\Checker\ManagersSanityChecker;
-use EonX\EasyAsync\Doctrine\Clearer\ManagersClearer;
 use EonX\EasyAsync\Messenger\Middleware\DoctrineManagersClearMiddleware;
 use EonX\EasyAsync\Messenger\Middleware\DoctrineManagersSanityCheckMiddleware;
 
@@ -14,14 +11,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->defaults()
         ->autowire()
         ->autoconfigure();
-
-    // Default managers clearer
-    $services->set(ManagersClearer::class);
-
-    // Default managers sanity checker
-    $services
-        ->set(ManagersSanityChecker::class)
-        ->tag('monolog.logger', ['channel' => BundleParam::LogChannel->value]);
 
     // Default managers clearer middleware (clear all managers)
     $services->set(DoctrineManagersClearMiddleware::class);
