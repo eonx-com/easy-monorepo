3f9017830c9e8eeaa9bfa268cf95a38d41eadb89
Nathan Page <nathan.page@eonx.com>
2026-01-14 10:23:01 +1100

    [EasyServerless] Register listener to call bugsnag shutdown strategy on Messenger Envelope dispatched (#1731)

--- packages/EasyServerless/bundle/config/easy_bugsnag.php
+++ packages/EasyServerless/bundle/config/easy_bugsnag.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use EonX\EasyBugsnag\Bundle\Enum\ConfigServiceId as EasyBugsnagConfigServiceId;
+use EonX\EasyBugsnag\Common\Listener\ShutdownStrategyListener;
 use EonX\EasyServerless\EasyBugsnag\Configurator\AwsLambdaConfigurator;
+use EonX\EasyServerless\Messenger\Event\EnvelopeDispatchedEvent;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
@@ -12,4 +15,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->autoconfigure();
 
     $services->set(AwsLambdaConfigurator::class);
+
+    $services
+        ->set('easy_serverless.easy_bugsnag.shutdown_strategy_listener', ShutdownStrategyListener::class)
+        ->arg('$shutdownStrategy', service(EasyBugsnagConfigServiceId::ShutdownStrategy->value))
+        ->tag('kernel.event_listener', [
+            'event' => EnvelopeDispatchedEvent::class,
+        ]);
 };
