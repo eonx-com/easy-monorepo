052002cf4734a44dcdbf845486080db33b705592
Ivan Torgov <ivan.torgov@eonx.com>
2026-01-14 12:10:13 +1100

    [EasyPagination] Fix ECS

--- packages/EasyPagination/src/Pagination/StatelessPagination.php
+++ packages/EasyPagination/src/Pagination/StatelessPagination.php
@@ -13,31 +13,37 @@ final readonly class StatelessPagination implements PaginationInterface
 
     public function getPage(): int
     {
-        return $this->paginationProvider->getPagination()->getPage();
+        return $this->paginationProvider->getPagination()
+            ->getPage();
     }
 
     public function getPageAttribute(): string
     {
-        return $this->paginationProvider->getPagination()->getPageAttribute();
+        return $this->paginationProvider->getPagination()
+            ->getPageAttribute();
     }
 
     public function getPerPage(): int
     {
-        return $this->paginationProvider->getPagination()->getPerPage();
+        return $this->paginationProvider->getPagination()
+            ->getPerPage();
     }
 
     public function getPerPageAttribute(): string
     {
-        return $this->paginationProvider->getPagination()->getPerPageAttribute();
+        return $this->paginationProvider->getPagination()
+            ->getPerPageAttribute();
     }
 
     public function getUrl(int $page): string
     {
-        return $this->paginationProvider->getPagination()->getUrl($page);
+        return $this->paginationProvider->getPagination()
+            ->getUrl($page);
     }
 
     public function setUrlResolver(?callable $urlResolver = null): PaginationInterface
     {
-        return $this->paginationProvider->getPagination()->setUrlResolver($urlResolver);
+        return $this->paginationProvider->getPagination()
+            ->setUrlResolver($urlResolver);
     }
 }
