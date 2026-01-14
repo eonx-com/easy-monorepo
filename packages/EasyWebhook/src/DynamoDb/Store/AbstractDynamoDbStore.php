45052bea1567be128b1d8d49e18523423b52016b
nathanpage <nathan.page@eonx.com>
2026-01-14 15:49:34 +1100

    [EasyWebhook] Implement AWS DynamoDB stores

---
+++ packages/EasyWebhook/src/DynamoDb/Store/AbstractDynamoDbStore.php
@@ -0,0 +1,97 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyWebhook\DynamoDb\Store;
+
+use AsyncAws\DynamoDb\DynamoDbClient;
+use AsyncAws\DynamoDb\Input\GetItemInput;
+use AsyncAws\DynamoDb\Input\PutItemInput;
+use AsyncAws\DynamoDb\ValueObject\AttributeValue;
+use Carbon\CarbonImmutable;
+use EonX\EasyRandom\Generator\RandomGeneratorInterface;
+use EonX\EasyWebhook\Common\Cleaner\DataCleanerInterface;
+use EonX\EasyWebhook\Common\Store\AbstractStore;
+
+abstract class AbstractDynamoDbStore extends AbstractStore
+{
+    public const DEFAULT_TTL_ATTRIBUTE = 'ttl';
+
+    public function __construct(
+        RandomGeneratorInterface $random,
+        DataCleanerInterface $dataCleaner,
+        private readonly DynamoDbClient $dynamoDbClient,
+        private readonly string $table,
+        private readonly ?string $ttl = null,
+        private readonly ?string $ttlAttribute = null,
+    ) {
+        parent::__construct($random, $dataCleaner);
+    }
+
+    /**
+     * @param \AsyncAws\DynamoDb\ValueObject\AttributeValue[]|null $previousData
+     *
+     * @return \AsyncAws\DynamoDb\ValueObject\AttributeValue[]
+     */
+    abstract protected function createItemFromInstance(object $instance, ?array $previousData = null): array;
+
+    abstract protected function getDefaultTable(): string;
+
+    /**
+     * @param \AsyncAws\DynamoDb\ValueObject\AttributeValue[] $item
+     */
+    abstract protected function instantiateFromResultItem(array $item): object;
+
+    protected function doFind(string $keyName, string $keyValue, ?bool $consistentRead = null): ?object
+    {
+        $result = $this->doFindRaw($keyName, $keyValue, $consistentRead);
+
+        return \is_array($result) ? $this->instantiateFromResultItem($result) : null;
+    }
+
+    /**
+     * @return \AsyncAws\DynamoDb\ValueObject\AttributeValue[]|null
+     */
+    protected function doFindRaw(string $keyName, string $keyValue, ?bool $consistentRead = null): ?array
+    {
+        $result = $this->dynamoDbClient->getItem(new GetItemInput([
+            'TableName' => $this->table ?? $this->getDefaultTable(),
+            'ConsistentRead' => $consistentRead ?? false,
+            'Key' => [
+                $keyName => AttributeValue::create(['S' => $keyValue]),
+            ],
+        ]));
+
+        $item = $result->getItem();
+
+        return \count($item) > 0 ? $item : null;
+    }
+
+    /**
+     * @param \AsyncAws\DynamoDb\ValueObject\AttributeValue[]|null $previousData
+     */
+    protected function doPutItem(object $instance, ?string $uniqueAttr = null, ?array $previousData = null): object
+    {
+        $item = $this->createItemFromInstance($instance, $previousData);
+
+        if (\is_string($this->ttl) && $this->ttl !== '') {
+            $ttlAttr = $this->ttlAttribute ?? self::DEFAULT_TTL_ATTRIBUTE;
+            $item[$ttlAttr] = AttributeValue::create([
+                'N' => (string)CarbonImmutable::now('UTC')->add($this->ttl)->getTimestamp(),
+            ]);
+        }
+
+        $input = [
+            'TableName' => $this->table ?? $this->getDefaultTable(),
+            'Item' => $item,
+        ];
+
+        if ($uniqueAttr !== null && $uniqueAttr !== '') {
+            $input['ConditionExpression'] = \sprintf('attribute_not_exists(%s)', $uniqueAttr);
+        }
+
+        $this->dynamoDbClient->putItem(new PutItemInput($input))
+            ->getAttributes();
+
+        return $instance;
+    }
+}
---
+++ packages/EasyWebhook/src/DynamoDb/Store/DynamoDbResultStore.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyWebhook\DynamoDb\Store;
+
+use AsyncAws\DynamoDb\Exception\ConditionalCheckFailedException;
+use AsyncAws\DynamoDb\ValueObject\AttributeValue;
+use Carbon\CarbonImmutable;
+use EonX\EasyWebhook\Common\Entity\WebhookResultInterface;
+use EonX\EasyWebhook\Common\Exception\MethodNotImplementedException;
+use EonX\EasyWebhook\Common\Helper\WebhookResultHelper;
+use EonX\EasyWebhook\Common\Store\ResultStoreInterface;
+
+final class DynamoDbResultStore extends AbstractDynamoDbStore implements ResultStoreInterface
+{
+    public const DEFAULT_TABLE = 'easy_webhook_results';
+
+    public function store(WebhookResultInterface $result): WebhookResultInterface
+    {
+        $result->setId($result->getId() ?? $this->random->uuid());
+
+        try {
+            // Attempt optimistic creation
+            $this->doPutItem($result, 'id');
+        } catch (ConditionalCheckFailedException) {
+            // If we get here, the item already exists, so we update it
+            $this->doPutItem(
+                instance: $result,
+                previousData: $this->doFindRaw('id', $result->getId(), true)
+            );
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param \EonX\EasyWebhook\Common\Entity\WebhookResultInterface $instance
+     *
+     * @return \AsyncAws\DynamoDb\ValueObject\AttributeValue[]
+     *
+     * @throws \Nette\Utils\JsonException
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    protected function createItemFromInstance(object $instance, ?array $previousData = null): array
+    {
+        $now = CarbonImmutable::now('UTC');
+        $data = \array_merge(WebhookResultHelper::getResultData($instance), [
+            'created_at' => isset($previousData['created_at']) ? $previousData['created_at']->getS() ?? $now : $now,
+            'updated_at' => $now,
+        ]);
+
+        return \array_map(
+            static fn ($value): AttributeValue => AttributeValue::create(['S' => (string)$value]),
+            $this->formatData($data)
+        );
+    }
+
+    protected function getDefaultTable(): string
+    {
+        return self::DEFAULT_TABLE;
+    }
+
+    protected function instantiateFromResultItem(array $item): object
+    {
+        throw new MethodNotImplementedException('This store does not support fetching WebhookResult instances');
+    }
+}
