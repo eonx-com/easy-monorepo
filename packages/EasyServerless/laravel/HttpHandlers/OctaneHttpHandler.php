40ce51d2d00958e8ce725d5bf7e82d00ebf6cd33
Nathan Page <nathan.page@eonx.com>
2026-01-26 10:20:48 +1100

    [EasyServerless] Improve HTTP handlers (#1734)
    * [EasyServerless] Improve HTTP handlers
    
    * [EasyServerless] Fix cs

---
+++ packages/EasyServerless/laravel/HttpHandlers/OctaneHttpHandler.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyServerless\Laravel\HttpHandlers;
+
+use Bref\Context\Context;
+use Bref\Event\Http\HttpRequestEvent;
+use Bref\Event\Http\HttpResponse;
+use Bref\LaravelBridge\Http\OctaneHandler;
+use EonX\EasyServerless\Aws\Transformer\HttpEventTransformer;
+use EonX\EasyServerless\Aws\Transformer\HttpEventTransformerInterface;
+
+final class OctaneHttpHandler extends OctaneHandler
+{
+    private const LAMBDA_REQUEST_CONTEXT_KEY = 'LAMBDA_REQUEST_CONTEXT';
+
+    public function __construct(
+        ?string $path = null,
+        private readonly HttpEventTransformerInterface $httpEventTransformer = new HttpEventTransformer(),
+    ) {
+        parent::__construct($path);
+    }
+
+    public function handle(mixed $event, Context $context): array
+    {
+        if (\is_array($event)) {
+            $event = $this->httpEventTransformer->transform($event);
+        }
+
+        return parent::handle($event, $context);
+    }
+
+    public function handleRequest(HttpRequestEvent $event, Context $context): HttpResponse
+    {
+        $this->setRequestContext((string)\json_encode($event->getRequestContext()));
+
+        return parent::handleRequest($event, $context);
+    }
+
+    private function setRequestContext(string $value): void
+    {
+        $_SERVER[self::LAMBDA_REQUEST_CONTEXT_KEY] = $_ENV[self::LAMBDA_REQUEST_CONTEXT_KEY] = $value;
+        \putenv(\sprintf('%s=%s', self::LAMBDA_REQUEST_CONTEXT_KEY, $value));
+    }
+}
