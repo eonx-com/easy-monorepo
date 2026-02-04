8ec960cc624b667b82f7080b455b0e509cd78502
Nathan Page <nathan.page@eonx.com>
2026-02-05 09:07:47 +1100

    [EasyServerless] Improve Bugsnag integration (#1747)
    * [EasyServerless] Improve Bugsnag integration
    
    * [EasyServerless] Fix cs

--- packages/EasyServerless/src/EasyBugsnag/Configurator/AwsLambdaConfigurator.php
+++ packages/EasyServerless/src/EasyBugsnag/Configurator/AwsLambdaConfigurator.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace EonX\EasyServerless\EasyBugsnag\Configurator;
 
 use Bugsnag\Client;
+use Bugsnag\Middleware\CallbackBridge;
+use Bugsnag\Report;
 use EonX\EasyBugsnag\Common\Configurator\AbstractClientConfigurator;
 use EonX\EasyServerless\Aws\Helper\LambdaContextHelper;
 
@@ -11,11 +13,17 @@ final class AwsLambdaConfigurator extends AbstractClientConfigurator
 {
     public function configure(Client $bugsnag): void
     {
-        $bugsnag->setMetaData([
-            'lambda' => [
-                'invocation' => LambdaContextHelper::getInvocationContext(),
-                'request' => LambdaContextHelper::getRequestContext(),
-            ],
-        ]);
+        $bugsnag
+            ->getPipeline()
+            ->pipe(new CallbackBridge(static function (Report $report): void {
+                $report->addMetaData([
+                    'lambda' => [
+                        'handler' => LambdaContextHelper::getHandler(),
+                        'invocation' => LambdaContextHelper::getInvocationContext(),
+                        'request' => LambdaContextHelper::getRequestContext(),
+                        'taskRoot' => LambdaContextHelper::getTaskRoot(),
+                    ],
+                ]);
+            }));
     }
 }
