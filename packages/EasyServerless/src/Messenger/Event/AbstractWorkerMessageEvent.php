bfaeeb50dd1be4498b313aa4ab7ac05916a409db
Anton Sharonov <anton.sharonov@eonx.com>
2026-03-11 14:01:05 +0300

    feature/easy-serverless-dispatch-additional-events
    - Add WorkerMessageHandledEvent and WorkerMessageFailedEvent

---
+++ packages/EasyServerless/src/Messenger/Event/AbstractWorkerMessageEvent.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyServerless\Messenger\Event;
+
+use Symfony\Component\Messenger\Event\AbstractWorkerMessageEvent as SymfonyAbstractWorkerMessageEvent;
+
+abstract class AbstractWorkerMessageEvent extends SymfonyAbstractWorkerMessageEvent
+{
+}
