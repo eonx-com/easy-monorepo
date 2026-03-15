5fef517329e1e9dc407c52cb3f4d7646d9cf04b3
Aleksey Polyvanyi <alexndlm@gmail.com>
2026-03-15 16:07:38 +0100

    feature/easy-api-platform-fix-test-for-4.3.0

--- packages/EasyApiPlatform/tests/Application/src/Common/ReturnNotFoundOnReadOperation/WhenPutReturnNotFoundOnReadOperationTest.php
+++ packages/EasyApiPlatform/tests/Application/src/Common/ReturnNotFoundOnReadOperation/WhenPutReturnNotFoundOnReadOperationTest.php
@@ -56,9 +56,7 @@ final class WhenPutReturnNotFoundOnReadOperationTest extends AbstractApplication
                 ],
             ]
         );
-        self::assertSame(500, $response->getStatusCode());
-        /** @var array $responseData */
-        $responseData = \json_decode($response->getContent(false), true);
-        self::assertSame(403, $responseData['custom_code']);
+
+        self::assertSame(200, $response->getStatusCode());
     }
 }
