8c70b4ce4ae58f4be76730d8eb49c0c343e98ccc
Ivan Torgov <ivan.torgov@eonx.com>
2026-03-26 14:43:34 +1100

    [EasyServerless] Fix "services.php" (#1780)

--- packages/EasyServerless/bundle/config/services.php
+++ packages/EasyServerless/bundle/config/services.php
@@ -3,11 +3,7 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Bref\Symfony\Messenger\Service\BusDriver;
-use EonX\EasyErrorHandler\Common\ErrorHandler\ErrorHandlerInterface;
-use EonX\EasyEventDispatcher\Dispatcher\EventDispatcherInterface;
 use EonX\EasyServerless\Aws\HttpHandler\SymfonyHttpHandler;
-use EonX\EasyServerless\Messenger\BusDriver\ReportBusDriver;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
@@ -19,10 +15,4 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services
         ->set(SymfonyHttpHandler::class)
         ->public(); // Must be public as Bref uses the PSR container to retrieve it
-
-    // ReportBusDriver decorates the original BusDriver to report exceptions and dispatch an event
-    $services->set(ReportBusDriver::class)
-        ->decorate(BusDriver::class, priority: -1000)
-        ->arg('$errorHandler', service(ErrorHandlerInterface::class)->nullOnInvalid())
-        ->arg('$eventDispatcher', service(EventDispatcherInterface::class)->nullOnInvalid());
 };
