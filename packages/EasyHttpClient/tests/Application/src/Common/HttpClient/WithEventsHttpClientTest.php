ae871685c7120d20965f240580d591b2d691989d
Aleksey Polyvanyi <aleksey.polyvanyi@eonx.com>
2026-06-27 17:11:40 +0200

    -refactoring

--- packages/EasyHttpClient/tests/Application/src/Common/HttpClient/WithEventsHttpClientTest.php
+++ packages/EasyHttpClient/tests/Application/src/Common/HttpClient/WithEventsHttpClientTest.php
@@ -10,13 +10,24 @@ use EonX\EasyHttpClient\Tests\Application\AbstractApplicationTestCase;
 use EonX\EasyHttpClient\Tests\Fixture\App\Client\SomeClient;
 use EonX\EasyTest\HttpClient\Factory\TestResponseFactory;
 use EonX\EasyTest\HttpClient\Response\SimpleTestResponse;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Symfony\Component\HttpClient\Exception\TransportException;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use Throwable;
 
 final class WithEventsHttpClientTest extends AbstractApplicationTestCase
 {
-    public function testCanceledResponseDoesNotDispatchAnyEvent(): void
+    /**
+     * @see testResponseWithErrorStatusCodeStillDispatchesEvent
+     */
+    public static function provideErrorStatusCodes(): iterable
+    {
+        yield 'client error 404' => [404];
+
+        yield 'server error 502' => [502];
+    }
+
+    public function testCancelledResponseDoesNotDispatchAnyEvent(): void
     {
         TestResponseFactory::addResponse(new SimpleTestResponse('https://eonx.com/'));
         $sut = self::getContainer()->get(SomeClient::class);
@@ -75,5 +86,26 @@ final class WithEventsHttpClientTest extends AbstractApplicationTestCase
         self::assertNull($event->getResponseData());
         self::assertSame($throwable, $event->getThrowable());
         self::assertInstanceOf(DateTimeInterface::class, $event->getThrowableThrownAt());
+    }
+
+    #[DataProvider('provideErrorStatusCodes')]
+    public function testResponseWithErrorStatusCodeStillDispatchesEvent(int $statusCode): void
+    {
+        TestResponseFactory::addResponse(new SimpleTestResponse(url: 'https://eonx.com/', responseCode: $statusCode));
+        $sut = self::getContainer()->get(SomeClient::class);
+
+        $sut->makeRequest()
+            ->getContent(false);
+
+        /** @var \EonX\EasyTest\EasyEventDispatcher\Dispatcher\EventDispatcherStub $eventDispatcher */
+        $eventDispatcher = self::getContainer()->get(EventDispatcherInterface::class);
+        self::assertCount(1, $eventDispatcher->getDispatchedEvents());
+
+        /** @var \EonX\EasyHttpClient\Common\Event\HttpRequestSentEvent $event */
+        $event = $eventDispatcher->getDispatchedEvents()[0];
+
+        self::assertInstanceOf(ResponseData::class, $event->getResponseData());
+        self::assertSame($statusCode, $event->getResponseData()->getStatusCode());
+        self::assertNull($event->getThrowable());
     }
 }
