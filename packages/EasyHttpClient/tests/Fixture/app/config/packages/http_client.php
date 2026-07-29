24996bb837d29aea21e57444a5cdc2f0cbc643b6
Aleksey Polyvanyi <aleksey.polyvanyi@eonx.com>
2026-07-29 15:56:28 +0200

    -refactoring

--- packages/EasyHttpClient/tests/Fixture/app/config/packages/http_client.php
+++ packages/EasyHttpClient/tests/Fixture/app/config/packages/http_client.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use EonX\EasyTest\HttpClient\Factory\TestResponseFactory;
-use Symfony\Component\HttpClient\MockHttpClient;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->services()
-        ->set('http_client.transport', MockHttpClient::class)
-        ->args([service(TestResponseFactory::class)]);
-};
+return App::config([
+    'framework' => [
+        'http_client' => [
+            'mock_response_factory' => TestResponseFactory::class,
+        ],
+    ],
+]);
